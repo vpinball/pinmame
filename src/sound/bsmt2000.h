257a3e93720198db25531a67260fc0605aefcd79
wpcmame <wpcmame@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2001-11-28 20:30:40 +0000

    BSMT2000 emulation
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@23 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

---
+++ src/sound/bsmt2000.h
@@ -0,0 +1,27 @@
+/**********************************************************************************************
+ *
+ *   Data East BSMT2000 driver
+ *   by Aaron Giles
+ *
+ **********************************************************************************************/
+
+#ifndef BSMT2000_H
+#define BSMT2000_H
+
+#define MAX_BSMT2000 			1
+
+struct BSMT2000interface
+{
+	int num;                  						/* total number of chips */
+	int baseclock[MAX_BSMT2000];						/* input clock */
+	int region[MAX_BSMT2000];							/* memory region where the sample ROM lives */
+	int mixing_level[MAX_BSMT2000];					/* master volume */
+};
+
+int BSMT2000_sh_start(const struct MachineSound *msound);
+void BSMT2000_sh_stop(void);
+
+READ16_HANDLER( BSMT2000_data_0_r );
+WRITE16_HANDLER( BSMT2000_data_0_w );
+
+#endif
