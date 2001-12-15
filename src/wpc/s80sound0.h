256d92b5dc1548900707afb77f660576cfc391c3
sellenoff <sellenoff@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2001-12-15 23:07:25 +0000

    Support for S80B Sound
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@102 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/s80sound0.h
+++ src/wpc/s80sound0.h
@@ -9,6 +9,14 @@
 	ROM_LOAD(n2, 0x0c00,  0x0400, chk2) \
     ROM_RELOAD(  0xfc00,  0x0400) /* reset vector */
 
+/*-- Sound Only Board, 1 X 2K Sound Rom --*/
+#define S80SOUND2K_ROMSTART(n1,chk1,n2,chk2) \
+  SOUNDREGION(0x10000, S80_MEMREG_SCPU1) \
+    ROM_LOAD(n1, 0x0000,  0x0800, chk1) \
+    ROM_RELOAD(  0xf000,  0x0800) \
+    ROM_LOAD(n2, 0x0c00,  0x0400, chk2) \
+    ROM_RELOAD(  0xfc00,  0x0400) /* reset vector */
+
 #define SOUND_ROMS_NOT_AVAILABLE \
 	SOUNDREGION(0x10000, S80_MEMREG_SCPU1)
 
