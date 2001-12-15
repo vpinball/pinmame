6326acb942559efe9aa92e3346eab96f9a0d9c58
volkenborn <volkenborn@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2001-12-15 14:14:59 +0000

    Added IDW_STATUS since resource.h is not included.
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@99 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/Win32/status.c
+++ src/Win32/status.c
@@ -2,7 +2,7 @@
 
     M.A.M.E.32  -  Multiple Arcade Machine Emulator for Win32
     Win32 Portions Copyright (C) 1997-98 Michael Soderstrom and Chris Kirmse
-    
+
     This file is part of MAME32, and may only be used, modified and
     distributed under the terms of the MAME license, in "readme.txt".
     By continuing to use, modify or distribute this file you indicate
@@ -52,35 +52,36 @@ static HBRUSH hbrDark;
 static uclock_t prev_update_time;
 
 #define LED_AREA_WIDTH 52
+#define IDW_STATUS 112
 
 /***************************************************************************
-    External functions  
+    External functions
  ***************************************************************************/
 
 void StatusCreate(void)
 {
     RECT rect;
     INT  widths[2];
-    
+
     leds_old = 0;
 
     leds[0] = FALSE;
     leds[1] = FALSE;
     leds[2] = FALSE;
-    
+
     hStatus = CreateStatusWindow(WS_CHILD | WS_VISIBLE,"",MAME32App.m_hWnd,IDW_STATUS);
     GetClientRect(MAME32App.m_hWnd,&rect);
-    
+
     widths[0] = LED_AREA_WIDTH;
     widths[1] = -1;
-    
+
     SendMessage(hStatus, SB_SETPARTS, 2, (LPARAM)widths);
     SendMessage(hStatus, SB_SETTEXT, 0 | SBT_OWNERDRAW, 0);
     SendMessage(hStatus, SB_SETTEXT, 1, (LPARAM)"");
-    
+
     hbrLight = CreateSolidBrush(RGB(0xff, 0, 0));
     hbrDark  = CreateSolidBrush(RGB(0x3f, 0, 0));
-    
+
     prev_update_time = 0;
 
     bErasePauseText = FALSE;
@@ -89,12 +90,12 @@ void StatusCreate(void)
 int GetStatusHeight(void)
 {
     RECT rect;
-    
+
     if (hStatus == NULL)
         return 0;
-    
+
     GetWindowRect(hStatus,&rect);
-    
+
     return rect.bottom - rect.top;
 }
 
@@ -102,7 +103,7 @@ void StatusWindowSize(UINT state,int cx,int cy)
 {
     if (hStatus == NULL)
         return;
-    
+
     SendMessage(hStatus, WM_SIZE, (WPARAM)state, MAKELPARAM(cx, cy));
 }
 
@@ -112,7 +113,7 @@ void StatusWindowSize(UINT state,int cx,int cy)
 void StatusDrawItem(const DRAWITEMSTRUCT *lpdi)
 {
     RECT rect;
-    
+
     rect.top    = 6;
     rect.left   = 6;
     rect.bottom = rect.top  + LIGHT_SIZE;
@@ -130,7 +131,7 @@ void StatusDrawItem(const DRAWITEMSTRUCT *lpdi)
         rect.bottom++;
         rect.right++;
     }
-    
+
     rect.left += LIGHT_SEPARATION;
     rect.right = rect.left + LIGHT_SIZE;
     FillRect(lpdi->hDC, &rect, hbrDark);
@@ -146,7 +147,7 @@ void StatusDrawItem(const DRAWITEMSTRUCT *lpdi)
         rect.bottom++;
         rect.right++;
     }
-    
+
     rect.left += LIGHT_SEPARATION;
     rect.right = rect.left + LIGHT_SIZE;
     FillRect(lpdi->hDC, &rect, hbrDark);
@@ -163,15 +164,15 @@ void StatusDrawItem(const DRAWITEMSTRUCT *lpdi)
 void StatusWrite(int leds_status)
 {
     RECT rect;
-    
+
     if (hStatus == NULL)
         return;
-    
+
     if (leds_status == leds_old)
         return;
 
     leds_old = leds_status;
-    
+
     leds[0] = leds_status & 0x1;
     leds[1] = leds_status & 0x2;
     leds[2] = leds_status & 0x4;
@@ -207,10 +208,10 @@ void StatusUpdateFPS(BOOL bShow, int nSpeed, int nFPS, int nMachineFPS, int nFra
 {
     char buf[100];
     uclock_t now;
-    
+
     if (hStatus == NULL)
         return;
-    
+
     if (MAME32App.m_bMamePaused || MAME32App.m_bAutoPaused)
     {
         StatusUpdate();
@@ -224,27 +225,27 @@ void StatusUpdateFPS(BOOL bShow, int nSpeed, int nFPS, int nMachineFPS, int nFra
     }
 
     now = uclock();
-    
+
     /* update twice a second or so */
     if ((now - prev_update_time) < (UCLOCKS_PER_SEC/2))
         return;
-    
+
     sprintf(buf, "\t\tfskp%2d%4d%%%4d/%d fps", nFrameskip, nSpeed, nFPS, nMachineFPS);
-    
+
     SendMessage(hStatus, SB_SETTEXT, 1, (LPARAM)buf);
-    
+
     prev_update_time = now;
 }
 
 void StatusSetString(char *str)
 {
     char buf[100];
-    
+
     if (hStatus == NULL)
         return;
-    
+
     sprintf(buf, "\t\t%s", str);
-    
+
     SendMessage(hStatus, SB_SETTEXT, 1, (LPARAM)buf);
 }
 
