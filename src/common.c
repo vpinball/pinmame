e0efed63e45797fbbd5bc762b2ffaafbe0b9a3c0
volkenborn <volkenborn@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2002-02-03 16:58:24 +0000

    Inserted back the original disclaimer message
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@335 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/common.c
+++ src/common.c
@@ -83,14 +83,14 @@ void showdisclaimer(void)   /* MAURY_BEGIN: dichiarazione */
                "together with ROM images is a violation of copyright law and should be promptly\n"
                "reported to the authors so that appropriate legal action can be taken.\n\n");
 #else
-		        printf("MAME is an emulator: it reproduces, more or less faithfully, the behaviour\n"
-               "of several pinball arcade machines. But hardware is useless without software,\n"
-               "so an image of the ROMs which run on that hardware is required. Such ROMs, like\n"
-               "any other commercial software, are copyrighted material and it is therefore\n"
-               "illegal to use them if you don't own the original arcade machine. Needless to\n"
-               "say, ROMs are not distributed together with MAME. Distribution of MAME\n"
-               "together with ROM images is a violation of copyright law and should be promptly\n"
-               "reported to the authors so that appropriate legal action can be taken.\n\n");
+	printf("MAME is an emulator: it reproduces, more or less faithfully, the behaviour of\n"
+		 "several arcade machines. But hardware is useless without software, so an image\n"
+		 "of the ROMs which run on that hardware is required. Such ROMs, like any other\n"
+		 "commercial software, are copyrighted material and it is therefore illegal to\n"
+		 "use them if you don't own the original arcade machine. Needless to say, ROMs\n"
+		 "are not distributed together with MAME. Distribution of MAME together with ROM\n"
+		 "images is a violation of copyright law and should be promptly reported to the\n"
+		 "authors so that appropriate legal action can be taken.\n\n");
 #endif
 }                           /* MAURY_END: dichiarazione */
 
