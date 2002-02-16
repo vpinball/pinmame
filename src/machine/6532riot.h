94c9db1ec4ec8394e38f8600fca23f536d85cc5a
Tom Haukap <tom.haukap@t-online.de>
2002-02-16 15:41:13 +0000

    - did some renaming - corrected some timer issues
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@387 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/machine/6532riot.h
+++ src/machine/6532riot.h
@@ -10,15 +10,7 @@
 #ifndef RIOT_6532
 #define RIOT_6532
 
-#define MAX_RIOT 8
-
-#define RIOT_PORTA	0x00
-#define RIOT_DDRA	0x01
-#define RIOT_PORTB	0x02
-#define RIOT_DDRB	0x03
-
-#define RIOT_TIMER	0x00
-#define RIOT_IRF	0x01
+#define MAX_RIOT_6532 8
 
 struct riot6532_interface
 {
@@ -33,112 +25,112 @@ struct riot6532_interface
 extern "C" {
 #endif
 
-void riot_unconfig(void);
-void riot_config(int which, const struct riot6532_interface *intf);
-void riot_set_clock(int which, int clock);
-void riot_reset(void);
-int  riot_read(int which, int offset);
-void riot_write(int which, int offset, int data);
-void riot_set_input_a(int which, int data);
-void riot_set_input_b(int which, int data);
+void riot6532_unconfig(void);
+void riot6532_config(int which, const struct riot6532_interface *intf);
+void riot6532_set_clock(int which, int clock);
+void riot6532_reset(void);
+int  riot6532_read(int which, int offset);
+void riot6532_write(int which, int offset, int data);
+void riot6532_set_input_a(int which, int data);
+void riot6532_set_input_b(int which, int data);
 
 /******************* Standard 8-bit CPU interfaces, D0-D7 *******************/
 
-READ_HANDLER( riot_0_r );
-READ_HANDLER( riot_1_r );
-READ_HANDLER( riot_2_r );
-READ_HANDLER( riot_3_r );
-READ_HANDLER( riot_4_r );
-READ_HANDLER( riot_5_r );
-READ_HANDLER( riot_6_r );
-READ_HANDLER( riot_7_r );
+READ_HANDLER( riot6532_0_r );
+READ_HANDLER( riot6532_1_r );
+READ_HANDLER( riot6532_2_r );
+READ_HANDLER( riot6532_3_r );
+READ_HANDLER( riot6532_4_r );
+READ_HANDLER( riot6532_5_r );
+READ_HANDLER( riot6532_6_r );
+READ_HANDLER( riot6532_7_r );
 
-WRITE_HANDLER( riot_0_w );
-WRITE_HANDLER( riot_1_w );
-WRITE_HANDLER( riot_2_w );
-WRITE_HANDLER( riot_3_w );
-WRITE_HANDLER( riot_4_w );
-WRITE_HANDLER( riot_5_w );
-WRITE_HANDLER( riot_6_w );
-WRITE_HANDLER( riot_7_w );
+WRITE_HANDLER( riot6532_0_w );
+WRITE_HANDLER( riot6532_1_w );
+WRITE_HANDLER( riot6532_2_w );
+WRITE_HANDLER( riot6532_3_w );
+WRITE_HANDLER( riot6532_4_w );
+WRITE_HANDLER( riot6532_5_w );
+WRITE_HANDLER( riot6532_6_w );
+WRITE_HANDLER( riot6532_7_w );
 
 /******************* Standard 16-bit CPU interfaces, D0-D7 *******************/
 
-READ16_HANDLER( riot_0_lsb_r );
-READ16_HANDLER( riot_1_lsb_r );
-READ16_HANDLER( riot_2_lsb_r );
-READ16_HANDLER( riot_3_lsb_r );
-READ16_HANDLER( riot_4_lsb_r );
-READ16_HANDLER( riot_5_lsb_r );
-READ16_HANDLER( riot_6_lsb_r );
-READ16_HANDLER( riot_7_lsb_r );
+READ16_HANDLER( riot6532_0_lsb_r );
+READ16_HANDLER( riot6532_1_lsb_r );
+READ16_HANDLER( riot6532_2_lsb_r );
+READ16_HANDLER( riot6532_3_lsb_r );
+READ16_HANDLER( riot6532_4_lsb_r );
+READ16_HANDLER( riot6532_5_lsb_r );
+READ16_HANDLER( riot6532_6_lsb_r );
+READ16_HANDLER( riot6532_7_lsb_r );
 
-WRITE16_HANDLER( riot_0_lsb_w );
-WRITE16_HANDLER( riot_1_lsb_w );
-WRITE16_HANDLER( riot_2_lsb_w );
-WRITE16_HANDLER( riot_3_lsb_w );
-WRITE16_HANDLER( riot_4_lsb_w );
-WRITE16_HANDLER( riot_5_lsb_w );
-WRITE16_HANDLER( riot_6_lsb_w );
-WRITE16_HANDLER( riot_7_lsb_w );
+WRITE16_HANDLER( riot6532_0_lsb_w );
+WRITE16_HANDLER( riot6532_1_lsb_w );
+WRITE16_HANDLER( riot6532_2_lsb_w );
+WRITE16_HANDLER( riot6532_3_lsb_w );
+WRITE16_HANDLER( riot6532_4_lsb_w );
+WRITE16_HANDLER( riot6532_5_lsb_w );
+WRITE16_HANDLER( riot6532_6_lsb_w );
+WRITE16_HANDLER( riot6532_7_lsb_w );
 
 /******************* Standard 16-bit CPU interfaces, D8-D15 *******************/
 
-READ16_HANDLER( riot_0_msb_r );
-READ16_HANDLER( riot_1_msb_r );
-READ16_HANDLER( riot_2_msb_r );
-READ16_HANDLER( riot_3_msb_r );
-READ16_HANDLER( riot_4_msb_r );
-READ16_HANDLER( riot_5_msb_r );
-READ16_HANDLER( riot_6_msb_r );
-READ16_HANDLER( riot_7_msb_r );
+READ16_HANDLER( riot6532_0_msb_r );
+READ16_HANDLER( riot6532_1_msb_r );
+READ16_HANDLER( riot6532_2_msb_r );
+READ16_HANDLER( riot6532_3_msb_r );
+READ16_HANDLER( riot6532_4_msb_r );
+READ16_HANDLER( riot6532_5_msb_r );
+READ16_HANDLER( riot6532_6_msb_r );
+READ16_HANDLER( riot6532_7_msb_r );
 
-WRITE16_HANDLER( riot_0_msb_w );
-WRITE16_HANDLER( riot_1_msb_w );
-WRITE16_HANDLER( riot_2_msb_w );
-WRITE16_HANDLER( riot_3_msb_w );
-WRITE16_HANDLER( riot_4_msb_w );
-WRITE16_HANDLER( riot_5_msb_w );
-WRITE16_HANDLER( riot_6_msb_w );
-WRITE16_HANDLER( riot_7_msb_w );
+WRITE16_HANDLER( riot6532_0_msb_w );
+WRITE16_HANDLER( riot6532_1_msb_w );
+WRITE16_HANDLER( riot6532_2_msb_w );
+WRITE16_HANDLER( riot6532_3_msb_w );
+WRITE16_HANDLER( riot6532_4_msb_w );
+WRITE16_HANDLER( riot6532_5_msb_w );
+WRITE16_HANDLER( riot6532_6_msb_w );
+WRITE16_HANDLER( riot6532_7_msb_w );
 
 /******************* 8-bit A/B port interfaces *******************/
 
-WRITE_HANDLER( riot_0_porta_w );
-WRITE_HANDLER( riot_1_porta_w );
-WRITE_HANDLER( riot_2_porta_w );
-WRITE_HANDLER( riot_3_porta_w );
-WRITE_HANDLER( riot_4_porta_w );
-WRITE_HANDLER( riot_5_porta_w );
-WRITE_HANDLER( riot_6_porta_w );
-WRITE_HANDLER( riot_7_porta_w );
+WRITE_HANDLER( riot6532_0_porta_w );
+WRITE_HANDLER( riot6532_1_porta_w );
+WRITE_HANDLER( riot6532_2_porta_w );
+WRITE_HANDLER( riot6532_3_porta_w );
+WRITE_HANDLER( riot6532_4_porta_w );
+WRITE_HANDLER( riot6532_5_porta_w );
+WRITE_HANDLER( riot6532_6_porta_w );
+WRITE_HANDLER( riot6532_7_porta_w );
 
-WRITE_HANDLER( riot_0_portb_w );
-WRITE_HANDLER( riot_1_portb_w );
-WRITE_HANDLER( riot_2_portb_w );
-WRITE_HANDLER( riot_3_portb_w );
-WRITE_HANDLER( riot_4_portb_w );
-WRITE_HANDLER( riot_5_portb_w );
-WRITE_HANDLER( riot_6_portb_w );
-WRITE_HANDLER( riot_7_portb_w );
+WRITE_HANDLER( riot6532_0_portb_w );
+WRITE_HANDLER( riot6532_1_portb_w );
+WRITE_HANDLER( riot6532_2_portb_w );
+WRITE_HANDLER( riot6532_3_portb_w );
+WRITE_HANDLER( riot6532_4_portb_w );
+WRITE_HANDLER( riot6532_5_portb_w );
+WRITE_HANDLER( riot6532_6_portb_w );
+WRITE_HANDLER( riot6532_7_portb_w );
 
-READ_HANDLER( riot_0_porta_r );
-READ_HANDLER( riot_1_porta_r );
-READ_HANDLER( riot_2_porta_r );
-READ_HANDLER( riot_3_porta_r );
-READ_HANDLER( riot_4_porta_r );
-READ_HANDLER( riot_5_porta_r );
-READ_HANDLER( riot_6_porta_r );
-READ_HANDLER( riot_7_porta_r );
+READ_HANDLER( riot6532_0_porta_r );
+READ_HANDLER( riot6532_1_porta_r );
+READ_HANDLER( riot6532_2_porta_r );
+READ_HANDLER( riot6532_3_porta_r );
+READ_HANDLER( riot6532_4_porta_r );
+READ_HANDLER( riot6532_5_porta_r );
+READ_HANDLER( riot6532_6_porta_r );
+READ_HANDLER( riot6532_7_porta_r );
 
-READ_HANDLER( riot_0_portb_r );
-READ_HANDLER( riot_1_portb_r );
-READ_HANDLER( riot_2_portb_r );
-READ_HANDLER( riot_3_portb_r );
-READ_HANDLER( riot_4_portb_r );
-READ_HANDLER( riot_5_portb_r );
-READ_HANDLER( riot_6_portb_r );
-READ_HANDLER( riot_7_portb_r );
+READ_HANDLER( riot6532_0_portb_r );
+READ_HANDLER( riot6532_1_portb_r );
+READ_HANDLER( riot6532_2_portb_r );
+READ_HANDLER( riot6532_3_portb_r );
+READ_HANDLER( riot6532_4_portb_r );
+READ_HANDLER( riot6532_5_portb_r );
+READ_HANDLER( riot6532_6_portb_r );
+READ_HANDLER( riot6532_7_portb_r );
 
 #ifdef __cplusplus
 }
