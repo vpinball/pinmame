176d8f40bdb77b8d408cff457a2ce0e83676d11e
volkenborn <volkenborn@yahoo.com>
2002-03-02 14:43:15 +0000

    Changed it so it can be integrated in PinMAME.
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@455 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/sound/votrax.c
+++ src/sound/votrax.c
@@ -30,16 +30,18 @@ struct  GameSamples *VotraxSamples;
 
 static const char *VotraxTable[65] =
 {
- "EH3","EH2","EH1","PA0","DT" ,"A1" ,"A2" ,"ZH",
- "AH2","I3" ,"I2" ,"I1" ,"M"  ,"N"  ,"B"  ,"V",
- "CH" ,"SH" ,"Z"  ,"AW1","NG" ,"AH1","OO1","OO",
- "L"  ,"K"  ,"J"  ,"H"  ,"G"  ,"F"  ,"D"  ,"S",
- "A"  ,"AY" ,"Y1" ,"UH3","AH" ,"P"  ,"O"  ,"I",
- "U"  ,"Y"  ,"T"  ,"R"  ,"E"  ,"W"  ,"AE" ,"AE1",
- "AW2","UH2","UH1","UH" ,"O2" ,"O1" ,"IU" ,"U1",
- "THV","TH" ,"ER" ,"EH" ,"E1" ,"AW" ,"PA1","STOP",
+ "EH3.WAV","EH2.WAV","EH1.WAV","PA0.WAV","DT.WAV" ,"A1.WAV" ,"A2.WAV" ,"ZH.WAV",
+ "AH2.WAV","I3.WAV" ,"I2.WAV" ,"I1.WAV" ,"M.WAV"  ,"N.WAV"  ,"B.WAV"  ,"V.WAV",
+ "CH.WAV" ,"SH.WAV" ,"Z.WAV"  ,"AW1.WAV","NG.WAV" ,"AH1.WAV","OO1.WAV","OO.WAV",
+ "L.WAV"  ,"K.WAV"  ,"J.WAV"  ,"H.WAV"  ,"G.WAV"  ,"F.WAV"  ,"D.WAV"  ,"S.WAV",
+ "A.WAV"  ,"AY.WAV" ,"Y1.WAV" ,"UH3.WAV","AH.WAV" ,"P.WAV"  ,"O.WAV"  ,"I.WAV",
+ "U.WAV"  ,"Y.WAV"  ,"T.WAV"  ,"R.WAV"  ,"E.WAV"  ,"W.WAV"  ,"AE.WAV" ,"AE1.WAV",
+ "AW2.WAV","UH2.WAV","UH1.WAV","UH.WAV" ,"O2.WAV" ,"O1.WAV" ,"IU.WAV" ,"U1.WAV",
+ "THV.WAV","TH.WAV" ,"ER.WAV" ,"EH.WAV" ,"E1.WAV" ,"AW.WAV" ,"PA1.WAV","STOP.WAV",
  0
 };
+
+void votrax_w(int data);
 
 void sh_votrax_start(int Channel)
 {
@@ -54,30 +56,45 @@ void sh_votrax_stop(void)
 	freesamples(VotraxSamples);
 }
 
+void repeat_votrax_w(int dummy)
+{
+	votrax_w(-1);
+}
+
 void votrax_w(int data)
 {
-	int Phoneme,Intonation;
+	static int buffer[128];
+	static int pos;
+	int i,Phoneme,Intonation;
 
-    Phoneme = data & 0x3F;
-    Intonation = data >> 6;
-
-  	logerror("Speech : %s at intonation %d\n",VotraxTable[Phoneme],Intonation);
-
-    if(Phoneme==63)
-   		mixer_stop_sample(VotraxChannel);
-
-    if(VotraxSamples->sample[Phoneme])
-	{
-		mixer_set_volume(VotraxChannel,VotraxBaseVolume+(8*Intonation)*100/255);
-		mixer_play_sample(VotraxChannel,VotraxSamples->sample[Phoneme]->data,
-				  VotraxSamples->sample[Phoneme]->length,
-				  VotraxBaseFrequency+(256*Intonation),
-				  0);
+	if (data >= 0) {
+		if (pos < 128) buffer[pos++] = data;
 	}
+	if (pos > 0 && votrax_status_r() < 1) {
+		data = buffer[0];
+		pos--;
+		for (i=0; i < pos; i++)
+			buffer[i] = buffer[i+1];
+		Phoneme = data & 0x3F;
+		Intonation = data >> 6;
+		logerror("Speech : %s at intonation %d\n",VotraxTable[Phoneme],Intonation);
+/*
+		if(Phoneme==63)
+			mixer_stop_sample(VotraxChannel);
+*/
+		if(VotraxSamples->sample[Phoneme])
+		{
+			mixer_set_volume(VotraxChannel,VotraxBaseVolume+(8*Intonation)*100/255);
+			mixer_play_sample(VotraxChannel,VotraxSamples->sample[Phoneme]->data,
+					  VotraxSamples->sample[Phoneme]->length,
+					  14000 /*VotraxBaseFrequency+(256*Intonation)*/,
+					  0);
+		}
+	} else
+		timer_set(TIME_IN_USEC(24000),0,repeat_votrax_w);
 }
 
 int votrax_status_r(void)
 {
     return mixer_is_sample_playing(VotraxChannel);
 }
-
