7784043179fef45ecac097107fb69a24c5f78e8f
wpcmame <no.author@mail.com>
2002-03-06 20:47:12 +0000

    multi intensity 128x16 + cleanup
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@481 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/dedmd.h
+++ src/wpc/dedmd.h
@@ -1,24 +1,20 @@
 #ifndef INC_DEDMD
 #define INC_DEDMD
 
-extern void de_dmd128x16_refresh(struct mame_bitmap *bitmap,int full_refresh);
-extern void de2_dmd128x16_refresh(struct mame_bitmap *bitmap,int full_refresh);
-extern void de_dmd128x32_refresh(struct mame_bitmap *bitmap,int full_refresh);
-extern void de_dmd192x64_refresh(struct mame_bitmap *bitmap,int full_refresh);
-
 /*--------------------- DMD 128x32 -------------------*/
+extern void de_dmd32refresh(struct mame_bitmap *bitmap,int full_refresh);
 extern const struct Memory_ReadAddress  de_dmd32readmem[];
 extern const struct Memory_WriteAddress de_dmd32writemem[];
 
 #define DE_DMD32CPUNO     2
-#define DE_DMD32CPUREGION (REGION_CPU1  + DE_DMD32CPUNO)
-#define DE_DMD32ROMREGION (REGION_USER1 + DE_DMD32CPUNO)
+#define DE_DMD32CPUREGION (REGION_CPU1 + DE_DMD32CPUNO)
+#define DE_DMD32ROMREGION (REGION_GFX1 + DE_DMD32CPUNO)
 
 #define DE_DMD32CPU { \
   CPU_M6809, 4000000, /* 4 Mhz*/ \
   de_dmd32readmem, de_dmd32writemem, \
 }
-#define DE_DMD32VIDEO  NULL, NULL, de_dmd128x32_refresh
+#define DE_DMD32VIDEO  NULL, NULL, de_dmd32refresh
 
 #define DE_DMD32ROM44(n1,chk1,n2,chk2) \
   NORMALREGION(0x10000, DE_DMD32CPUREGION) \
@@ -32,6 +28,7 @@ extern const struct Memory_WriteAddress de_dmd32writemem[];
     ROM_LOAD(n1, 0x00000, 0x80000, chk1) \
 
 /*--------------------- DMD 192x64 -------------------*/
+extern void de_dmd64refresh(struct mame_bitmap *bitmap,int full_refresh);
 extern const struct Memory_ReadAddress16  de_dmd64readmem[];
 extern const struct Memory_WriteAddress16 de_dmd64writemem[];
 
@@ -42,7 +39,7 @@ extern const struct Memory_WriteAddress16 de_dmd64writemem[];
   CPU_M68000, 6000000, /* 12 Mhz*/ \
   de_dmd64readmem, de_dmd64writemem \
 }
-#define DE_DMD64VIDEO NULL, NULL, de_dmd192x64_refresh
+#define DE_DMD64VIDEO NULL, NULL, de_dmd64refresh
 
 #define DE_DMD64ROM88(n1,chk1,n2,chk2) \
   NORMALREGION(0x01000000, DE_DMD64CPUREGION) \
@@ -50,28 +47,32 @@ extern const struct Memory_WriteAddress16 de_dmd64writemem[];
     ROM_LOAD16_BYTE(n2, 0x00000000, 0x00080000, chk2)
 
 /*--------------------- DMD 128x16 -------------------*/
+extern void de_dmd16refresh(struct mame_bitmap *bitmap,int full_refresh);
 extern const struct Memory_ReadAddress  de_dmd16readmem[];
 extern const struct Memory_WriteAddress de_dmd16writemem[];
 extern const struct IO_ReadPort         de_dmd16readport[];
 extern const struct IO_WritePort        de_dmd16writeport[];
 #define DE_DMD16CPUNO 2
 #define DE_DMD16CPUREGION (REGION_CPU1  + DE_DMD16CPUNO)
-#define DE_DMD16ROMREGION (REGION_USER1 + DE_DMD16CPUNO)
+#define DE_DMD16ROMREGION (REGION_GFX1  + DE_DMD16CPUNO)
+#define DE_DMD16DMDREGION (REGION_USER1 + DE_DMD16CPUNO)
 
 #define DE_DMD16CPU { \
   CPU_Z80, 2000000, /* 4 Mhz */ \
   de_dmd16readmem, de_dmd16writemem, de_dmd16readport, de_dmd16writeport \
 }
-#define DE_DMD16VIDEO  NULL, NULL, de2_dmd128x16_refresh
+#define DE_DMD16VIDEO  NULL, NULL, de_dmd16refresh
 
 #define DE_DMD16ROM1(n1,chk1) \
   NORMALREGION(0x10000, DE_DMD16CPUREGION) \
+  NORMALREGION(0x400,   DE_DMD16DMDREGION) /* 16x128x2bitsx2buffers */ \
   NORMALREGION(0x20000, DE_DMD16ROMREGION) \
     ROM_LOAD(n1, 0x00000, 0x10000, chk1) \
       ROM_RELOAD(0x10000, 0x10000)
 
 #define DE_DMD16ROM2(n1,chk1) \
   NORMALREGION(0x10000, DE_DMD16CPUREGION) \
+  NORMALREGION(0x400,   DE_DMD16DMDREGION) /* 16x128x2bitsx2buffers */ \
   NORMALREGION(0x20000, DE_DMD16ROMREGION) \
     ROM_LOAD(n1, 0x00000, 0x20000, chk1)
 
