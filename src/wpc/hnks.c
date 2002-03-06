3cd0d044ebef331af3c9ea56fecfb893e8a73cd0
haukap <haukap@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2002-03-06 20:04:02 +0000

    sound works (with some (minor?) issues)
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@479 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/hnks.c
+++ src/wpc/hnks.c
@@ -14,11 +14,25 @@
 / 0x0000 - 0x007F: RAM
 / 0x0080 - 0x0083: PIA I/O
 / 0x1000 - 0x17FF: ROM
+/ 
+/ (the wavetable ROM is mapped to f000-f1ff 
+   and the main rom is repeated at f800-ffff
+   for the interupt vector)
 /-----------------------------------------*/
+
+/*
+  - pitch handling isn't implemented
+  - can't say if the waveform is correct
+  - base pitch is set to 1000Hz, not sure if this is ok
+  - counter reset/hold not implemented
+*/
 
 static struct {
   struct sndbrdData brdData;
   int sndCmd;
+
+  int   channel;
+  INT8  wavetable[16][32];
 } hnks_locals;
 
 #define SP_PIA0  2
@@ -39,17 +53,24 @@ MEMORY_WRITE_START(hnks_writemem)
   { 0xf800, 0xffff, MWA_ROM }, /* reset vector */
 MEMORY_END
 
+/*
+  PA4-PA7 : output: select waveform (0-15, upper bits of wave table ROM)
+*/
 static WRITE_HANDLER(pia0a_w)
 {
+  mixer_play_sample(hnks_locals.channel, (char*) &hnks_locals.wavetable[(data&0xf0)>>4], 32, 1000, 1);
 }
 
+/*
+  PB0-PB3 : output: replay speed
+  PB4-PB7 : output: set volume
+*/
 static WRITE_HANDLER(pia0b_w)
 {
-}
-
-static void pia0_irq(int state)
-{
-  cpu_set_irq_line(hnks_locals.brdData.cpuNo, M6802_IRQ_LINE, state ? ASSERT_LINE : CLEAR_LINE);
+  // pitch is not supported yet
+  // logerror("speed : %02x\n", data&0x0f);
+  
+  mixer_set_volume(hnks_locals.channel, ((data&0xf0)>>4)*0x10);
 }
 
 /*
@@ -64,20 +85,39 @@ static void pia0_irq(int state)
 static const struct pia6821_interface sp_pia = {
   /*i: A/B,CA/B1,CA/B2 */ 0, 0, 0, 0, 0, 0,
   /*o: A/B,CA/B2       */ pia0a_w, pia0b_w, 0, 0,
-  /*irq: A/B           */ pia0_irq, pia0_irq
+  /*irq: A/B           */ 0,0
 };
 
 static WRITE_HANDLER(hnks_data_w)
 {
-	hnks_locals.sndCmd = data & 0x0f;
+    pia_set_input_a(SP_PIA0, data&0x0f);
+    cpu_set_irq_line(hnks_locals.brdData.cpuNo, M6802_IRQ_LINE, PULSE_LINE);
 }
 
-static void hnks_init(struct sndbrdData *brdData) {
-  logerror("sound init\n");
+static void hnks_init(struct sndbrdData *brdData)
+{
+  int i,j;
   memset(&hnks_locals, 0x00, sizeof(hnks_locals));
   hnks_locals.brdData = *brdData;
 
+  /* load the "wave table" (from the ROM, it's mapped to 0xf000-0xf1ff */
+  for (i=0; i<16; i++) {
+	  unsigned char* adr = memory_region(HNK_MEMREG_SCPU) + 0xf000 + (i*32);
+	  for(j=0; j<32; j++)
+		hnks_locals.wavetable[i][j] = (*adr++)<<4;
+  }
+
   pia_config(SP_PIA0, PIA_STANDARD_ORDERING, &sp_pia);
+}
+
+static int hnks_sh_start(const struct MachineSound *msound) {
+  hnks_locals.channel = mixer_allocate_channel(15);
+  mixer_set_volume(hnks_locals.channel,0x00);
+  return 0;
+}
+
+static void hnks_sh_stop(void) {
+  mixer_stop_sample(hnks_locals.channel);
 }
 
 /*-------------------
@@ -86,4 +126,6 @@ static void hnks_init(struct sndbrdData *brdData) {
 const struct sndbrdIntf hankinIntf = {
   hnks_init, NULL, NULL, hnks_data_w, NULL, NULL, NULL, SNDBRD_NODATASYNC|SNDBRD_NOCTRLSYNC
 };
+
+struct CustomSound_interface hnks_custInt = {hnks_sh_start, hnks_sh_stop};
 
