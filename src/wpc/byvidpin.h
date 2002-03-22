b6098fd38d5660463cb12d36afa84bde9909ba73
volkenborn <volkenborn@yahoo.com>
2002-03-22 21:12:01 +0000

    Added dip names, switched to Shift keys for flipper buttons.
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@551 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/byvidpin.h
+++ src/wpc/byvidpin.h
@@ -1,7 +1,15 @@
 #ifndef INC_BYVP
 #define INC_BYVP
 
-/* DIP SWITCH SETTINGS: 
+#include "core.h"
+#include "wpcsam.h"
+#include "sim.h"
+
+#define BYVP_SOLSMOOTH       2 /* Smooth the Solenoids over this numer of VBLANKS */
+#define BYVP_LAMPSMOOTH      2 /* Smooth the lamps over this number of VBLANKS */
+#define BYVP_DISPLAYSMOOTH   4 /* Smooth the display over this number of VBLANKS */
+
+/* BABY PACMAN DIP SWITCH SETTINGS:
 
  05 04 03 02 01 (Right Side Coin Chute)		Credits Per Coin
  13 12 11 10 09 (Left Side Coin Chute)
@@ -15,7 +23,7 @@
   07 = Show Fruit @ Start of Game (ON = YES, OFF = NO)
   08 = Side Tunnels Open @ Start of Game (ON = YES, OFF = NO)
   09 - 13 (See Coin Settings Above)
-  14 = Outhole Ball Adjustmeng:
+  14 = Outhole Ball Adjustment:
        (ON = After 3 Ball ejects and no score, ball still in play!)
 	   (OFF = After 3 Ball ejects and no score, maze will lite)
 
@@ -48,59 +56,226 @@
   1   1  2
 */
 
-#include "core.h"
-#include "wpcsam.h"
-#include "sim.h"
-
-#define BYVP_SOLSMOOTH       4 /* Smooth the Solenoids over this numer of VBLANKS */
-#define BYVP_LAMPSMOOTH      4 /* Smooth the lamps over this number of VBLANKS */
-#define BYVP_DISPLAYSMOOTH   4 /* Smooth the display over this number of VBLANKS */
-
-#define BYVP_DIPS \
+#define BYVP_babypac_PORT \
+  PORT_START /* 0 */ \
+    /* These are put in switch column 0 */ \
+    COREPORT_BIT(     0x0001, "Self Test",        KEYCODE_7) \
+    COREPORT_BIT(     0x0008, "Video Diagnostic", KEYCODE_8) \
+    COREPORT_BIT(     0x0002, "CPU Diagnostic",   KEYCODE_9) \
+    COREPORT_BIT(     0x0004, "Sound Diagnostic", KEYCODE_0) \
+    COREPORT_BIT(     0x0020, "Joystick Left",	  KEYCODE_LEFT) \
+    COREPORT_BIT(     0x0010, "Joystick Right",	  KEYCODE_RIGHT) \
+    COREPORT_BIT(     0x0080, "Joystick Up",	  KEYCODE_UP) \
+    COREPORT_BIT(     0x0040, "Joystick Down",	  KEYCODE_DOWN) \
+    /* Switch Column 1 (Switches #6 & #3)*/ \
+    COREPORT_BITDEF(  0x0200, IPT_START1,         IP_KEY_DEFAULT) \
+    COREPORT_BITDEF(  0x0100, IPT_START2,         IP_KEY_DEFAULT) \
+    /* Switch Column 2 (Switches #1,#2,#7,#8)*/ \
+    COREPORT_BITDEF(  0x0800, IPT_COIN1,          IP_KEY_DEFAULT) \
+    COREPORT_BITDEF(  0x0400, IPT_COIN2,          IP_KEY_DEFAULT) \
+    COREPORT_BIT(     0x1000, "Ball Tilt",        KEYCODE_DEL) \
+    COREPORT_BIT(     0x2000, "Slam Tilt",        KEYCODE_HOME) \
   PORT_START /* 1 */ \
-    COREPORT_DIPNAME( 0x0001, 0x0000, "S1") \
+    COREPORT_DIPNAME( 0x0001, 0x0000, "Rt. Coin Chute #1") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0001, "1" ) \
-    COREPORT_DIPNAME( 0x0002, 0x0000, "S2") \
+    COREPORT_DIPNAME( 0x0002, 0x0000, "Rt. Coin Chute #2") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0002, "1" ) \
-    COREPORT_DIPNAME( 0x0004, 0x0000, "S3") \
+    COREPORT_DIPNAME( 0x0004, 0x0000, "Rt. Coin Chute #3") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0004, "1" ) \
-    COREPORT_DIPNAME( 0x0008, 0x0000, "S4") \
+    COREPORT_DIPNAME( 0x0008, 0x0000, "Rt. Coin Chute #4") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0008, "1" ) \
-    COREPORT_DIPNAME( 0x0010, 0x0000, "S5") \
+    COREPORT_DIPNAME( 0x0010, 0x0000, "Rt. Coin Chute #5") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0010, "1" ) \
-    COREPORT_DIPNAME( 0x0020, 0x0000, "S6") \
+    COREPORT_DIPNAME( 0x0020, 0x0000, "Center Arrow Memory") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0020, "1" ) \
-    COREPORT_DIPNAME( 0x0040, 0x0040, "S7") \
+    COREPORT_DIPNAME( 0x0040, 0x0040, "Fruit at Start") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0040, "1" ) \
-    COREPORT_DIPNAME( 0x0080, 0x0080, "S8") \
+    COREPORT_DIPNAME( 0x0080, 0x0080, "Side Tunnels open") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0080, "1" ) \
-    COREPORT_DIPNAME( 0x0100, 0x0000, "S9") \
+    COREPORT_DIPNAME( 0x0100, 0x0000, "Lt. Coin Chute #1") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0100, "1" ) \
-    COREPORT_DIPNAME( 0x0200, 0x0000, "S10") \
+    COREPORT_DIPNAME( 0x0200, 0x0000, "Lt. Coin Chute #2") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0200, "1" ) \
-    COREPORT_DIPNAME( 0x0400, 0x0000, "S11") \
+    COREPORT_DIPNAME( 0x0400, 0x0000, "Lt. Coin Chute #3") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0400, "1" ) \
-    COREPORT_DIPNAME( 0x0800, 0x0000, "S12") \
+    COREPORT_DIPNAME( 0x0800, 0x0000, "Lt. Coin Chute #4") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0800, "1" ) \
-    COREPORT_DIPNAME( 0x1000, 0x0000, "S13") \
+    COREPORT_DIPNAME( 0x1000, 0x0000, "Lt. Coin Chute #5") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x1000, "1" ) \
+    COREPORT_DIPNAME( 0x2000, 0x0000, "Force Pinball Mode") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x2000, "1" ) \
+    COREPORT_DIPNAME( 0x4000, 0x0000, "Energizer Memory") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x4000, "1" ) \
+    COREPORT_DIPNAME( 0x8000, 0x0000, "Disable Video Game") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x8000, "1" ) \
+  PORT_START /* 2 */ \
+    COREPORT_DIPNAME( 0x0001, 0x0000, "S17") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0001, "1" ) \
+    COREPORT_DIPNAME( 0x0002, 0x0000, "S18") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0002, "1" ) \
+    COREPORT_DIPNAME( 0x0004, 0x0000, "S19") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0004, "1" ) \
+    COREPORT_DIPNAME( 0x0008, 0x0000, "S20") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0008, "1" ) \
+    COREPORT_DIPNAME( 0x0010, 0x0000, "S21") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0010, "1" ) \
+    COREPORT_DIPNAME( 0x0020, 0x0000, "No. Mazes for Special #1") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0020, "1" ) \
+    COREPORT_DIPNAME( 0x0040, 0x0000, "No. Mazes for Special #2") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0040, "1" ) \
+    COREPORT_DIPNAME( 0x0080, 0x0000, "Center Arrows after Drain") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0080, "1" ) \
+    COREPORT_DIPNAME( 0x0100, 0x0000, "S25") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0100, "1" ) \
+    COREPORT_DIPNAME( 0x0200, 0x0000, "S26") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0200, "1" ) \
+    COREPORT_DIPNAME( 0x0400, 0x0400, "Credits Display") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0400, "1" ) \
+    COREPORT_DIPNAME( 0x0800, 0x0000, "S28") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0800, "1" ) \
+    COREPORT_DIPNAME( 0x1000, 0x0000, "S29") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x1000, "1" ) \
+    COREPORT_DIPNAME( 0x2000, 0x0000, "Free Play ") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x2000, "1" ) \
+    COREPORT_DIPNAME( 0x4000, 0x0000, "Lives per Game #1") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x4000, "1" ) \
+    COREPORT_DIPNAME( 0x8000, 0x0000, "Lives per Game #2") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x8000, "1" )
+
+/* GRANNY & THE GATORS DIP SWITCH SETTINGS:
+
+ 05 04 03 02 01 (Right Side Coin Chute)		Credits Per Coin
+ 13 12 11 10 09 (Left Side Coin Chute)
+ --------------
+  0 0 0 0 0  1/1 Coin
+  ....
+  1 1 1 1 1  2/5 Coins
+
+  01 - 05 (See Coin Settings Above)
+  06 = Extra canoe feature: (on = short hoop, off = long hoop)
+       Making the top right loop flashes a center rollover button,
+       making this afterwards will stop the flashing and light it permanently.
+       Making all 8 rollovers lights the "Extra canoe" light.
+  07 = ??
+  08 = Center rollover button lights (on = come on for next canoe, off = won't)
+  09 - 13 (See Coin Settings Above)
+  14 = ??
+  15 = "Extra canoe" light memory (on = yes)
+  16 - 23 = ??
+  24 = "Exit to video" light memory
+       on = recall for each new canoe,
+       off = recall after power paddle)
+  25 = ??
+  26 = ??
+  27 = Display # of Credits
+  28 = ??
+  29 = ??
+  30 = Free Play? (ON = YES, OFF = NO)
+
+  32 31	# of Canoes (Lives) per game
+  -----
+  0   0  3
+  0   1  5
+  1   0  4
+  1   1  2
+*/
+
+#define BYVP_granny_PORT \
+  PORT_START /* 0 */ \
+    /* These are put in switch column 0 */ \
+    COREPORT_BIT(     0x0001, "Self Test",        KEYCODE_7) \
+    COREPORT_BIT(     0x0008, "Video Diagnostic", KEYCODE_8) \
+    COREPORT_BIT(     0x0002, "CPU Diagnostic",   KEYCODE_9) \
+    COREPORT_BIT(     0x0004, "Sound Diagnostic", KEYCODE_0) \
+    COREPORT_BIT(     0x0020, "Paddle Left",      KEYCODE_LEFT) \
+    COREPORT_BIT(     0x0010, "Paddle Right",     KEYCODE_RIGHT) \
+    COREPORT_BIT(     0x0040, "Fire/Start 1",     KEYCODE_LCONTROL) \
+    COREPORT_BIT(     0x0080, "Fire/Start 2",     KEYCODE_RCONTROL) \
+    /* Switch Column 2 (Switches #8,#6,#5)*/ \
+    COREPORT_BIT(     0x4000, "Power Paddle",     KEYCODE_UP) \
+    COREPORT_BITDEF(  0x0200, IPT_START1,         IP_KEY_DEFAULT) \
+    COREPORT_BITDEF(  0x0100, IPT_START2,         IP_KEY_DEFAULT) \
+    /* Switch Column 2 (Switches #1,#2,#7)*/ \
+    COREPORT_BITDEF(  0x0800, IPT_COIN1,          IP_KEY_DEFAULT) \
+    COREPORT_BITDEF(  0x0400, IPT_COIN2,          IP_KEY_DEFAULT) \
+    COREPORT_BIT(     0x1000, "Ball Tilt",        KEYCODE_DEL) \
+    COREPORT_BIT(     0x2000, "Slam Tilt",        KEYCODE_HOME) \
+  PORT_START /* 1 */ \
+    COREPORT_DIPNAME( 0x0001, 0x0000, "Rt. Coin Chute #1") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0001, "1" ) \
+    COREPORT_DIPNAME( 0x0002, 0x0000, "Rt. Coin Chute #2") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0002, "1" ) \
+    COREPORT_DIPNAME( 0x0004, 0x0000, "Rt. Coin Chute #3") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0004, "1" ) \
+    COREPORT_DIPNAME( 0x0008, 0x0000, "Rt. Coin Chute #4") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0008, "1" ) \
+    COREPORT_DIPNAME( 0x0010, 0x0000, "Rt. Coin Chute #5") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0010, "1" ) \
+    COREPORT_DIPNAME( 0x0020, 0x0000, "Easy Hoop Shot") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0020, "1" ) \
+    COREPORT_DIPNAME( 0x0040, 0x0000, "S7") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0040, "1" ) \
+    COREPORT_DIPNAME( 0x0080, 0x0000, "Rollover Lights Memory") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0080, "1" ) \
+    COREPORT_DIPNAME( 0x0100, 0x0000, "Lt. Coin Chute #1") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0100, "1" ) \
+    COREPORT_DIPNAME( 0x0200, 0x0000, "Lt. Coin Chute #2") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0200, "1" ) \
+    COREPORT_DIPNAME( 0x0400, 0x0000, "Lt. Coin Chute #3") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0400, "1" ) \
+    COREPORT_DIPNAME( 0x0800, 0x0000, "Lt. Coin Chute #4") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0800, "1" ) \
+    COREPORT_DIPNAME( 0x1000, 0x0000, "Lt. Coin Chute #5") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x1000, "1" ) \
     COREPORT_DIPNAME( 0x2000, 0x0000, "S14") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x2000, "1" ) \
-    COREPORT_DIPNAME( 0x4000, 0x0000, "S15") \
+    COREPORT_DIPNAME( 0x4000, 0x4000, "EXTRA CANOE Light Memory") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x4000, "1" ) \
     COREPORT_DIPNAME( 0x8000, 0x0000, "S16") \
@@ -128,7 +303,7 @@
     COREPORT_DIPNAME( 0x0040, 0x0000, "S23") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0040, "1" ) \
-    COREPORT_DIPNAME( 0x0080, 0x0000, "S24") \
+    COREPORT_DIPNAME( 0x0080, 0x0080, "EXIT TO VIDEO for new Canoe") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0080, "1" ) \
     COREPORT_DIPNAME( 0x0100, 0x0000, "S25") \
@@ -137,7 +312,7 @@
     COREPORT_DIPNAME( 0x0200, 0x0000, "S26") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0200, "1" ) \
-    COREPORT_DIPNAME( 0x0400, 0x0400, "S27") \
+    COREPORT_DIPNAME( 0x0400, 0x0400, "Credits Display") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0400, "1" ) \
     COREPORT_DIPNAME( 0x0800, 0x0000, "S28") \
@@ -146,60 +321,15 @@
     COREPORT_DIPNAME( 0x1000, 0x0000, "S29") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x1000, "1" ) \
-    COREPORT_DIPNAME( 0x2000, 0x0000, "S30") \
+    COREPORT_DIPNAME( 0x2000, 0x0000, "Free Play ") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x2000, "1" ) \
-    COREPORT_DIPNAME( 0x4000, 0x0000, "S31") \
+    COREPORT_DIPNAME( 0x4000, 0x0000, "Canoes per Game #1") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x4000, "1" ) \
-    COREPORT_DIPNAME( 0x8000, 0x0000, "S32") \
+    COREPORT_DIPNAME( 0x8000, 0x0000, "Canoes per Game #2") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x8000, "1" )
-
-
-#define BYVP_babypac_PORT \
-  PORT_START /* 0 */ \
-    /* These are put in switch column 0 */ \
-    COREPORT_BIT(     0x0001, "Self Test",        KEYCODE_7) \
-    COREPORT_BIT(     0x0002, "CPU Diagnostic",   KEYCODE_9) \
-    COREPORT_BIT(     0x0004, "Sound Diagnostic", KEYCODE_0) \
-    COREPORT_BIT(     0x0008, "Video Diagnostic", KEYCODE_8) \
-	COREPORT_BIT(	  0x0010, "Joystick Right",	  KEYCODE_RIGHT) \
-    COREPORT_BIT(	  0x0020, "Joystick Left",	  KEYCODE_LEFT) \
-	COREPORT_BIT(	  0x0040, "Joystick Down",	  KEYCODE_DOWN) \
-	COREPORT_BIT(	  0x0080, "Joystick Up",	  KEYCODE_UP) \
-    /* Switch Column 1 (Switches #3 & #6)*/ \
-    COREPORT_BITDEF(  0x0100, IPT_START2,        KEYCODE_2)  \
-    COREPORT_BITDEF(  0x0200, IPT_START1,        KEYCODE_1)  \
-    /* Switch Column 2 (Switches #1,#2,#7,#8)*/ \
-    COREPORT_BITDEF(  0x0400, IPT_COIN1,          IP_KEY_DEFAULT) \
-    COREPORT_BITDEF(  0x0800, IPT_COIN2,          IP_KEY_DEFAULT) \
-	COREPORT_BIT(     0x1000, "Ball Tilt",        KEYCODE_DEL)  \
-    COREPORT_BIT(     0x2000, "Slam Tilt",        KEYCODE_HOME)  \
-	BYVP_DIPS
-
-#define BYVP_granny_PORT \
-  PORT_START /* 0 */ \
-    /* These are put in switch column 0 */ \
-    COREPORT_BIT(     0x0001, "Self Test",        KEYCODE_7) \
-    COREPORT_BIT(     0x0002, "CPU Diagnostic",   KEYCODE_9) \
-    COREPORT_BIT(     0x0004, "Sound Diagnostic", KEYCODE_0) \
-    COREPORT_BIT(     0x0008, "Video Diagnostic", KEYCODE_8) \
-	COREPORT_BIT(	  0x0010, "Paddle Right",	  KEYCODE_RIGHT) \
-    COREPORT_BIT(	  0x0020, "Paddle Left",	  KEYCODE_LEFT) \
-	COREPORT_BIT(	  0x0040, "Fire/Start 2",	  KEYCODE_LSHIFT) \
-	COREPORT_BIT(	  0x0080, "Fire/Start 1",	  KEYCODE_RSHIFT) \
-    /* Switch Column 2 (Switches #5 & #6)*/ \
-    COREPORT_BITDEF(  0x0100, IPT_START2,        KEYCODE_2)  \
-    COREPORT_BITDEF(  0x0200, IPT_START1,        KEYCODE_1)  \
-    /* Switch Column 2 (Switches #1,#2,#7,#8)*/ \
-    COREPORT_BITDEF(  0x0400, IPT_COIN1,          IP_KEY_DEFAULT) \
-    COREPORT_BITDEF(  0x0800, IPT_COIN2,          IP_KEY_DEFAULT) \
-	COREPORT_BIT(     0x1000, "Ball Tilt",        KEYCODE_DEL)  \
-    COREPORT_BIT(     0x2000, "Slam Tilt",        KEYCODE_HOME)  \
-	COREPORT_BIT(     0x4000, "Power",		      KEYCODE_UP)  \
-	BYVP_DIPS
-
 
 /*-- Standard input ports --*/
 #define BYVP_INPUT_PORTS_START(name,balls) \
