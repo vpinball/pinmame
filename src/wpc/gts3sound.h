1cbb450285b8d5c0eb928ed8b6eddfd476abbfe1
Tom Haukap <tom.haukap@t-online.de>
2002-03-28 10:10:32 +0000

    - core_init will be called from the init routine - no check if sound is enabled (will be handle by the sndbrd functions) - the exit function will clear the timers
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@571 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/gts3sound.h
+++ src/wpc/gts3sound.h
@@ -94,8 +94,6 @@ extern struct OKIM6295interface GTS3_okim6295_interface;
 extern struct Samplesinterface	samples_interface;
 
 /*-- Sound interface communications --*/
-extern void GTS3_sinit(int num);
-extern void GTS3_sound_exit(void);
 extern void GTS3_SoundCommand(int data);
 
 //Y-CPU
