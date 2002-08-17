eab1ee0d439e1f1f8de724de346aca7371c29014
haukap <haukap@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2002-08-17 18:53:59 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@832 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/taitos.h
+++ src/wpc/taitos.h
@@ -12,7 +12,7 @@
 
 extern MACHINE_DRIVER_EXTERN(taitos);
 
-#define TAITO_SOUNDROMS(rom1,chk1,rom2,chk2) \
+#define TAITO_SOUNDROMS22(rom1,chk1,rom2,chk2) \
   SOUNDREGION(0x10000, TAITO_MEMREG_SCPU) \
     ROM_LOAD(rom1, 0x1800, 0x0800, chk1) \
     ROM_LOAD(rom2, 0x1000, 0x0800, chk2) \
