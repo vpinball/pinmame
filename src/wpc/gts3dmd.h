55b82b0a99398bd92673bcdab0336ab92ca52805
volkenborn <volkenborn@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2002-09-05 12:11:12 +0000

    Fixed GTS3 sound, DMD.
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@897 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/gts3dmd.h
+++ src/wpc/gts3dmd.h
@@ -1,7 +1,7 @@
 #ifndef INC_GTS3DMD
 #define INC_GTS3DMD
 
-#define GTS3DMD_FRAMES 	30  //# of Frames to capture
+#define GTS3DMD_FRAMES 	60  //# of Frames to capture
 
 // void gts3_dmd128x32_refresh(struct mame_bitmap *bitmap,int full_refresh);
 
