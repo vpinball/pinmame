a6b71892c6d2241ef3864abe94036cf009b74151
wpcmame <no.author@mail.com>
2003-01-16 21:38:00 +0000

    cleaned up
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@1037 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/hnks.h
+++ src/wpc/hnks.h
@@ -1,35 +1,13 @@
-#ifndef INC_HANKINSND
-#define INC_HANKINSND
-
-/* Hankin Sound Hardware Info:
-
-  Sound System
-  ------------
-  M6802
-  WAVEFORM ROM
-
-*/
-
-extern const struct Memory_ReadAddress  hnks_readmem[];
-extern const struct Memory_WriteAddress hnks_writemem[];
-
-#define HNK_SOUND_CPU { \
-  CPU_M6802 | CPU_AUDIO_CPU, 900000,	/* 0.9 MHz */					\
-  hnks_readmem, hnks_writemem, 0, 0, \
-  0,1 \
-}
+#ifndef INC_HNKS
+#define INC_HNKS
 
 extern MACHINE_DRIVER_EXTERN(hnks);
-
-// extern struct CustomSound_interface hnks_custInt;
-// #define HNK_SOUND {SOUND_CUSTOM,&hnks_custInt}, SAMPLESINTERFACE
 
 #define HNK_SOUNDROMS(ic14,chk14,ic3,chk3) \
   SOUNDREGION(0x10000, HNK_MEMREG_SCPU) \
     ROM_LOAD(ic14, 0x1000, 0x0800, chk14) \
-	    ROM_RELOAD(0xf800, 0x0800) \
+      ROM_RELOAD(  0xf800, 0x0800) \
     ROM_LOAD(ic3,  0xf000, 0x0200, chk3)
 
-#endif /* INC_HANKINSND */
-
+#endif /* INC_HNKS */
 
