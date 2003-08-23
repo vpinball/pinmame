adcb129f9447c39551d9fffea7395c679fe16ed1
Steve Ellenoff <sellenoff@comcast.net>
2003-08-23 16:44:01 +0000

    Improved sound, but still some issues
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@1273 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/alvgs.c
+++ src/wpc/alvgs.c
@@ -28,50 +28,34 @@ const struct sndbrdIntf alvgsIntf = {
   //"BSMT", alvgs_init, NULL, NULL, soundlatch_w, soundlatch_w, NULL, NULL, NULL, SNDBRD_NODATASYNC
 };
 
-/* Older 11 Voice Style BSMT Chip */
-//NOTE: Do not put a volume adjustment here, otherwise 128x16 games have audible junk played at the beggining
-static struct BSMT2000interface alvgs_bsmt2000aInt = {
-  1, {24000000}, {11}, {ALVGS_ROMREGION}, {100}, {0000}
-};
 /* Newer 12 Voice Style BSMT Chip */
-static struct BSMT2000interface alvgs_bsmt2000bInt = {
-  1, {24000000}, {12}, {ALVGS_ROMREGION}, {100}, {2000}
-};
-/* Older 11 Voice Style BSMT Chip but needs large volume adjustment */
-static struct BSMT2000interface alvgs_bsmt2000cInt = {
-  1, {24000000}, {11}, {ALVGS_ROMREGION}, {100}, {4000}
+static struct BSMT2000interface alvgs_bsmt2000Int = {
+  1, {24000000}, {12}, {ALVGS_ROMREGION}, {100}, {6000}, 0
 };
 
 WRITE_HANDLER(data_to_main_cpu) { logerror("SOUND DATA TO MAIN %x\n",data); }
 
 WRITE_HANDLER(bsmt_write)
 {
-	static int data_low = 0;
 	static int data_hi = 0;
 	static int addr = 0;
 
-	int hi = !(offset & 0x01);
+	int lo = (offset & 0x01);
+	//printf("offset=%x, data=%x\n",offset,data);
 
-	#if 0
-		data = data^0xff;
-		offset = offset^0xff;
-	#endif
-
-	if(hi) {
+	if(lo)
+		BSMT2000_data_0_w(addr, ((data_hi<<8)|data), 0);
+	else {
 		addr = (offset>>1) & 0x7f;
 		data_hi = data;
-		BSMT2000_data_0_w(addr, ((data_hi<<8)|data_low), 0);
-	}
-	else {
-		data_low = data;
-		//BSMT2000_data_0_w(addr, ((data_hi<<8)|data_low), 0);
 	}
 }
-static READ_HANDLER(bsmtready_r) { return 0x80; }
+//RDSTATE line looks to D6&D7 for a busy from BSMT
+static READ_HANDLER(bsmtready_r) { return 0xc0; }
 
 static MEMORY_READ_START(alvgs_readmem)
   { 0x0100, 0x0100, bsmtready_r },
-  { 0x0800, 0x0800, soundlatch_r },
+  { 0x0800, 0x0800, soundlatch_r},
   { 0x2000, 0x3fff, MRA_RAM },
   { 0x4000, 0xffff, MRA_ROM },
 MEMORY_END
@@ -88,9 +72,9 @@ MACHINE_DRIVER_START(alvgs)
   MDRV_CPU_ADD(M6809, 2000000)
   MDRV_CPU_FLAGS(CPU_AUDIO_CPU)
   MDRV_CPU_MEMORY(alvgs_readmem, alvgs_writemem)
-  MDRV_CPU_PERIODIC_INT(alvgs_firq, 256) /* Fixed FIRQ of 489Hz as measured on real machine*/
+  MDRV_CPU_PERIODIC_INT(alvgs_firq, 2000000/2048) /*Not sure what the freq is.. based on a jumper setting */
   MDRV_INTERLEAVE(50)
-  MDRV_SOUND_ADD_TAG("bsmt", BSMT2000, alvgs_bsmt2000bInt)
+  MDRV_SOUND_ADD_TAG("bsmt", BSMT2000, alvgs_bsmt2000Int)
   MDRV_SOUND_ATTRIBUTES(SOUND_SUPPORTS_STEREO)
 MACHINE_DRIVER_END
 
@@ -106,8 +90,5 @@ static void alvgs_init(struct sndbrdData *brdData) {
 }
 
 static INTERRUPT_GEN(alvgs_firq) {
-  //NOTE: Odd that it will NOT WORK without HOLD_LINE - although we don't clear it anywaywhere!
-  //cpu_set_irq_line(alvgslocals.brdData.cpuNo, M6809_FIRQ_LINE, HOLD_LINE);
-  //cpu_set_irq_line(1, M6809_FIRQ_LINE, HOLD_LINE);
   cpu_set_irq_line(1, M6809_FIRQ_LINE, PULSE_LINE);
 }
--- src/wpc/alvgs.h
+++ src/wpc/alvgs.h
@@ -9,13 +9,21 @@ extern MACHINE_DRIVER_EXTERN(alvgs);
 
 /*-- Sound rom macros --*/
 /*-- 64K Sound CPU Rom, 4 X 256K Voice Roms --*/
+
+/* Load 2Mb Rom(256K) to fit into 1Meg Rom Space  */
+#define ALVGS_ROMLOAD4(start, n, chk) \
+  ROM_LOAD(n,  start,  0x40000, chk) \
+    ROM_RELOAD(start + 0x40000, 0x40000)\
+    ROM_RELOAD(start + 0x80000, 0x40000)\
+    ROM_RELOAD(start + 0xc0000, 0x40000)
+
 #define ALVGS_SOUNDROM(n1,chk1,n2,chk2,n3,chk3,n4,chk4,n5,chk5) \
   SOUNDREGION(0x10000, ALVGS_CPUREGION) \
     ROM_LOAD(n1, 0x0000,  0x10000, chk1) \
-	SOUNDREGION(0x100000, ALVGS_ROMREGION) \
-    ROM_LOAD(n2, 0x00000, 0x40000, chk2) \
-    ROM_LOAD(n3, 0x40000, 0x40000, chk3) \
-    ROM_LOAD(n4, 0x80000, 0x40000, chk4) \
-    ROM_LOAD(n5, 0xc0000, 0x40000, chk5)
+	SOUNDREGION(0x400000, ALVGS_ROMREGION) \
+	ALVGS_ROMLOAD4(0x000000, n2, chk2) \
+	ALVGS_ROMLOAD4(0x100000, n3, chk3) \
+	ALVGS_ROMLOAD4(0x200000, n4, chk4) \
+	ALVGS_ROMLOAD4(0x300000, n5, chk5) 
 
 #endif /* INC_ALVGSOUND */
