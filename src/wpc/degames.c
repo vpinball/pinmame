ddd7ed7d8fda034247bf8d347c7bbb54a06e92ab
volkenborn <volkenborn@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2003-10-13 01:36:14 +0000

    Added Batman 1.03, IJ L3, L5, and L6.
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@1455 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/degames.c
+++ src/wpc/degames.c
@@ -294,15 +294,24 @@ CORE_GAMEDEF(tmnt,104,"Teenage Mutant Ninja Turtles (1.04)",1991,"Data East",de_
 / Batman - CPU Rev 3 /DMD Type 1 128K Rom 16/32K CPU Roms
 /------------------------------------------------------------*/
 INITGAMES11(btmn,GEN_DEDMD16, de_128x16DMD, FLIP1516, SNDBRD_DE2S, SNDBRD_DEDMD16, 0)
-DE_ROMSTART88(btmn_101,"batcpub5.101",CRC(a7f5754e) SHA1(2c24cab4cc5f1e05539d2843a49b4b1a8d507630),
-                    "batcpuc5.101",CRC(1fcb85ca) SHA1(daf1e1297975b9b577c796d50b973885f925508e))
-DE_DMD16ROM2(       "batdsp.106",CRC(4c4120e7) SHA1(ba7d78c933f6709b3db4efcca5e7bb9099074550))
+DE_ROMSTART48(btmn_103,"batcpub5.103",CRC(6f160581) SHA1(0f2d6c396324fbf116309a872cf95d9a05446cea),
+                    "batcpuc5.103",CRC(8588c5a8) SHA1(41b159c9e4ca523b37f0b893e57f166c85e812e9))
+DE_DMD16ROM2(       "batdsp.102",CRC(4c4120e7) SHA1(ba7d78c933f6709b3db4efcca5e7bb9099074550))
 DE2S_SOUNDROM021(   "batman.u7" ,CRC(b2e88bf5) SHA1(28f814ea73f8eefd1bb5499a599e67a6850c92c0),
                     "batman.u17" ,CRC(b84914dd) SHA1(333d88033428705cbd0a40d70d938c0021bb0015),
                     "batman.u21" ,CRC(42dab6ac) SHA1(facf993db2ce240c9e825ca9a21ac65a0fbba188))
 DE_ROMEND
 #define input_ports_btmn input_ports_des11
-CORE_GAMEDEF(btmn,101,"Batman (1.01)",1992,"Data East",de_mDEDMD16S2A,0)
+CORE_GAMEDEF(btmn,103,"Batman (1.03)",1992,"Data East",de_mDEDMD16S2A,0)
+
+DE_ROMSTART88(btmn_101,"batcpub5.101",CRC(a7f5754e) SHA1(2c24cab4cc5f1e05539d2843a49b4b1a8d507630),
+                    "batcpuc5.101",CRC(1fcb85ca) SHA1(daf1e1297975b9b577c796d50b973885f925508e))
+DE_DMD16ROM2(       "batdsp.102",CRC(4c4120e7) SHA1(ba7d78c933f6709b3db4efcca5e7bb9099074550))
+DE2S_SOUNDROM021(   "batman.u7" ,CRC(b2e88bf5) SHA1(28f814ea73f8eefd1bb5499a599e67a6850c92c0),
+                    "batman.u17" ,CRC(b84914dd) SHA1(333d88033428705cbd0a40d70d938c0021bb0015),
+                    "batman.u21" ,CRC(42dab6ac) SHA1(facf993db2ce240c9e825ca9a21ac65a0fbba188))
+DE_ROMEND
+CORE_CLONEDEF(btmn,101,103,"Batman (1.01)",1992,"Data East",de_mDEDMD16S2A,0)
 
 /*-------------------------------------------------------------
 / Star Trek - CPU Rev 3 /DMD Type 1 128K Rom - 64K CPU Rom
