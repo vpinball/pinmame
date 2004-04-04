1a0ba5a30e1f7dae2e66eeb969c879233362d031
volkenborn <volkenborn@yahoo.com>
2004-04-04 15:05:53 +0000

    Lots of corrections and additions.
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@1653 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/cpu/scamp/scamp.h
+++ src/cpu/scamp/scamp.h
@@ -10,6 +10,12 @@ enum {
 /* Serial I/O Line */
 #define SCAMP_SERIAL_PORT 0x100
 
+/* F0, F1, F2 Lines */
+#define SCAMP_FLAG_PORT 0x101
+
+/* H Line (pulsed with halt instruction?) */
+#define SCAMP_H_PORT 0x102
+
 extern int SCAMP_ICount;
 
 extern void SCAMP_init(void);
@@ -21,6 +27,8 @@ extern void SCAMP_set_context(void *src);
 extern unsigned SCAMP_get_reg(int regnum);
 extern void SCAMP_set_reg(int regnum, unsigned val);
 extern void SCAMP_set_irq_line(int irqline, int state);
+extern void SCAMP_set_sense_a(int state);
+extern void SCAMP_set_sense_b(int state);
 extern void SCAMP_set_irq_callback(int (*callback)(int irqline));
 extern const char *SCAMP_info(void *context, int regnum);
 extern unsigned SCAMP_dasm(char *buffer, unsigned pc);
