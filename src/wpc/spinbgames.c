6a994df33f6a36a0e49af606c3d882afe5828201
sellenoff <sellenoff@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2004-08-04 04:24:11 +0000

    Greatly improved sound, and cleaned up driver a lot
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@1710 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/spinbgames.c
+++ src/wpc/spinbgames.c
@@ -1,8 +1,7 @@
 #include "driver.h"
 #include "sim.h"
 #include "spinb.h"
-//#include "spinbs.h"
-//#include "sndbrd.h"
+#include "sndbrd.h"
 
 #define GEN_SPINB GEN_ALVG
 
@@ -27,7 +26,7 @@ static struct core_dispLayout spinb_dispDMD[] = {
 /*-------------------------------------------------------------------
 / Jolly Park
 /-------------------------------------------------------------------*/
-INITGAME(jolypark, DMD, FLIP78, 3/*?*/, 0, 0)
+INITGAME(jolypark, DMD, FLIP78, 3/*?*/, SNDBRD_SPINB, 0)
 SPINB_ROMSTART(jolypark,	"jpcpu0.rom", CRC(061967af) SHA1(45048e1d9f17efa3382460fd474a5aeb4191d617),
 							"jpcpu1.rom", CRC(ea99202f) SHA1(e04825e73fd25f6469b3315f063f598ea1ab44c7))
 SPINB_DMDROM(			    "jpdmd0.rom", CRC(b57565cb) SHA1(3fef66d298893029de78fdb6ecdb562c33d76180),
