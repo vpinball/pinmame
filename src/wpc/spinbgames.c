e0a271d8c4af2b61790346283724bb14e00bef93
volkenborn <volkenborn@yahoo.com>
2004-08-15 02:41:41 +0000

    Added digital volume, corrected flipper buttons (used to adjust volume too)
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@1720 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/spinbgames.c
+++ src/wpc/spinbgames.c
@@ -5,10 +5,6 @@
 
 #define GEN_SPINB GEN_ALVG
 
-#define DMD       spinb_dispDMD
-
-#define FLIP78    FLIP_SWNO(16,24)
-
 /* Dot-Matrix display */
 static struct core_dispLayout spinb_dispDMD[] = {
   {0,0,32,128,CORE_DMD,(void *)SPINBdmd_update}, {0}
@@ -26,7 +22,7 @@ static struct core_dispLayout spinb_dispDMD[] = {
 /*-------------------------------------------------------------------
 / Jolly Park
 /-------------------------------------------------------------------*/
-INITGAME(jolypark, DMD, FLIP78, 3/*?*/, SNDBRD_SPINB, 0)
+INITGAME(jolypark, spinb_dispDMD, FLIP_SWNO(47,57), 4/*?*/, SNDBRD_SPINB, 0)
 SPINB_ROMSTART(jolypark,	"jpcpu0.rom", CRC(061967af) SHA1(45048e1d9f17efa3382460fd474a5aeb4191d617),
 							"jpcpu1.rom", CRC(ea99202f) SHA1(e04825e73fd25f6469b3315f063f598ea1ab44c7))
 SPINB_DMDROM(			    "jpdmd0.rom", CRC(b57565cb) SHA1(3fef66d298893029de78fdb6ecdb562c33d76180),
