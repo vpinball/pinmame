72caac04410f7022a739b03fac1b378c31c68188
Thomas Behrens <tomb1709@gmail.com>
2004-08-19 18:47:08 +0000

    Added support for dip-switches
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@1728 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/s7.h
+++ src/wpc/s7.h
@@ -18,13 +18,64 @@
     COREPORT_BITTOG(  0x0200, "Auto/Manual",      KEYCODE_7) \
     COREPORT_BIT(     0x0400, "CPU Diagnostic",   KEYCODE_9) \
     COREPORT_BIT(     0x0800, "Sound Diagnostic", KEYCODE_0) \
+    COREPORT_BIT(     0x1000, "Master Command Enter", KEYCODE_PGUP) \
   PORT_START /* 1 */ \
-    COREPORT_DIPNAME( 0x0001, 0x0000, "Sound  Dip 1") \
+    COREPORT_DIPNAME( 0x0001, 0x0000, "Sound Dip 1") \
       COREPORT_DIPSET(0x0001, "1" ) \
       COREPORT_DIPSET(0x0000, "0" ) \
     COREPORT_DIPNAME( 0x0002, 0x0002, "Sound Dip 2") \
       COREPORT_DIPSET(0x0002, "1" ) \
-      COREPORT_DIPSET(0x0000, "0" )
+      COREPORT_DIPSET(0x0000, "0" ) \
+    COREPORT_DIPNAME( 0x0100, 0x0000, "F1") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0100, "1" ) \
+    COREPORT_DIPNAME( 0x0200, 0x0000, "F2") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0200, "1" ) \
+    COREPORT_DIPNAME( 0x0400, 0x0000, "F3") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0400, "1" ) \
+    COREPORT_DIPNAME( 0x0800, 0x0000, "F4") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0800, "1" ) \
+    COREPORT_DIPNAME( 0x1000, 0x0000, "F5") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x1000, "1" ) \
+    COREPORT_DIPNAME( 0x2000, 0x0000, "F6") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x2000, "1" ) \
+    COREPORT_DIPNAME( 0x4000, 0x0000, "F7") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x4000, "1" ) \
+    COREPORT_DIPNAME( 0x8000, 0x0000, "F8") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x8000, "1" ) \
+  PORT_START /* 2 */ \
+    COREPORT_DIPNAME( 0x0001, 0x0000, "D1 Clear Audits") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0001, "1" ) \
+    COREPORT_DIPNAME( 0x0002, 0x0000, "D2 Reset Defaults") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0002, "1" ) \
+    COREPORT_DIPNAME( 0x0004, 0x0000, "D3 Auto-Cycle Mode") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0004, "1" ) \
+    COREPORT_DIPNAME( 0x0008, 0x0000, "D4") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0008, "1" ) \
+    COREPORT_DIPNAME( 0x0010, 0x0000, "D5") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0010, "1" ) \
+    COREPORT_DIPNAME( 0x0020, 0x0000, "D6") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0020, "1" ) \
+    COREPORT_DIPNAME( 0x0040, 0x0000, "D7") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0040, "1" ) \
+    COREPORT_DIPNAME( 0x0080, 0x0000, "D8") \
+      COREPORT_DIPSET(0x0000, "0" ) \
+      COREPORT_DIPSET(0x0080, "1" )
+
 
 /*-- Standard input ports --*/
 #define S7_INPUT_PORTS_START(name,balls) \
@@ -42,6 +93,7 @@
 #define S7_SWUPDN        -6
 #define S7_SWCPUDIAG     -5
 #define S7_SWSOUNDDIAG   -4
+#define S7_ENTER         -3
 
 /* GameOn Solenoid */
 #define S7_GAMEONSOL 25
