a515d9e54395ebe6c0963768df6d8581db509b72
sellenoff <sellenoff@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2004-08-27 13:53:14 +0000

    First cut at Mr. Game emulation - cpu mostly works, video & sound not done yet
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@1736 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

---
+++ src/wpc/mrgame.c
@@ -0,0 +1,566 @@
+/************************************************************************************************
+  Mr. Game (Italy)
+  ----------------
+  by Steve Ellenoff (08/23/2004)
+  
+  Main CPU Board:
+
+  CPU: Motorola M68000
+  Clock: 6 Mhz
+  Interrupt: Tied to a Fixed System Timer
+  I/O: DMA
+
+  Issues/Todo:
+  #1) Solenoids fire too fast to be detected - try the solenoid test, to see what I mean
+  #2) Sound & Video are 90% not done yet..
+  #3) Video commands to video board are treated as raw ascii and sent to pinmame display for debugging (remove when real video emulation done)
+************************************************************************************************/
+#include "driver.h"
+#include "cpu/m68000/m68000.h"
+#include "cpu/z80/z80.h"
+#include "machine/8255ppi.h"
+#include "vidhrdw/generic.h"
+#include "core.h"
+#include "sim.h"
+
+//use this to comment out video and sound cpu for quicker debugging of main cpu
+#define TEST_MAIN_CPU
+
+#define MRGAME_DISPLAYSMOOTH 2
+#define MRGAME_SOLSMOOTH 1
+#define MRGAME_CPUFREQ 6000000
+
+//Jumper on board shows 200Hz hard wired ( should double check it's actually 200Hz and not a divide by 200)
+#define MRGAME_IRQ_FREQ TIME_IN_HZ(200)
+
+#if 1
+#define LOG(x) printf x
+#else
+#define LOG(x) logerror x
+#endif
+
+//Declarations
+READ_HANDLER(i8255_porta_r);
+READ_HANDLER(i8255_portb_r);
+READ_HANDLER(i8255_portc_r);
+WRITE_HANDLER(i8255_porta_w);
+WRITE_HANDLER(i8255_portb_w);
+WRITE_HANDLER(i8255_portc_w);
+
+static struct {
+  int vblankCount;
+  UINT32 solenoids;
+  UINT16 sols2;
+  int irq_count;
+  int DispNoWait;
+  int SwCol;
+  int LampCol;
+  int diagnosticLED;
+  int acksnd;
+  int ackspk;
+  int row_pin5;
+  int a0a2;
+  int d0d1;
+} locals;
+
+/* -------------------*/
+/* --- Interfaces --- */
+/* -------------------*/
+
+/* I8255 CHIPS */
+static ppi8255_interface ppi8255_intf =
+{
+	1,					/* 1 chip */
+	{i8255_porta_r},	/* Port A read */
+	{i8255_portb_r},	/* Port B read */
+	{i8255_portc_r},	/* Port C read */
+	{i8255_porta_w},	/* Port A write */
+	{i8255_portb_w},	/* Port B write */
+	{i8255_portc_w},	/* Port C write */
+};
+
+#if 0
+/* MSM6585 ADPCM CHIP INTERFACE */
+static struct MSM5205interface SPINB_msm6585Int = {
+	2,										//# of chips
+	640000,									//640Khz Clock Frequency
+	{SPINB_S1_msmIrq, SPINB_S2_msmIrq},		//VCLK Int. Callback
+	{MSM5205_S48_4B, MSM5205_S48_4B},		//Sample Mode
+	{100,75}								//Volume
+};
+/* Sound board */
+const struct sndbrdIntf spinbIntf = {
+   "SPINB", NULL, NULL, NULL, spinb_sndCmd_w, NULL, NULL, NULL, NULL, SNDBRD_NODATASYNC
+};
+#endif
+
+
+//Generate a level 1 IRQ - IP0,IP1,IP2 = 0
+static void mrgame_irq(int data)
+{
+  cpu_set_irq_line(0, MC68000_IRQ_1, PULSE_LINE);
+}
+
+static INTERRUPT_GEN(vblank) {
+  /*-------------------------------
+  /  copy local data to interface
+  /--------------------------------*/
+  locals.vblankCount += 1;
+
+  memcpy(coreGlobals.lampMatrix, coreGlobals.tmpLampMatrix, sizeof(coreGlobals.tmpLampMatrix));
+
+#if 1
+  coreGlobals.solenoids = locals.solenoids;
+  if ((locals.vblankCount % MRGAME_SOLSMOOTH) == 0) {
+	locals.solenoids = coreGlobals.pulsedSolState;
+  }
+#else
+  coreGlobals.solenoids = coreGlobals.pulsedSolState;
+#endif
+
+  /*-- display --*/
+  if ((locals.vblankCount % MRGAME_DISPLAYSMOOTH) == 0) {
+	/*update leds*/
+	coreGlobals.diagnosticLed = locals.diagnosticLED;
+  }
+
+  core_updateSw(1);
+}
+
+static SWITCH_UPDATE(mrgame) {
+  if (inports) {
+	  coreGlobals.swMatrix[1] = (coreGlobals.swMatrix[1] & 0x80) | (inports[CORE_COREINPORT] & 0x007f);		//Column 1 Switches
+	  coreGlobals.swMatrix[2] = (coreGlobals.swMatrix[2] & 0xf9) | (inports[CORE_COREINPORT] & 0x0180)>>6;  //Column 2 Switches
+  }
+}
+
+static MACHINE_INIT(mrgame) {
+  memset(&locals, 0, sizeof(locals));
+
+  /* init PPI */
+  ppi8255_init(&ppi8255_intf);
+
+  //setup IRQ timer
+  timer_pulse(MRGAME_IRQ_FREQ,0,mrgame_irq);
+}
+
+
+//Reads current switch column (really row) - Inverted
+static READ16_HANDLER(col_r) { 
+	UINT8 switches = coreGlobals.swMatrix[locals.SwCol+1];	//+1 so we begin by reading column 1 of input matrix instead of 0 which is used for special switches in many drivers
+	return switches^0xff;
+}
+
+/*
+Return Dip Switches & Sound Ack (Inverted)
+Bits 0-3 Dips (Inverted)
+Bits 4   Ack Sound
+Bits 5   Ack Spk
+Bits 6-7 (Always 0?)
+*/
+static READ16_HANDLER(rsw_ack_r) { 
+    int data = core_getDip(0) & 0x0f;
+	data |= (locals.acksnd << 4);
+	data |= (locals.ackspk << 5);
+	return data^0x3f;	//Bit 6&7 always 0!
+}
+
+/*Solenoids - Need to verify correct solenoid # here!*/
+static WRITE_HANDLER(solenoid_w)
+{
+	int sol = locals.a0a2;				//Controls which individual solenoid we're updating
+	int msk = (~(1<<sol)) & 0xff;		//Mask it off..
+	data &= msk;
+	data |= (locals.d0d1&1)<<sol;
+
+	switch(offset){
+		case 0:
+			coreGlobals.pulsedSolState = (coreGlobals.pulsedSolState & 0xFFFFFF00) | data;
+			break;
+		case 1:
+			coreGlobals.pulsedSolState = (coreGlobals.pulsedSolState & 0xFFFF00FF) | (data<<8);
+            break;
+		case 2:
+			coreGlobals.pulsedSolState = (coreGlobals.pulsedSolState & 0xFF00FFFF) | (data<<16);
+            break;
+		case 3:
+			coreGlobals.pulsedSolState = (coreGlobals.pulsedSolState & 0x00FFFFFF) | (data<<24);
+			break;
+		default:
+			LOG(("Solenoid_W Logic Error\n"));
+	}
+}
+
+static WRITE16_HANDLER(sound_w) { 
+	//LOG(("%08x: sound_w = %04x\n",activecpu_get_pc(),data)); 
+}
+
+//8 bit data to this latch comes from D8-D15 (ie, upper bits only)
+//TEMP HACK TO DISPLAY SOME TEXT ON SCREEN
+static xpos = 0x20;
+static ypos = 0;
+static WRITE16_HANDLER(video_w) { 
+	char tmp[2];
+	int mdata = (data>>8)^0xff;
+	//LOG(("%08x: video_w = %04x (%c)\n",activecpu_get_pc(),mdata,mdata)); 
+#if 0
+	if(mdata == 0xef) printf("\n");
+	else	printf("%c",mdata);
+#else
+	if(mdata == 0xef) {
+		xpos = 0x20;
+		ypos = (ypos + 10);
+		if(ypos > 350) {
+			fillbitmap(Machine->scrbitmap, get_black_pen(), NULL);
+			schedule_full_refresh();
+			ypos = 0;
+		}
+	}
+	else {
+	xpos = (xpos + 8);
+	if(xpos > 250) xpos = 0x20;
+	sprintf(tmp,"%c",mdata);
+	core_textOut(tmp, 1,xpos , ypos, 5);
+	}
+#endif
+}
+
+/*
+Bits 0-2 = D0-D2 selecting 0-7 output of selected bank (see below)
+Bits 3-4 = Selects 1 of the 3 banks (s0-7, s9-19, s20-24)
+Bits 5-6 = ??
+Bits 7   = Controls what value is sent to S0-S7 of CN14 & CN14A
+
+IC01 - Data Bits 3 = 0, 4 = 0 -> S0-S7 of CN14 & CN14A - Bit 7 is sent to S0-S7 via D0-D2
+IC36 - Data Bits 3 = 1, 4 = 0 -> S9,10,11,15,16,17,18,19 of CN12 (D0-D2 generate 0-7 bits)
+IC37 - Data Bits 3 = 0, 4 = 1 -> S20,21,22,23,24 of CN12         (D0-D2 generate 0-7 bits)
+
+S9,S10,S11 = Solenoid bank 1,2,3 X 8 = 24 Sols
+S15-S24 = Lamp Col 1-10 = 80 Lamps
+*/
+static last=0;
+static WRITE16_HANDLER(ic35b_w) { 
+	int output = data & 0x07;
+	int bank = (data & 0x18)>>3;
+	int sol = locals.a0a2;
+	int lmpmsk = (~(1<<sol)) & 0xff;
+
+	switch(bank) {
+		//IC01 - Data Bits 3 = 0, 4 = 0 -> S0-S7 of CN14 & CN14A - Bit 7 is sent to S0-S7 via D0-D2
+		case 0:
+		break;
+		//IC36 - Data Bits 3 = 1, 4 = 0 -> S9,10,11,15,16,17,18,19 of CN12 (D0-D2 generate 0-7 bits)
+		case 1:
+			switch(output) {
+				//S9 - Solenoids 1-8
+				case 0:
+					solenoid_w(0,0);
+					break;
+				//S10 - Solenoids 9-16
+				case 1:
+					solenoid_w(1,0);
+					break;
+				//S11 - Solenoids 17-24
+				case 2:
+					solenoid_w(2,0);
+					break;
+				//S15 - S19 (LAMP COLUMN 1,2,3,4,5)
+				default:
+					locals.LampCol = output-3;
+					coreGlobals.tmpLampMatrix[locals.LampCol] &= lmpmsk;
+					coreGlobals.tmpLampMatrix[locals.LampCol] |= (locals.d0d1&1)<<sol;
+			}
+		break;
+
+		//IC37 - Data Bits 3 = 0, 4 = 1 -> S20,21,22,23,24 of CN12 (D0-D2 generate 0-7 bits)
+		//LAMP COLUMN 6,7,8,9,10
+		case 2:
+			//bits 5,6,7 not connected
+			if(output < 5) {
+				locals.LampCol = 5+output;
+				coreGlobals.tmpLampMatrix[locals.LampCol] &= lmpmsk;
+				coreGlobals.tmpLampMatrix[locals.LampCol] |= (locals.d0d1&1)<<sol;
+			}
+		break;
+	}
+}
+
+/*
+Bits 0 - Bit 1 = D0-D1 to CN12 (i/o) AND CN14 (video)
+Bits 2 - Bit 7 = D2-D7 to CN14 (video board)
+*/
+static WRITE16_HANDLER(data_w) { 
+	locals.d0d1 = data & 0x03;
+#if 0
+	if(locals.d0d1 > 0)
+		LOG(("* * * %08x: data_w = %04x, aoa2 = %x\n",activecpu_get_pc(),data,locals.a0a2)); 
+//	else
+//		LOG(("%08x: data_w = %04x\n",activecpu_get_pc(),data)); 
+#endif
+}
+
+/*
+Bits 0-2 = A0-A2 of CN12 & CN14
+Bits 3-7 = NC
+*/
+static WRITE16_HANDLER(extadd_w) { 
+	locals.a0a2 = data & 0x07;
+#if 0
+	if(locals.d0d1)
+		LOG(("%08x: extadd_w = %04x\n",activecpu_get_pc(),data)); 
+#endif
+}
+
+/*
+Bit 0 - Bit 2 => Rows 0->7
+Bit 3 = RFSH Line
+Bit 4 = LED
+Bit 5 = Pin 5 - CN10 & Pin 18 - CN11 (??)
+Bit 6 = NA
+Bit 7 = /RUNEN Line
+*/
+static WRITE16_HANDLER(row_w) { 
+	locals.SwCol = core_BitColToNum(data & 0x7);
+	locals.diagnosticLED = GET_BIT4;
+	locals.row_pin5 = GET_BIT5;
+//	LOG(("%08x: row_w = %04x\n",activecpu_get_pc(),data)); 
+}
+
+//NVRAM
+static UINT16 *NVRAM;
+static NVRAM_HANDLER(mrgame_nvram) {
+  //core_nvram(file, read_or_write, NVRAM, 0x2000, 0x00);
+}
+
+/***************************************************************************/
+/************************** VIDEO HANDLING *********************************/
+/***************************************************************************/
+READ_HANDLER(i8255_porta_r) { LOG(("i8255_porta_r\n")); return 0; }
+READ_HANDLER(i8255_portb_r) { LOG(("i8255_portb_r\n")); return 0; }
+READ_HANDLER(i8255_portc_r) { LOG(("i8255_portc_r\n")); return 0; }
+WRITE_HANDLER(i8255_porta_w) { LOG(("i8255_porta_w=%x\n",data)); }
+WRITE_HANDLER(i8255_portb_w) { LOG(("i8255_portb_w=%x\n",data)); }
+WRITE_HANDLER(i8255_portc_w) { LOG(("i8255_portc_w=%x\n",data)); }
+
+static READ_HANDLER(soundg1_1_port_r) {
+	return 0;
+}
+static WRITE_HANDLER(soundg1_1_port_w) {
+}
+static READ_HANDLER(soundg1_2_port_r) {
+	return 0;
+}
+static WRITE_HANDLER(soundg1_2_port_w) {
+}
+static READ_HANDLER(videog1_port_r) {
+	return 0;
+}
+static WRITE_HANDLER(videog1_port_w) {
+}
+static READ_HANDLER(videog2_port_r) {
+	return 0;
+}
+static WRITE_HANDLER(videog2_port_w) {
+}
+
+static VIDEO_START(mrgame) {
+  tmpbitmap = auto_bitmap_alloc(Machine->drv->screen_width,Machine->drv->screen_height);
+  return (tmpbitmap == 0);
+}
+
+static WRITE_HANDLER(vram_w) {
+}
+
+PINMAME_VIDEO_UPDATE(mrgame_update) {
+//  copybitmap(bitmap,tmpbitmap,0,0,0,0,&GTS80locals.vidClip,TRANSPARENCY_NONE,0);
+  return 0;
+}
+
+/***********************/
+/* Main CPU Memory Map */
+/***********************/
+static MEMORY_READ16_START(readmem)
+  { 0x000000, 0x01ffff, MRA16_ROM },
+  { 0x020000, 0x02ffff, MRA16_RAM },
+  { 0x030000, 0x030001, rsw_ack_r },
+  { 0x03000c, 0x03000d, col_r },
+MEMORY_END
+static MEMORY_WRITE16_START(writemem)
+  { 0x000000, 0x01ffff, MWA16_ROM },
+  { 0x020000, 0x02ffff, MWA16_RAM },
+  { 0x030002, 0x030003, sound_w },
+  { 0x030004, 0x030005, video_w },
+  { 0x030006, 0x030007, ic35b_w },
+  { 0x030008, 0x030009, data_w },
+  { 0x03000a, 0x03000b, row_w },
+  { 0x03000e, 0x03000f, extadd_w },
+MEMORY_END
+/*******************************/
+/* Video CPU Gen #1 Memory Map */
+/*******************************/
+static MEMORY_READ_START(videog1_readmem)
+  { 0x0000, 0x3fff, MRA_ROM },
+  { 0x4000, 0x7fff, MRA_RAM },
+  { 0x8100, 0x8103, ppi8255_0_r},
+  //{ 0xa800, 0xa805, misc_r},
+MEMORY_END
+static MEMORY_WRITE_START(videog1_writemem)
+  { 0x0000, 0x3fff, MWA_ROM },
+  { 0x4000, 0x7fff, MWA_RAM },
+  { 0x8100, 0x8103, ppi8255_0_w},
+  //{ 0xa800, 0xa805, misc_w},
+MEMORY_END
+static PORT_READ_START(videog1_readport)
+  { 0x00, 0xff, videog1_port_r },
+MEMORY_END
+static PORT_WRITE_START(videog1_writeport)
+  { 0x00, 0xff, videog1_port_w },
+MEMORY_END
+
+/*******************************/
+/* Video CPU Gen #2 Memory Map */
+/*******************************/
+static MEMORY_READ_START(videog2_readmem)
+  { 0x0000, 0x7fff, MRA_ROM },
+  { 0x8000, 0xbfff, MRA_RAM },
+  { 0xc000, 0xc003, ppi8255_0_r},
+  //{ 0xa800, 0xa805, misc_r},
+MEMORY_END
+static MEMORY_WRITE_START(videog2_writemem)
+  { 0x0000, 0x7fff, MWA_ROM },
+  { 0x8000, 0xbfff, MWA_RAM },
+  { 0xc000, 0xc003, ppi8255_0_w},
+  //{ 0xa800, 0xa805, misc_w},
+MEMORY_END
+static PORT_READ_START(videog2_readport)
+  { 0x00, 0xff, videog2_port_r },
+MEMORY_END
+static PORT_WRITE_START(videog2_writeport)
+  { 0x00, 0xff, videog2_port_w },
+MEMORY_END
+/**********************************/
+/* Sound CPU #1 Gen #1 Memory Map */
+/**********************************/
+static MEMORY_READ_START(soundg1_1_readmem)
+  { 0x0000, 0x7fff, MRA_ROM },
+  { 0x8000, 0xfaff, MRA_ROM },
+  { 0xfb00, 0xffff, MRA_RAM },
+MEMORY_END
+static MEMORY_WRITE_START(soundg1_1_writemem)
+  { 0x0000, 0x7fff, MWA_ROM },
+  { 0x8000, 0xfaff, MWA_ROM },
+  { 0xfb00, 0xffff, MWA_RAM },
+MEMORY_END
+static PORT_READ_START(soundg1_1_readport)
+  { 0x00, 0xff, soundg1_1_port_r },
+MEMORY_END
+static PORT_WRITE_START(soundg1_1_writeport)
+  { 0x00, 0xff, soundg1_1_port_w },
+MEMORY_END
+/**********************************/
+/* Sound CPU #2 Gen #1 Memory Map */
+/**********************************/
+static MEMORY_READ_START(soundg1_2_readmem)
+  { 0x0000, 0x7fff, MRA_ROM },
+  { 0x8000, 0xfaff, MRA_ROM },
+  { 0xfb00, 0xffff, MRA_RAM },
+MEMORY_END
+static MEMORY_WRITE_START(soundg1_2_writemem)
+  { 0x0000, 0x7fff, MWA_ROM },
+  { 0x8000, 0xfaff, MWA_ROM },
+  { 0xfb00, 0xffff, MWA_RAM },
+MEMORY_END
+static PORT_READ_START(soundg1_2_readport)
+  { 0x00, 0xff, soundg1_2_port_r },
+MEMORY_END
+static PORT_WRITE_START(soundg1_2_writeport)
+  { 0x00, 0xff, soundg1_2_port_w },
+MEMORY_END
+
+/* Manual starts with a switch # of 0 */
+static int mrgame_sw2m(int no) { return no+7+1; }
+static int mrgame_m2sw(int col, int row) { return col*8+row-7-1; }
+
+/* MAIN CPU DRIVER */
+
+
+/* VIDEO GENERATION 1 DRIVER */
+MACHINE_DRIVER_START(mrgame_vid1)
+  MDRV_CPU_ADD(Z80, 3000000)	/*3 Mhz?*/
+  MDRV_CPU_MEMORY(videog1_readmem, videog1_writemem)
+  MDRV_CPU_PORTS(videog1_readport, videog1_writeport)
+MACHINE_DRIVER_END
+
+/* VIDEO GENERATION 2 DRIVER */
+MACHINE_DRIVER_START(mrgame_vid2)
+  MDRV_CPU_ADD(Z80, 3000000)	/*3 Mhz?*/
+  MDRV_CPU_MEMORY(videog2_readmem, videog2_writemem)
+  MDRV_CPU_PORTS(videog2_readport, videog2_writeport)
+MACHINE_DRIVER_END
+
+/* SOUND GENERATION 1 DRIVER */
+MACHINE_DRIVER_START(mrgame_snd1)
+  MDRV_CPU_ADD(Z80, 4000000)	/*4 Mhz*/
+  MDRV_CPU_FLAGS(CPU_AUDIO_CPU)
+  MDRV_CPU_MEMORY(soundg1_1_readmem, soundg1_1_writemem)
+  MDRV_CPU_PORTS(soundg1_1_readport, soundg1_1_writeport)
+  MDRV_CPU_ADD(Z80, 4000000)	/*4 Mhz*/
+  MDRV_CPU_FLAGS(CPU_AUDIO_CPU)
+  MDRV_CPU_MEMORY(soundg1_2_readmem, soundg1_2_writemem)
+  MDRV_CPU_PORTS(soundg1_2_readport, soundg1_2_writeport)
+MACHINE_DRIVER_END
+
+/* Gen 1 - Vid & Sound */
+MACHINE_DRIVER_START(mrgame_vidsnd_g1)
+  MDRV_IMPORT_FROM(mrgame_vid1)
+  MDRV_IMPORT_FROM(mrgame_snd1)
+  MDRV_INTERLEAVE(50)
+MACHINE_DRIVER_END
+
+/* Gen 2 - Vid & Sound */
+MACHINE_DRIVER_START(mrgame_vidsnd_g2)
+  MDRV_IMPORT_FROM(mrgame_vid2)
+  MDRV_IMPORT_FROM(mrgame_snd1)
+  MDRV_INTERLEAVE(50)
+MACHINE_DRIVER_END
+
+/* Main CPU - Common among all */
+MACHINE_DRIVER_START(mrgame_cpu)
+  MDRV_IMPORT_FROM(PinMAME)
+  MDRV_CORE_INIT_RESET_STOP(mrgame, NULL, NULL)
+  MDRV_CPU_ADD_TAG("mcpu", M68000, MRGAME_CPUFREQ)
+  MDRV_CPU_MEMORY(readmem, writemem)
+  MDRV_CPU_VBLANK_INT(vblank, 1)
+  MDRV_SWITCH_UPDATE(mrgame)
+  MDRV_SWITCH_CONV(mrgame_sw2m,mrgame_m2sw)
+  MDRV_DIAGNOSTIC_LEDH(1)
+//  MDRV_NVRAM_HANDLER(mrgame_nvram)
+MACHINE_DRIVER_END
+
+/* Video Board - Common among all */
+MACHINE_DRIVER_START(mrgame_video_common)
+  MDRV_SCREEN_SIZE(640, 400)
+  MDRV_VISIBLE_AREA(0, 255, 0, 399)
+  MDRV_GFXDECODE(0)
+  MDRV_VIDEO_ATTRIBUTES(VIDEO_TYPE_RASTER | VIDEO_MODIFIES_PALETTE)
+  MDRV_VIDEO_START(mrgame)
+MACHINE_DRIVER_END
+
+
+//Generation 1 
+MACHINE_DRIVER_START(mrgame1)
+	MDRV_IMPORT_FROM(mrgame_cpu)
+#ifndef TEST_MAIN_CPU
+	MDRV_IMPORT_FROM(mrgame_vidsnd_g1)
+#endif
+	MDRV_IMPORT_FROM(mrgame_video_common)
+MACHINE_DRIVER_END
+
+//Generation 2
+MACHINE_DRIVER_START(mrgame2)
+  MDRV_IMPORT_FROM(mrgame_cpu)
+#ifndef TEST_MAIN_CPU
+  MDRV_IMPORT_FROM(mrgame_vidsnd_g2)
+#endif
+  MDRV_IMPORT_FROM(mrgame_video_common)
+MACHINE_DRIVER_END
---
+++ src/wpc/mrgamegames.c
@@ -0,0 +1,112 @@
+#include "driver.h"
+#include "sim.h"
+#include "mrgame.h"
+#include "sndbrd.h"
+
+#define GEN_MRGAME GEN_ALVG
+
+
+/* 10' Color Video Monitor */
+#if 0
+core_tLCDLayout mrgame_disp[] = {
+  {70,0,240,256,CORE_VIDEO,(void *)mrgame_update},{0}
+};
+#else
+core_tLCDLayout mrgame_disp[] = {
+  {0},{0}
+};
+#endif
+
+#define INITGAME(name, disptype, flippers, balls, sb, db) \
+	MRGAME_INPUT_PORTS_START(name, balls) MRGAME_INPUT_PORTS_END \
+	static core_tGameData name##GameData = {GEN_MRGAME,disptype,{flippers,0,3,0,sb,db}}; \
+	static void init_##name(void) { \
+		core_gameData = &name##GameData; \
+	}
+
+/* GAMES APPEAR IN PRODUCTION ORDER (MORE OR LESS) */
+
+/*-------------------------------------------------------------------
+/ Dakar (1988?)
+/-------------------------------------------------------------------*/
+INITGAME(dakar, mrgame_disp, 0, 4/*?*/, 0, 0)
+MRGAME_ROMSTART(dakar,	"cpu_ic13.rom", CRC(83183929) SHA1(977ac10a1e78c759eb0550794f2639fe0e2d1507),
+						"cpu_ic14.rom", CRC(2010d28d) SHA1(d262dabd9298566df43df298cf71c974bee1434a))
+MRGAME_VIDEOROM1(		"vid_ic14.rom", CRC(88a9ca81) SHA1(9660d416b2b8f1937cda7bca51bd287641c7730c),
+						"vid_ic55.rom", CRC(3c68b448) SHA1(f416f00d2de0c71c021fec0e9702ba79b761d5e7),
+						"vid_ic56.rom", CRC(0aac43e9) SHA1(28edfeddb2d54e40425488bad37e3819e4488b0b),
+						"vid_ic66.rom", CRC(c8269b27) SHA1(daa83bfdb1e255b846bbade7f200abeaa9399c06))
+MRGAME_SOUNDROM15(		"snd_ic06.rom", CRC(29e9417e) SHA1(24f465993da7c93d385ec453497f2af4d8abb6f4),
+						"snd_ic07.rom", CRC(71ab15fe) SHA1(245842bb41410ea481539700f79c7ef94f8f8924),
+						"snd_ic22.rom", CRC(e6c1098e) SHA1(06bf8917a27d5e46e4aab93e1f212918418e3a82),
+						"snd_ic35.rom", CRC(7b2394d1) SHA1(f588f5105d75b54dd65bb6448a2d7774fb8477ec),
+						"snd_ic36.rom", CRC(4039ea65) SHA1(390fce94d1e48b395157d8d9afaa485114c58d52))
+MRGAME_ROMEND
+CORE_GAMEDEFNV(dakar,"Dakar",1988,"Mr. Game (Italy)",mMRGAME1,0)
+
+/*-------------------------------------------------------------------
+/ Motor Show (1988?)
+/-------------------------------------------------------------------*/
+INITGAME(motrshow, mrgame_disp, 0, 4/*?*/, 0, 0)
+MRGAME_ROMSTART(motrshow,	"cpu_ic13.rom", CRC(e862ca71) SHA1(b02e5f39f9427d58b70b7999a5ff6075beff05ae),
+							"cpu_ic14.rom", CRC(c898ae25) SHA1(f0e1369284a1e0f394f1d40281fd46252016602e))
+MRGAME_VIDEOROM1(		"vid_ic14.rom", CRC(1d4568e2) SHA1(bfc2bb59708ce3a09f9a1b3460ed8d5269840c97),
+						"vid_ic55.rom", CRC(c27a4ded) SHA1(9c2c9b17f1e71afb74bdfbdcbabb99ef935d32db),
+						"vid_ic56.rom", CRC(1664ec8d) SHA1(e7b15acdac7dfc51b668e908ca95f02a2b569737),
+						"vid_ic66.rom", CRC(5b585252) SHA1(b88e56ebdce2c3a4b170aff4b05018e7c21a79b8))
+MRGAME_SOUNDROM14(		"snd_ic06.rom", CRC(fba5a8f1) SHA1(ddf989abebe05c569c9ecdd498bd8ea409df88ac),
+						"snd_ic22.rom", CRC(e6c1098e) SHA1(06bf8917a27d5e46e4aab93e1f212918418e3a82),
+						"snd_ic35.rom", CRC(9dec153d) SHA1(8a0140257316aa19c0401456839e11b6896609b1),
+						"snd_ic36.rom", CRC(4f42be6e) SHA1(684e988f413cd21c785ad5d60ef5eaddddaf72ab))
+MRGAME_ROMEND
+CORE_GAMEDEFNV(motrshow,"Motor Show",1988,"Mr. Game (Italy)",mMRGAME1,0)
+
+
+/*-------------------------------------------------------------------
+/ Mac Attack (1989?)
+/-------------------------------------------------------------------*/
+INITGAME(macattck, mrgame_disp, 0, 4/*?*/, 0, 0)
+MRGAME_ROMSTART(macattck,	"cpu_ic13.rom", NO_DUMP,
+							"cpu_ic14.rom", NO_DUMP)
+MRGAME_VIDEOROM2(		"vid_ic14.rom", NO_DUMP,
+						"vid_ic15.rom", NO_DUMP,
+						"vid_ic16.rom", NO_DUMP,
+						"vid_ic17.rom", NO_DUMP,
+						"vid_ic18.rom", NO_DUMP,
+						"vid_ic61.rom", NO_DUMP,
+						"vid_ic91.rom", NO_DUMP)
+MRGAME_SOUNDROM14(		"snd_ic06.rom", NO_DUMP,
+						"snd_ic07.rom", NO_DUMP,
+						"snd_ic22.rom", NO_DUMP,
+						"snd_ic35.rom", NO_DUMP)
+
+MRGAME_ROMEND
+CORE_GAMEDEFNV(macattck,"Mac Attack",1989,"Mr. Game (Italy)",mMRGAME1,0)
+
+
+/*-------------------------------------------------------------------
+/ World Cup 90 (1988?)
+/-------------------------------------------------------------------*/
+INITGAME(wcup90, mrgame_disp, 0, 4/*?*/, 0, 0)
+#if 0
+MRGAME_ROMSTART(wcup90,		"cpu_ic13.rom", NO_DUMP,
+							"cpu_ic14.rom", NO_DUMP)
+#else
+//Use dakar roms to test wcup90
+MRGAME_ROMSTART(wcup90,	"cpu_ic13.rom", CRC(83183929) SHA1(977ac10a1e78c759eb0550794f2639fe0e2d1507),
+						"cpu_ic14.rom", CRC(2010d28d) SHA1(d262dabd9298566df43df298cf71c974bee1434a))
+#endif
+MRGAME_VIDEOROM2(		"vid_ic91.rom", CRC(3287ad20) SHA1(d5a453efc7292670073f157dca04897be857b8ed),
+						"vid_ic14.rom", CRC(a101d562) SHA1(ad9ad3968f13169572ec60e22e84acf43382b51e),
+						"vid_ic15.rom", CRC(40791e7a) SHA1(788760b8527df48d1825be88099491b6e94f0a19),
+						"vid_ic16.rom", CRC(a7214157) SHA1(a4660180e8491a37028fec8533cf13daf839a7c4),
+						"vid_ic17.rom", CRC(caf4fb04) SHA1(81784a4dc7c671090cf39cafa7d34a6b34523168),
+						"vid_ic18.rom", CRC(83ad2a10) SHA1(37664e5872e6322ee6bb61ec9385876626598152),
+						"vid_ic61.rom", CRC(538c72ae) SHA1(f704492568257fcc4a4f1189207c6fb6526eb81c))
+MRGAME_SOUNDROM25(		"snd_ic06.rom", CRC(19a66331) SHA1(fbd71bc378b5a04247fd1754529c66b086eb33d8),
+						"snd_ic21.rom", CRC(e6c1098e) SHA1(06bf8917a27d5e46e4aab93e1f212918418e3a82),
+						"snd_ic44.rom", CRC(00946570) SHA1(83e7dd89844679571ab2a803295c8ca8941a4ac7),
+						"snd_ic45.rom", CRC(265aa979) SHA1(9ca10c41526a2d227c21f246273ca14bec7f1bc7),
+						"snd_ic46.rom", CRC(7edb321e) SHA1(b242e94c24e996d2de803d339aa9bf6e93586a4c))
+MRGAME_ROMEND
+CORE_GAMEDEFNV(wcup90,"World Cup 90",1990,"Mr. Game (Italy)",mMRGAME2,0)
