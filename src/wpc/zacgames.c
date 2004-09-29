3c7aa8e8eb0405c7bf751f39dd93e0fa663340b5
destruk <destruk@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2004-09-29 16:29:01 +0000

    changed Star God to use 1125 sound
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@1789 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/zacgames.c
+++ src/wpc/zacgames.c
@@ -132,7 +132,7 @@ CORE_GAMEDEFNV(firemntn,"Fire Mountain",1980,"Zaccaria",mZAC1125,SOUNDFLAG)
 / Star God (05/80)
 /-------------------------------*/
 // This game needs ROM #4 duplicated in slot #5?
-INITGAME1(stargod,GEN_ZAC1,dispZAC1,SNDBRD_ZAC1346,125)
+INITGAME1(stargod,GEN_ZAC1,dispZAC1,SNDBRD_ZAC1125,125)
 ZAC_ROMSTART84444(stargod,	"zac_boot.lgc",CRC(62a3da59) SHA1(db571139aff61757f6c0fda6fa0d1fea8257cb15),
 							"stargod2.lgc",CRC(7a784b03) SHA1(bc3490b69913f52e3e9db5c3de5617ab89efe073),
 							"stargod3.lgc",CRC(95492ac0) SHA1(992ad53efc5b53020e3939dfca5431fd50b6571c),
@@ -141,7 +141,7 @@ ZAC_ROMSTART84444(stargod,	"zac_boot.lgc",CRC(62a3da59) SHA1(db571139aff61757f6c
 //							"stargod5.lgc",CRC(03cd4e24) SHA1(b73d383dc71e44277de9116a702b899a54ce32b9))
 ZAC_SOUNDROM_0(				"stargod.snd", CRC(5079e493) SHA1(51d366cdd09ad00b8b016b0ea1c85ac95ef94d71))
 ZAC_ROMEND
-CORE_GAMEDEFNV(stargod,"Star God",1980,"Zaccaria",mZAC1346,SOUNDFLAG)
+CORE_GAMEDEFNV(stargod,"Star God",1980,"Zaccaria",mZAC1125,SOUNDFLAG)
 
 /*--------------------------------
 / Space Shuttle (09/80)
