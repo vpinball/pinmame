15db02fd226b50a38678ecedf9832b1fdc6fcc74
volkenborn <volkenborn@yahoo.com>
2004-11-23 00:14:19 +0000

    Removed some unneeded bit shift counters.
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@1929 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/machine/4094.c
+++ src/machine/4094.c
@@ -7,8 +7,8 @@
  *   1 Bit Data on the Data Pin is shifted into the 8 bit shift register during 0->1 clock transition
  *   As far as I can tell, this will occur regardless of the strobe & output enable pins..
  *
- *   On the 7th Positve clock strobe, the 7th bit is shifted to the 8th and to QS serial output pin.
- *   On the 7th Negative clock strobe, the value of the new 8th bit is output to the Q'S serial output pin.
+ *   On a positve clock strobe, the 7th bit is shifted to the QS serial output pin.
+ *   On a negative clock strobe, the value of the previous 7th bit is output to the Q'S serial output pin.
  *
  *   When Strobe is 1, each of the 8 bit shift register values is output to the 8 bit store register
  *   When Output Enable is 1, the output pins mirror the 8 bit store register.
@@ -43,13 +43,13 @@ struct HC4094Chip
 	UINT8 shift_register;					// Internal Shift Register Value
 	UINT8 store_register;					// Store Register 8 bit
 	UINT8 clock;							// Status of clock pin
-	UINT8 clkcount;							// Count of the clock strobes
 	UINT8 strobe;							// Status of strobe pin
 	UINT8 oe;								// Status of output enable
 	UINT8 data;								// Status of data pin
-	UINT8 poutput;							// Parrallel Output pins as 1 Byte
+	UINT8 poutput;							// Parallel Output pins as 1 Byte
 	UINT8 qs;								// QS Serial Output pin
 	UINT8 qs1;								// Q'S Serial Output pin
+	UINT8 q_6;								// Old value of the highest bit
 	mem_write_handler parallel_data_out;	/* Parallel Data Out Callback */
 	mem_write_handler qspin_data_out;		/* QS Serial Data Out Callback */
 	mem_write_handler qs1pin_data_out;		/* QS1 Serial Data Out Callback */
@@ -63,9 +63,9 @@ static int numchips = 0;
 static struct HC4094Chip hc4094[MAX_HC4094];		// Each Chip
 
 /**********************************************************************************************
-	Send Data To Parrallel Outputs
+	Send Data To Parallel Outputs
 ***********************************************************************************************/
-static void send_data_to_parrallel_output(int chipnum)
+static void send_data_to_parallel_output(int chipnum)
 {
 	hc4094[chipnum].poutput = hc4094[chipnum].store_register;
 
@@ -80,17 +80,18 @@ static void send_data_to_parrallel_output(int chipnum)
 static void shift_reg_to_storage_reg(int chipnum)
 {
 	hc4094[chipnum].store_register = hc4094[chipnum].shift_register;
-
+/*
 	//If output enabled pin - also send to outputs
 	if(hc4094[chipnum].oe)
-		send_data_to_parrallel_output(chipnum);
+		send_data_to_parallel_output(chipnum);
+*/
 }
 /**********************************************************************************************
 	Send Data To QS Output
 ***********************************************************************************************/
 static void send_data_to_qs_output(int chipnum)
 {
-	hc4094[chipnum].qs = (hc4094[chipnum].shift_register>>7);
+	hc4094[chipnum].qs = hc4094[chipnum].q_6;
 
 	//Send the callback function the output data
 	if(hc4094[chipnum].qspin_data_out)
@@ -102,7 +103,7 @@ static void send_data_to_qs_output(int chipnum)
 ***********************************************************************************************/
 static void send_data_to_qs1_output(int chipnum)
 {
-	hc4094[chipnum].qs1 = (hc4094[chipnum].shift_register>>7);
+	hc4094[chipnum].qs1 = hc4094[chipnum].shift_register >> 7;
 
 	//Send the callback function the output data
 	if(hc4094[chipnum].qs1pin_data_out)
@@ -114,11 +115,13 @@ static void send_data_to_qs1_output(int chipnum)
 ***********************************************************************************************/
 static void shift_data(int chipnum)
 {
-	hc4094[chipnum].shift_register = ((hc4094[chipnum].shift_register<<1) & 0xff) | 
+	hc4094[chipnum].shift_register = ((hc4094[chipnum].shift_register<<1) & 0xff) |
 									 (hc4094[chipnum].data & 1);
+/*
 	//Is strobe enabled?
 	if(hc4094[chipnum].strobe)
 		shift_reg_to_storage_reg(chipnum);
+*/
 }
 
 
@@ -146,7 +149,7 @@ WRITE_HANDLER( HC4094_data_w )
 	}
 
 	//Store to data pin
-	hc4094[chipnum].data = data & 1;
+	hc4094[chipnum].data = data ? 1 : 0;
 }
 /**********************************************************************************************
      HC4094_oe_w -- writes data (1 bit) to the output enable pin
@@ -161,46 +164,7 @@ WRITE_HANDLER( HC4094_oe_w )
 	}
 
 	//Store to output enable pin
-	hc4094[chipnum].oe = data & 1;
-
-	//If Output Enable is high - move storage register to parrallel output pins
-	if(data & 1)
-		send_data_to_parrallel_output(chipnum);
-}
-
-/**********************************************************************************************
-     HC4094_clock_w -- writes data (1 bit) to the Clock pin
-***********************************************************************************************/
-WRITE_HANDLER( HC4094_clock_w )
-{
-	//Safety check
-	int chipnum = offset;
-	int strb = data & 1;
-	if(numchips < chipnum) {
-		LOG(("HC4094_CLOCK_W: Error trying to send data to undefined chip #%d\n",chipnum));
-		return;
-	}
-
-	//Positive Transition? (0->1) - Read Data pin and shift into the shift register
-	if(!hc4094[chipnum].clock && strb) {
-		//Shift the data
-		shift_data(chipnum);
-		//7th Positive transition?
-		if(hc4094[chipnum].clkcount == 7)
-			send_data_to_qs_output(chipnum);
-	}
-	else {
-		//Negative Transition?
-		if(hc4094[chipnum].clock && !strb) {
-			//7th Negative transition?
-			if(hc4094[chipnum].clkcount == 7)
-				send_data_to_qs1_output(chipnum);
-		//Update count (only done after negative transition
-		hc4094[chipnum].clkcount = (hc4094[chipnum].clkcount + 1) % 8;
-		}
-	}
-	//Store to clock
-	hc4094[chipnum].clock = strb;
+	hc4094[chipnum].oe = data ? 1 : 0;
 }
 
 /**********************************************************************************************
@@ -216,11 +180,44 @@ WRITE_HANDLER( HC4094_strobe_w )
 	}
 
 	//Store to strobe pin
-	hc4094[chipnum].strobe = data & 1;
+	hc4094[chipnum].strobe = data ? 1 : 0;
+}
+
+/**********************************************************************************************
+     HC4094_clock_w -- writes data (1 bit) to the Clock pin
+***********************************************************************************************/
+WRITE_HANDLER( HC4094_clock_w )
+{
+	//Safety check
+	int chipnum = offset;
+	int clock = data ? 1 : 0;
+	if(numchips < chipnum) {
+		LOG(("HC4094_CLOCK_W: Error trying to send data to undefined chip #%d\n",chipnum));
+		return;
+	}
+
+	//Positive Transition? (0->1) - Read Data pin and shift into the shift register
+	if(!hc4094[chipnum].clock && clock) {
+		//Save high bit, no matter what
+		hc4094[chipnum].q_6 = hc4094[chipnum].shift_register >> 7;
+		//Shift the data
+		shift_data(chipnum);
+		send_data_to_qs_output(chipnum);
+	}
+	else if(hc4094[chipnum].clock && !clock) {
+		//Negative Transition?
+		send_data_to_qs1_output(chipnum);
+	}
+	//Store to clock
+	hc4094[chipnum].clock = clock;
 
 	//If Strobe is 1 - store shift register to the storage register
-	if(data & 1)
+	if(hc4094[chipnum].strobe)
 		shift_reg_to_storage_reg(chipnum);
+
+	//If Output Enable is high - move storage register to parallel output pins
+	if(hc4094[chipnum].oe)
+		send_data_to_parallel_output(chipnum);
 }
 
 /**********************************************************************************************
