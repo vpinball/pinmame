1c0293cc7ccff2faf4d6a720e15ae69a1a309772
volkenborn <volkenborn@yahoo.com>
2005-03-02 19:45:21 +0000

    Added Sonic "Pole Position" (Peyper hardware)
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@2065 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/peypergames.c
+++ src/wpc/peypergames.c
@@ -6,9 +6,9 @@
 
 #define GEN_PEYPER 0
 
-#define INITGAME(name, disptype, balls) \
+#define INITGAME(name, disptype, balls, lamps) \
 	PEYPER_INPUT_PORTS_START(name, balls) PEYPER_INPUT_PORTS_END \
-	static core_tGameData name##GameData = {GEN_PEYPER,disptype,{FLIP_SW(FLIP_L), 0,6,0, SNDBRD_NONE}}; \
+	static core_tGameData name##GameData = {GEN_PEYPER,disptype,{FLIP_SW(FLIP_L),0,lamps,0,SNDBRD_NONE}}; \
 	static void init_##name(void) { \
 		core_gameData = &name##GameData; \
 	}
@@ -32,7 +32,7 @@ static core_tLCDLayout peyperDisp7[] = {
 /*-------------------------------------------------------------------
 / Odisea Paris-Dakar (1987)
 /-------------------------------------------------------------------*/
-INITGAME(odisea, peyperDisp7, 1)
+INITGAME(odisea, peyperDisp7, 1, 6)
 PEYPER_ROMSTART(odisea,	"odiseaa.bin", CRC(29a40242) SHA1(321e8665df424b75112589fc630a438dc6f2f459),
 						"odiseab.bin", CRC(8bdf7c17) SHA1(7202b4770646fce5b2ba9e3b8ca097a993123b14),
 						"odiseac.bin", CRC(832dee5e) SHA1(9b87ffd768ab2610f2352adcf22c4a7880de47ab))
@@ -40,3 +40,36 @@ PEYPER_ROMEND
 CORE_GAMEDEFNV(odisea,"Odisea Paris-Dakar",1987,"Peyper (Spain)",gl_mPEYPER,0)
 
 // Sir Lancelot  1994 (using advanced hardware)
+
+//---------------
+
+// Sonic games below - using same hardware
+
+static core_tLCDLayout sonicDisp7[] = {
+  {0, 0, 8,6,CORE_SEG7}, {0,12,22,1,CORE_SEG7},
+  {0,22, 0,6,CORE_SEG7}, {0,34,22,1,CORE_SEG7},
+  {3, 0,16,6,CORE_SEG7}, {3,12,22,1,CORE_SEG7},
+  {3,22,24,6,CORE_SEG7}, {3,34,22,1,CORE_SEG7},
+  {1,23,31,1,CORE_SEG7S},{1,25,30,1,CORE_SEG7S},{1,29,15,1,CORE_SEG7S},{1,32,14,1,CORE_SEG7S},
+  {3,17, 6,1,CORE_SEG7},
+  {0}
+};
+
+// Night Fever (1979)
+// Storm (1979)
+// Odin De Luxe (1985)
+// Gamatron (1986)
+// Solar Wars (1986)
+// Star Wars (1987)
+
+/*-------------------------------------------------------------------
+/ Pole Position (1987)
+/-------------------------------------------------------------------*/
+INITGAME(poleposn, sonicDisp7, 1, 4)
+PEYPER_ROMSTART(poleposn, "1.bin", CRC(fdd37f6d) SHA1(863fef32ab9b5f3aca51788b6be9373a01fa0698),
+						  "2.bin", CRC(967cb72b) SHA1(adef17018e2caf65b64bbfef72fe159b9704c409),
+						  "3.bin", CRC(461fe9ca) SHA1(01bf35550e2c55995f167293746f355cfd484af1))
+PEYPER_ROMEND
+CORE_GAMEDEFNV(poleposn,"Pole Position (Sonic)",1987,"Sonic (Spain)",gl_mPEYPER,0)
+
+// Hang-On (1988)
