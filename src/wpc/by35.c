7020b7741592a258a6dc773a88de04857dde4ba8
tomb1709 <tomb1709@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2005-05-26 09:57:09 +0000

    Fixed switch column 6 access
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@2104 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/by35.c
+++ src/wpc/by35.c
@@ -170,7 +170,7 @@ static READ_HANDLER(pia0b_r) {
   if (locals.a0 & 0x80) return core_getDip(2); // DIP#3 17-24
   if ((locals.hw & BY35HW_DIP4) && locals.cb20) return core_getDip(3); // DIP#4 25-32
   {
-    UINT8 sw = core_getSwCol((locals.a0 & 0x1f) | ((locals.b1 & 0x80)>>2));
+    UINT8 sw = core_getSwCol((locals.a0 & 0x1f) | ((locals.b1 & 0x10)<<1));
     return (locals.hw & BY35HW_REVSW) ? core_revbyte(sw) : sw;
   }
 }
