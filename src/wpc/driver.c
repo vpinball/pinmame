08da4f3f2c8ba2e7ac0a7ac384531783a1066490
destruk <destruk@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2005-06-17 18:20:51 +0000

    strike zone 1984
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@2109 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/driver.c
+++ src/wpc/driver.c
@@ -867,8 +867,8 @@ DRIVER(strlt,l1)        //S7-530:   06/84 W Star Light (Came out after System 9 
 DRIVER(pfevr,p3)        //S9-526:   05/84 W Pennant Fever (pitch & bat)
                         //S?-538:   10/84 W Gridiron
 DRIVER(sshtl,l7)        //S9-535:   12/84 W Space Shuttle
-DRIVER(szone,l2)        //S9-916:   ??/8? W Strike Zone (L-2) (Shuffle)
-DRIVER(szone,l5)        //S9-916:   ??/8? W Strike Zone (L-5) (Shuffle)
+DRIVER(szone,l2)        //S9-916:   ??/84 W Strike Zone (L-2) (Shuffle)
+DRIVER(szone,l5)        //S9-916:   ??/84 W Strike Zone (L-5) (Shuffle)
 DRIVER(sorcr,l1)        //S9-532:   03/85 W Sorcerer
 DRIVER(comet,l4)        //S9-540:   06/85 W Comet (L-4)
 DRIVER(comet,l5)        //S9-540:   06/85 W Comet (L-5)
