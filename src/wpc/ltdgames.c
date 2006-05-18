fc39525f599ebc937c4720cbb9849b19be294230
volkenborn <volkenborn@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2006-05-18 04:54:41 +0000

    Some corrections for System4.
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@2300 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/ltdgames.c
+++ src/wpc/ltdgames.c
@@ -17,10 +17,40 @@
 	}
 
 core_tLCDLayout ltd_disp[] = {
-  { 0, 0, 0,10,CORE_SEG7 },
-  { 2, 0,10,10,CORE_SEG7 },
+  { 0, 0, 0,16,CORE_SEG7 },
+  { 2, 0,16,16,CORE_SEG7 },
   {0}
 };
+
+/*
+O Gaucho 1977
+Samba 1977
+Grand Prix 1978
+Al Capone 1981
+Amazon 1981
+Arizona 1981
+Atlantis 1981
+Black Hole 1981
+Carnaval no Rio 1981
+Cowboy Eight Ball 1981
+Disco Dancing 1981
+Force 1981
+Galaxia 1981
+Haunted Hotel 1981
+Hustler 1981
+King Kong 1981
+Kung Fu 1981
+Pec Men 1981
+Martian Queen 1981
+Space Poker 1981
+Time Machine 1981
+Zephy 1981
+Alien Warrior 1982
+Columbia 1982
+Cowboy 2 1982
+Trick Shooter 1982
+Viking King
+*/
 
 /*-------------------------------------------------------------------
 / Atlantis
@@ -63,23 +93,28 @@ CORE_GAMEDEFNV(pecman,"Pec Man",19??,"LTD",gl_mLTD,GAME_NO_SOUND)
 / Zephy
 /-------------------------------------------------------------------*/
 core_tLCDLayout zephy_disp[] = {
-  { 0, 0,14,6,CORE_SEG7 }, { 0,14, 8,6,CORE_SEG7 }, { 3, 0, 2,6,CORE_SEG7 },
-  { 3,14, 1,1,CORE_SEG7 }, { 3,18, 0,1,CORE_SEG7 },
+  { 0, 0,14,6,CORE_SEG7 }, { 3, 0, 8,6,CORE_SEG7 }, { 6, 0, 2,6,CORE_SEG7 },
+  { 9, 2, 1,1,CORE_SEG7 }, { 9, 8, 0,1,CORE_SEG7 },
   {0}
 };
 INITGAME2(zephy, zephy_disp, 1)
 LTD_4_ROMSTART(zephy, "zephy.l2", CRC(8dd11287) SHA1(8133d0c797eb0fdb56d83fc55da91bfc3cddc9e3))
 LTD_ROMEND
-CORE_GAMEDEFNV(zephy,"Zephy",19??,"LTD",gl_mLTD,GAME_NO_SOUND)
+CORE_GAMEDEFNV(zephy,"Zephy",1982,"LTD",gl_mLTD,GAME_NO_SOUND)
 
 /*-------------------------------------------------------------------
-/ Cowboy
+/ Cowboy Eight Ball
 /-------------------------------------------------------------------*/
-INITGAME(cowboy, ltd_disp, 1)
+core_tLCDLayout cowboy_disp[] = {
+  { 0, 0,12,6,CORE_SEG7 }, { 3, 0, 6,6,CORE_SEG7 }, { 6, 0, 0,6,CORE_SEG7 },
+  { 9, 0,18,2,CORE_SEG7 }, { 9, 5,20,2,CORE_SEG7 }, { 9,10,22,2,CORE_SEG7 },
+  {0}
+};
+INITGAME2(cowboy, cowboy_disp, 1)
 LTD_44_ROMSTART(cowboy, "cauboy.l",     CRC(87befe2a) SHA1(93fdf40b10e53d7d95e5dc72923b6be887411fc0),
                         "cauboy.h",     CRC(105e5d7b) SHA1(75edeab8c8ba19f334479133802acbc25f405763))
 LTD_ROMEND
-CORE_GAMEDEFNV(cowboy,"Cowboy",19??,"LTD",gl_mLTD4,GAME_NOT_WORKING|GAME_NO_SOUND)
+CORE_GAMEDEFNV(cowboy,"Cowboy Eight Ball",1981,"LTD",gl_mLTD4,GAME_NOT_WORKING|GAME_NO_SOUND)
 
 /*-------------------------------------------------------------------
 / Columbia
