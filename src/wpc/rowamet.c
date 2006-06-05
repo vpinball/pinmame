4312df9a3ecb38ce9bee416b978213d97c88599a
volkenborn <volkenborn@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2006-06-05 15:29:55 +0000

    Added sound section, Heavy Metal is almost working now!
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@2336 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/rowamet.c
+++ src/wpc/rowamet.c
@@ -3,12 +3,16 @@
   ---------
   by Gerrit Volkenborn
 
-  Main CPU Board:
-
+  Main CPU board:
+  ---------------
   CPU: Z80
-  Clock: 2,5 Mhz
-  Interrupt: 125 Hz
-  Sound: Z80
+  Clock: 2 Mhz
+
+  Sound board:
+  ------------
+  CPU: Z80
+  Clock: 2 Mhz
+  DAC
 ************************************************************************************************/
 #include "driver.h"
 #include "cpu/z80/z80.h"
@@ -16,8 +20,7 @@
 #include "sim.h"
 
 #define ROWAMET_SOLSMOOTH 4
-#define ROWAMET_CPUFREQ 2500000
-#define ROWAMET_IRQFREQ 125
+#define ROWAMET_CPUFREQ 2000000
 
 #if 0
 #define LOG(x) printf x
@@ -29,11 +32,17 @@ static struct {
   int vblankCount;
   core_tSeg segments;
   UINT32 solenoids;
+  UINT8 sndCmd;
 } locals;
 
-static INTERRUPT_GEN(rowamet_irq)
-{
-  cpu_set_irq_line(0, 0, PULSE_LINE);
+static INTERRUPT_GEN(rowamet_irq) {
+  static int irq = 0;
+  irq++;
+  if (irq == 5) {
+    cpu_set_irq_line(0, 0, ASSERT_LINE);
+    cpu_set_irq_line(0, 0, CLEAR_LINE);
+    irq = 0;
+  }
 }
 
 static INTERRUPT_GEN(vblank) {
@@ -47,9 +56,9 @@ static INTERRUPT_GEN(vblank) {
 
   coreGlobals.solenoids = locals.solenoids;
   if ((locals.vblankCount % ROWAMET_SOLSMOOTH) == 0) {
-	locals.solenoids = coreGlobals.pulsedSolState;
+    locals.solenoids = 0;
   }
-  core_updateSw(1);
+  core_updateSw(core_getSol(1));
 }
 
 static SWITCH_UPDATE(rowamet) {
@@ -64,45 +73,47 @@ static MACHINE_INIT(rowamet) {
 
 static READ_HANDLER(sw_r) {
   UINT8 sw = coreGlobals.swMatrix[offset];
-  return offset ? sw ^ 0xff : sw ^ 0xef;
+  return sw ^ 0xff;
 }
 
 static WRITE_HANDLER(lamp_w) {
-  coreGlobals.tmpLampMatrix[offset] = data;
+  int oldData = coreGlobals.tmpLampMatrix[offset / 2];
+  coreGlobals.tmpLampMatrix[offset / 2] = offset % 2 ? (oldData & 0x0f) | ((data & 0x0f) << 4) : (oldData & 0xf0) | (data & 0x0f);
 }
 
 static WRITE_HANDLER(sol_w) {
-  coreGlobals.pulsedSolState = (coreGlobals.pulsedSolState & 0xFF00) | data;
-}
-
-static WRITE_HANDLER(sound_w) {
-  LOG(("sound_w = %04x\n",data));
+  UINT16 d = data;
+  locals.solenoids |= d << offset;
 }
 
 static WRITE_HANDLER(disp_w) {
-  locals.segments[2*offset].w = core_bcd2seg[data & 0x0f];
-  locals.segments[2*offset+1].w = core_bcd2seg[data >> 4];
+  locals.segments[2*offset+1].w = core_bcd2seg[data & 0x0f];
+  locals.segments[2*offset].w = core_bcd2seg[data >> 4];
 }
 
 static WRITE_HANDLER(peri_w) {
-  if (offset <= 0x0d) {
-    disp_w(offset, data);
-  } else if (offset >= 0x10 && offset <= 0x1c) {
+  if (offset <= 0x0f) {
+    disp_w(0x0f - offset, data);
+  } else {
     lamp_w(offset - 0x10, data);
-  } else if (offset == 0x80) { // correct this!
-    sol_w(0, data);
-  } else if (offset >= 0x20 && offset <= 0x31) {
-    // ignore - used for switch debuffering (9 columns including col #0)
-  } else
-    LOG(("peri_w %02x = %02x\n", offset, data));
+    if (offset == 0x12) {
+      if (((locals.sndCmd & 0xf0) | (data >> 4)) != locals.sndCmd) {
+        locals.sndCmd = (locals.sndCmd & 0xf0) | (data >> 4);
+        cpu_set_nmi_line(1, PULSE_LINE);
+      }
+    }
+    else if (offset == 0x13) locals.sndCmd = (locals.sndCmd & 0x0f) | (data & 0xf0);
+    else sol_w(offset - 0x10, data & 0x80 ? 1 : 0);
+    LOG(("peri_w %02x = %x\n", offset, data >> 4));
+  }
 }
 
 static WRITE_HANDLER(ram_w) {
   generic_nvram[offset] = data;
-  if (offset >= 0x80) peri_w(offset-0x80, data);
+  if (offset >= 0x80 && offset < 0xa0) peri_w(offset-0x80, data);
 }
 
-//Memory Map for Main CPU
+//Memory map for main CPU
 static MEMORY_READ_START(cpu_readmem)
   { 0x0000, 0x1fff, MRA_ROM },
   { 0x2800, 0x2808, sw_r },
@@ -113,23 +124,55 @@ static MEMORY_WRITE_START(cpu_writemem)
   { 0x4000, 0x40ff, ram_w, &generic_nvram, &generic_nvram_size },
 MEMORY_END
 
+//Memory map for sound CPU
+static MEMORY_READ_START(snd_readmem)
+  { 0x0000, 0x0fff, MRA_ROM },
+  { 0x1000, 0x17ff, MRA_RAM },
+MEMORY_END
+
+static MEMORY_WRITE_START(snd_writemem)
+  { 0x1000, 0x17ff, MWA_RAM },
+MEMORY_END
+
+static READ_HANDLER(snd_data_r) {
+  return locals.sndCmd;
+}
+
+//Port map for sound CPU
+static PORT_READ_START(snd_readport)
+  { 0x00, 0x00, snd_data_r },
+PORT_END
+
+static PORT_WRITE_START(snd_writeport)
+  { 0x01, 0x01, DAC_0_data_w },
+PORT_END
+
+static struct DACinterface rowamet_dac_intf = { 1, { 25 }};
+
 MACHINE_DRIVER_START(rowamet)
   MDRV_IMPORT_FROM(PinMAME)
   MDRV_CORE_INIT_RESET_STOP(rowamet, NULL, NULL)
+  MDRV_DIAGNOSTIC_LEDH(1)
   MDRV_CPU_ADD_TAG("mcpu", Z80, ROWAMET_CPUFREQ)
-  MDRV_CPU_PERIODIC_INT(rowamet_irq, ROWAMET_IRQFREQ)
   MDRV_CPU_MEMORY(cpu_readmem, cpu_writemem)
   MDRV_CPU_VBLANK_INT(vblank, 1)
+  MDRV_CPU_PERIODIC_INT(rowamet_irq, 1)
   MDRV_SWITCH_UPDATE(rowamet)
   MDRV_NVRAM_HANDLER(generic_0fill)
+
+  MDRV_CPU_ADD_TAG("scpu", Z80, 1500000)
+  MDRV_CPU_MEMORY(snd_readmem, snd_writemem)
+  MDRV_CPU_PORTS(snd_readport, snd_writeport)
+  MDRV_SOUND_ADD(DAC, rowamet_dac_intf)
 MACHINE_DRIVER_END
 
 static core_tLCDLayout disp[] = {
-  {0, 0, 0, 6,CORE_SEG7},
-  {2, 0, 6, 6,CORE_SEG7},
-  {4, 0,12, 6,CORE_SEG7},
-  {6, 0,18, 6,CORE_SEG7},
-  {8, 0,24, 4,CORE_SEG7},
+  {0, 2, 5, 1,CORE_SEG7},
+  {3, 0,26, 6,CORE_SEG7},
+  {5, 0,20, 6,CORE_SEG7},
+  {7, 0,14, 6,CORE_SEG7},
+  {9, 0, 8, 6,CORE_SEG7},
+  {12,2, 7, 1,CORE_SEG7},
   {0}
 };
 static core_tGameData heavymtlGameData = {GEN_ZAC2, disp};
@@ -181,6 +224,8 @@ ROM_START(heavymtl) \
   NORMALREGION(0x10000, REGION_CPU1) \
     ROM_LOAD("hvymtl_c.bin", 0x0000, 0x1000, CRC(9dacb8c8) SHA1(2eaeeb84ff466adcd05830d6a68815a054adb27d)) \
     ROM_LOAD("hvymtl_b.bin", 0x1000, 0x1000, CRC(7aa7228d) SHA1(261ecad7348c64938c8ddf2ebbb8e8307c3a52b9)) \
+  NORMALREGION(0x10000, REGION_CPU2) \
+    ROM_LOAD("hvymtl_s.bin", 0x0000, 0x1000, CRC(b8406c85) SHA1(a18fbc2f088be89ded3ed15a149fb7414b859ce1)) \
 ROM_END
 
-CORE_GAMEDEFNV(heavymtl, "Heavy Metal", 198?, "Rowamet", rowamet, GAME_NO_SOUND)
+CORE_GAMEDEFNV(heavymtl, "Heavy Metal", 198?, "Rowamet", rowamet, 0)
