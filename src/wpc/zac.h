79774fb77e45a5ede0f5db101374890685d08597
volkenborn <volkenborn@yahoo.com>
2008-04-29 20:51:19 +0000

    Raised TMS5220 clock again to compare for chip emulation (sound too low)
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@2687 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/zac.h
+++ src/wpc/zac.h
@@ -117,23 +117,23 @@
     COREPORT_DIPNAME( 0x0008, 0x0000, "S4 prog enable") \
       COREPORT_DIPSET(0x0000, "0" ) \
       COREPORT_DIPSET(0x0008, "1" ) \
-    COREPORT_DIPNAME( 0x00f0, 0x00b0, "Speech clock speed") \
-      COREPORT_DIPSET(0x0000, "530 kHz" ) \
-      COREPORT_DIPSET(0x0010, "540 kHz" ) \
-      COREPORT_DIPSET(0x0020, "550 kHz" ) \
-      COREPORT_DIPSET(0x0030, "560 kHz" ) \
-      COREPORT_DIPSET(0x0040, "570 kHz" ) \
-      COREPORT_DIPSET(0x0050, "580 kHz" ) \
-      COREPORT_DIPSET(0x0060, "590 kHz" ) \
-      COREPORT_DIPSET(0x0070, "600 kHz" ) \
-      COREPORT_DIPSET(0x0080, "610 kHz" ) \
-      COREPORT_DIPSET(0x0090, "620 kHz" ) \
-      COREPORT_DIPSET(0x00a0, "630 kHz" ) \
-      COREPORT_DIPSET(0x00b0, "640 kHz" ) \
-      COREPORT_DIPSET(0x00c0, "650 kHz" ) \
-      COREPORT_DIPSET(0x00d0, "660 kHz" ) \
-      COREPORT_DIPSET(0x00e0, "670 kHz" ) \
-      COREPORT_DIPSET(0x00f0, "680 kHz" )
+    COREPORT_DIPNAME( 0x00f0, 0x0060, "Speech clock speed") \
+      COREPORT_DIPSET(0x0000, "640 kHz" ) \
+      COREPORT_DIPSET(0x0010, "646 kHz" ) \
+      COREPORT_DIPSET(0x0020, "654 kHz" ) \
+      COREPORT_DIPSET(0x0030, "660 kHz" ) \
+      COREPORT_DIPSET(0x0040, "666 kHz" ) \
+      COREPORT_DIPSET(0x0050, "674 kHz" ) \
+      COREPORT_DIPSET(0x0060, "680 kHz" ) \
+      COREPORT_DIPSET(0x0070, "686 kHz" ) \
+      COREPORT_DIPSET(0x0080, "694 kHz" ) \
+      COREPORT_DIPSET(0x0090, "700 kHz" ) \
+      COREPORT_DIPSET(0x00a0, "706 kHz" ) \
+      COREPORT_DIPSET(0x00b0, "714 kHz" ) \
+      COREPORT_DIPSET(0x00c0, "720 kHz" ) \
+      COREPORT_DIPSET(0x00d0, "726 kHz" ) \
+      COREPORT_DIPSET(0x00e0, "734 kHz" ) \
+      COREPORT_DIPSET(0x00f0, "740 kHz" )
 
 /*-- Standard input ports --*/
 #define ZACOLD_INPUT_PORTS_START(name,balls) \
