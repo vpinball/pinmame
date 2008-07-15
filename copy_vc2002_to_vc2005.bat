d10cfe1699bbbfc7a85d43089ab8ee721446563b
Matthias Buecher <maddes.b@arcor.de>
2008-07-15 07:39:54 +0000

    Added setup and compilation information for VC2005 and VC2008
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@2757 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

---
+++ copy_vc2002_to_vc2005.bat
@@ -0,0 +1,10 @@
+copy /V /-Y "InstallVPinMAME_VC2002.vcproj" "InstallVPinMAME_VC2005.vcproj"
+copy /V /-Y "PinMAME_VC2002.vcproj" "PinMAME_VC2005.vcproj"
+copy /V /-Y "PinMAME32_VC2002.vcproj" "PinMAME32_VC2005.vcproj"
+copy /V /-Y "VPinMAME_VC2002.vcproj" "VPinMAME_VC2005.vcproj"
+@echo.
+@echo Now replace all occurrences of VC2002 in them with VC2005
+@echo then convert them with VC2005.
+@echo Add _CRT_SECURE_NO_WARNINGS to the C/C++ preprocessor definitions in all 
+@echo PinMAME/32 builds.
+@pause
