86135246287cfe9faac6d8493c5f669ac813bfe6
volkenborn <volkenborn@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2009-01-19 09:37:21 +0000

    Found sound & game enable.
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@2840 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/vd.c
+++ src/wpc/vd.c
@@ -5,10 +5,10 @@
    Hardware:
    ---------
 		CPU:     Z80 @ 4? MHz
-			INT: IRQ @ 1000? Hz
+			INT: IRQ @ 500 Hz (Uptime counter in RAM seems to run accurate then)
 		IO:      CPU ports
 		DISPLAY: 7-digit 8-segment panels, direct segment access
-		SOUND:	 ?
+		SOUND:	 2 x AY-8910
  ************************************************************************************************/
 
 #include "driver.h"
@@ -17,7 +17,7 @@
 #include "sound/ay8910.h"
 #include "sndbrd.h"
 
-#define JP_VBLANKFREQ   60 /* VBLANK frequency */
+#define JP_IRQFREQ 500 /* IRQ frequency */
 #define JP_CPUFREQ 4000000 /* CPU clock frequency */
 
 /*----------------
@@ -47,7 +47,7 @@ static INTERRUPT_GEN(VD_vblank) {
   /*-- display --*/
   memcpy(coreGlobals.segments, locals.segments, sizeof(locals.segments));
 
-  core_updateSw(TRUE);
+  core_updateSw(core_getSol(17));
 }
 
 static SWITCH_UPDATE(VD) {
@@ -62,8 +62,8 @@ static SWITCH_UPDATE(VD) {
 
 struct AY8910interface VD_ay8910Int = {
   2,         /* 2 chips ? */
-  2500000,   /* 2.5 MHz ? */
-  { 30, 30 } /* Volume */
+  2000000,   /* 2 MHz ? */
+  { MIXER(30,MIXER_PAN_LEFT), MIXER(30,MIXER_PAN_RIGHT) } /* Volume */
 };
 
 static READ_HANDLER(sw_r) {
@@ -76,7 +76,7 @@ static READ_HANDLER(sw0_r) {
 
 static WRITE_HANDLER(col_w) {
   if ((data & 0x0f) < 0x08) locals.col = data & 0x07;
-  else logerror("col_w:%02x\n", data);
+  else locals.solenoids = (locals.solenoids & 0xffff) | ((data & 0xe0) << 11);
 }
 
 static WRITE_HANDLER(lamp_w) {
@@ -90,55 +90,36 @@ static WRITE_HANDLER(disp_w) {
   if (digit) locals.segments[digit-1].w = (locals.segments[digit-1].w & 0x7f) | ((data & 1) << 7);
 }
 
-static WRITE_HANDLER(p60_w) {
-  logerror("p60_w:%02x\n", data);
-}
-
-static WRITE_HANDLER(p62_w) {
-  logerror("p62_w:%02x\n", data);
-}
-
-static WRITE_HANDLER(p80_w) {
-  logerror("p80_w:%02x\n", data);
-}
-
-static WRITE_HANDLER(p82_w) {
-  logerror("p82_w:%02x\n", data);
-}
-
-static READ_HANDLER(pa0_r) {
-  logerror("pa0_r:\n");
-  return 0xff;
-}
-
 static WRITE_HANDLER(sol_w) {
-  if (data) locals.solenoids = (1 << (data-1));
-  else locals.solenoids = 0;
+  if (data) locals.solenoids = (locals.solenoids & 0x10000) | (1 << (data-1));
+  else locals.solenoids = locals.solenoids & 0x10000;
 }
 
 static MEMORY_READ_START(VD_readmem)
   {0x0000,0x5fff, MRA_ROM},
-  {0x6000,0x67ff, MRA_RAM},
+  {0x6000,0x63ff, MRA_RAM},
+  {0x6700,0x67ff, MRA_RAM},
 MEMORY_END
 
 static MEMORY_WRITE_START(VD_writemem)
-  {0x6000,0x67ff, MWA_RAM, &generic_nvram, &generic_nvram_size},
+  {0x6000,0x63ff, MWA_RAM, &generic_nvram, &generic_nvram_size},
+  {0x6700,0x67ff, MWA_RAM},
 MEMORY_END
 
 static PORT_READ_START(VD_readport)
   {0x00,0x05, sw_r},
   {0x61,0x61, sw0_r},
-  {0xa0,0xa0, pa0_r},
+  {0xa0,0xa0, AY8910_read_port_1_r},
 PORT_END
 
 static PORT_WRITE_START(VD_writeport)
   {0x20,0x27, lamp_w},
   {0x28,0x28, sol_w},
   {0x40,0x44, disp_w},
-  {0x60,0x60, p60_w},
-  {0x62,0x62, p62_w},
-  {0x80,0x80, p80_w},
-  {0x82,0x82, p82_w},
+  {0x60,0x60, AY8910_control_port_0_w},
+  {0x62,0x62, AY8910_write_port_0_w},
+  {0x80,0x80, AY8910_control_port_1_w},
+  {0x82,0x82, AY8910_write_port_1_w},
   {0xc0,0xc0, col_w},
 PORT_END
 
@@ -165,11 +146,12 @@ MACHINE_DRIVER_START(VD)
   MDRV_CPU_MEMORY(VD_readmem, VD_writemem)
   MDRV_CPU_PORTS(VD_readport, VD_writeport)
   MDRV_CPU_VBLANK_INT(VD_vblank, 1)
-  MDRV_CPU_PERIODIC_INT(VD_irq, 1000)
+  MDRV_CPU_PERIODIC_INT(VD_irq, JP_IRQFREQ)
   MDRV_CORE_INIT_RESET_STOP(VD,NULL,NULL)
   MDRV_NVRAM_HANDLER(generic_0fill)
   MDRV_SWITCH_UPDATE(VD)
-//  MDRV_SOUND_ADD(AY8910, VD_ay8910Int)
+  MDRV_SOUND_ADD(AY8910, VD_ay8910Int)
+  MDRV_SOUND_ATTRIBUTES(SOUND_SUPPORTS_STEREO)
 MACHINE_DRIVER_END
 
 static const core_tLCDLayout dispVD[] = {
@@ -193,6 +175,6 @@ ROM_START(break) \
     ROM_LOAD("break3.cpu", 0x4000, 0x2000, CRC(3cdfedc2) SHA1(309fd04c81b8facdf705e6297c0f4d507957ae1f))
 ROM_END
 VD_INPUT_PORTS_START(break, 1) INPUT_PORTS_END
-CORE_GAMEDEFNV(break, "Break", 1986, "Videodens", VD, GAME_NO_SOUND)
+CORE_GAMEDEFNV(break, "Break", 1986, "Videodens", VD, 0)
 
 // Papillon (1986)
