348aa0c83cb06d165a0ec223136220a3502d3e10
volkenborn <volkenborn@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2009-01-26 10:11:41 +0000

    Added ifdef for PinMAME
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@2846 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/png.c
+++ src/png.c
@@ -876,7 +876,11 @@ int png_write_bitmap(mame_file *fp, struct mame_bitmap *bitmap)
 #ifdef MESS
 	sprintf (text, "MESS %s", build_version);
 #else
+#ifdef PINMAME
+	sprintf (text, "PinMAME %s", build_version);
+#else
 	sprintf (text, "MAME %s", build_version);
+#endif
 #endif
 	png_add_text("Software", text);
 	sprintf (text, "%s %s", Machine->gamedrv->manufacturer, Machine->gamedrv->description);
