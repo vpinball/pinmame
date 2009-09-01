4c0465d90b61fa96022a33883138112af4caef97
David Rodriguez <netsplits@yahoo.es>
2009-09-01 22:20:00 +0000

    Leds rearranged and added code for switch matrix. Not working yet.
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@2883 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/wico.c
+++ src/wpc/wico.c
@@ -16,6 +16,7 @@
 #include "sim.h"
 #include "gen.h"
 #include "cpu/m6809/m6809.h"
+#include "sound/sn76496.h"
 
 /*----------------
 /  Local variables
@@ -26,7 +27,9 @@ static struct {
   UINT8  sndCmd;
   UINT8  swCol;
   UINT8  lampCol;
-  UINT8	 zcEnable;
+  UINT8	 zcIRQEnable;
+  UINT8	 gtIRQEnable;
+  UINT8	 diagnosticLed;
 } locals;
 
 static int wico_data2seg[16] = {
@@ -40,19 +43,23 @@ static void WICO_firq_0(int data) {
 }
 
 static INTERRUPT_GEN(WICO_irq_0) {
-  static int firqtimer = 0;
-  if (locals.zcEnable != 0x00) cpu_set_irq_line(0, M6809_IRQ_LINE, PULSE_LINE);
-  
-  // firq kicks every 4 zero crossings
-  firqtimer++;
-  if (firqtimer > 4) {
-    WICO_firq_0(0);
-	firqtimer = 0;
-  }
+  if (locals.zcIRQEnable) cpu_set_irq_line(0, M6809_IRQ_LINE, PULSE_LINE); 
 }
 
 static INTERRUPT_GEN(WICO_irq_1) {
-  cpu_set_irq_line(1, M6809_IRQ_LINE, PULSE_LINE);
+  static int firqtimer = 0;
+
+  if (!locals.gtIRQEnable)
+  {
+	  cpu_set_irq_line(1, M6809_IRQ_LINE, PULSE_LINE);
+	  
+	  // firq of CPU 0 kicks in every 4 interrupts of the general timer
+	  firqtimer++;
+	  if (firqtimer > 4) {
+		  WICO_firq_0(0);
+		  firqtimer = 0;
+	  }
+  }
 }
 
 static void WICO_firq_1(int data) {
@@ -78,51 +85,81 @@ static SWITCH_UPDATE(WICO) {
   if (inports) {
     CORE_SETKEYSW(inports[CORE_COREINPORT], 0xff, 0);
   }
-  cpu_set_nmi_line(0, coreGlobals.swMatrix[0] & 1 ? ASSERT_LINE : CLEAR_LINE);
-  if (coreGlobals.swMatrix[0] & 2) WICO_firq_0(0);
-  cpu_set_nmi_line(1, coreGlobals.swMatrix[0] & 4 ? ASSERT_LINE : CLEAR_LINE);
-  if (coreGlobals.swMatrix[0] & 8) WICO_firq_1(0);
+//  cpu_set_nmi_line(0, coreGlobals.swMatrix[0] & 1 ? ASSERT_LINE : CLEAR_LINE);
+//  if (coreGlobals.swMatrix[0] & 2) WICO_firq_0(0);
+//  cpu_set_nmi_line(1, coreGlobals.swMatrix[0] & 4 ? ASSERT_LINE : CLEAR_LINE);
+//  if (coreGlobals.swMatrix[0] & 8) WICO_firq_1(0);
 }
 
+static UINT8 *ram_01;
 static WRITE_HANDLER(io0_w) {
   switch (offset) {
-/*	case 0xe0: // not sure yet, but probably a write enable to the shared ram
-		break;*/
-	case 0xe2:
-		coreGlobals.segments[data & 0x0f].w = wico_data2seg[data >> 4];
+	case 0xe0: // matrix scan begin???
+		break;
+
+	case 0xe2: // segment display
+		coreGlobals.segments[6 - (data & 0x0f)].w = wico_data2seg[data >> 4];
+		if ((data >> 4) == 0x08) logerror("CPU0 SELF TEST DONE -------------------\n");
+		break;
+
+	case 0xe6: // watchdog keep alive??? 
+		if (data == 0x00) coreGlobals.diagnosticLed = locals.diagnosticLed & 0x01;
+		else logerror("io0_w: offset %x, data %02x not handled\n", offset, data);
+		locals.diagnosticLed = ~locals.diagnosticLed; 
 		break;
 
 	case 0xe7: // zero crossing interrupt
-		locals.zcEnable = data; // enable/disable zero crossing interrupt
+		locals.zcIRQEnable = data; // enable/disable zero crossing interrupt
+		logerror("io0_w: ZC INT ENABLE offset %x, data %02x\n", offset, data);
 		break;
-
-/*    case 0xe6:
-      coreGlobals.tmpLampMatrix[offset - 0xe0] = data; break;*/
-
+	
     default:
-      logerror("io0_w: offset %x, data %02x\n", offset, data);
+      logerror("io0_w: PC=%X offset %x, data %02x\n", cpunum_get_pc(0), offset, data);
   }
 }
+
 static READ_HANDLER(io0_r) {
   UINT8 ret = 0;
+  static int r=0;
+
   switch (offset) {
-/*    case 0xeb: case 0xec: case 0xed: case 0xee: case 0xef:
-      ret = coreGlobals.swMatrix[offset - 0xea]; break;*/
+	case 0xeb: 
+		ret = coreGlobals.swMatrix[ram_01[0x0096] + 1]; //break;
+		break;
+	case 0xef:	
+		ret = coreGlobals.swMatrix[ram_01[0x0095] + 1]; break;
+	  break;
     default:
-      logerror("io0_r: offset %x\n", offset);
+      //logerror("io0_r: offset %x\n", offset);
+	  logerror("io0_r: PC=%X offset %x ret %02x RAM VAL %02x\n", cpunum_get_pc(0), offset, ret, ram_01[0x0096]);
+
   }
+
   return ret;
 }
-static UINT8 *io1_shadow;
 static WRITE_HANDLER(io1_w) {
   switch (offset) {
 	case 0xe2: // segment display digit
-		coreGlobals.segments[data & 0x0f].w = wico_data2seg[data >> 4];
+		coreGlobals.segments[6 - (data & 0x0f)].w = wico_data2seg[data >> 4];
+		if ((data >> 4) == 0x0c) logerror("CPU1 SELF TEST DONE -------------------\n");
 		break;
 
+/*	case 0xe5: // don't know
+		coreGlobals.tmpLampMatrix[offset - 0xe0] = data; break;
+		break;*/
+
 	case 0xe6: // watchdog housekeeping cpu reset line
-	  if (data == 0xff) cpunum_set_reset_line(0, CLEAR_LINE); // release reset line so housekeeping (cpu0) starts
-	  else logerror("io1_w: offset %x, data %02x not handled\n", offset, data);
+		if (data == 0xff) cpunum_set_reset_line(0, CLEAR_LINE); // release reset line so housekeeping (cpu0) starts
+		else 
+		{
+			cpunum_set_reset_line(0, ASSERT_LINE);
+			logerror("watchdog assert line\n");
+		}
+	  break;
+
+	case 0xe9: // enable/disable general timing interrupt
+		locals.gtIRQEnable = data;
+		logerror("io1_w: INFO GT IRQ offset %x, data %02x\n", offset, data);
 	  break;
 
 /*    case 0xe0:
@@ -138,20 +175,16 @@ static WRITE_HANDLER(io1_w) {
 	  //coreGlobals.segments[0].w = data; break;
 */
     default:
-      logerror("io1_w: offset %x, data %02x\n", offset, data);
+      logerror("io1_w: PC=%X offset %x, data %02x\n", cpunum_get_pc(1), offset, data);
   }
-
-  io1_shadow[offset] = data;
 }
 static READ_HANDLER(io1_r) {
 	switch (offset) {
     default:
-      logerror("io1_r: offset %x\n", offset); return 0;
+      logerror("io1_r: PC=%X offset %x\n", cpunum_get_pc(1), offset); return 0;
   }
-  return io1_shadow[offset];
 }
 
-static UINT8 *ram_01;
 static READ_HANDLER(ram_01_r) {
   return ram_01[offset];
 }
@@ -189,13 +222,15 @@ MEMORY_END
 
 static MEMORY_WRITE_START(WICO_1_writemem)
   {0x0000,0x07ff, ram_01_w},
-  {0x1f00,0x1fff, io1_w, &io1_shadow},
+  {0x1f00,0x1fff, io1_w},
   {0x4000,0x40ff, nvram_w, &nvram},
 MEMORY_END
 
 static MACHINE_INIT(WICO) {
   memset(&locals, 0, sizeof locals);  
   cpunum_set_reset_line(0, ASSERT_LINE);
+
+//  coreGlobals.swMatrix[16] = 0x80; // DEBUG SELF-TEST
 }
 
 struct SN76496interface WICO_sn76494Int = {
@@ -211,6 +246,7 @@ MACHINE_DRIVER_START(aftor)
   MDRV_SWITCH_UPDATE(WICO)
   MDRV_DIPS(16)
   MDRV_NVRAM_HANDLER(generic_0fill)
+  MDRV_DIAGNOSTIC_LEDH(2)
 
   MDRV_CPU_ADD_TAG("mcpu housekeeping", M6809, 1500000)
 //  MDRV_CPU_VBLANK_INT(WICO_vblank, 1)
@@ -232,13 +268,14 @@ INPUT_PORTS_START(aftor) \
   SIM_PORTS(1) \
   PORT_START /* 0 */ \
     COREPORT_BIT(     0x0001, "Test 1", KEYCODE_1) \
-    COREPORT_BIT(     0x0002, "Test 2", KEYCODE_2) \
-    COREPORT_BIT(     0x0004, "Test 3", KEYCODE_3) \
-    COREPORT_BIT(     0x0008, "Test 4", KEYCODE_4) \
-    COREPORT_BIT(     0x0010, "Test 5", KEYCODE_5) \
-    COREPORT_BIT(     0x0020, "Test 6", KEYCODE_6) \
-    COREPORT_BIT(     0x0040, "Test 7", KEYCODE_7) \
-    COREPORT_BIT(     0x0080, "Test 8", KEYCODE_8) \
+	COREPORT_BIT(     0x0002, "Test 2", KEYCODE_2) \
+	COREPORT_BIT(     0x0004, "Test 3", KEYCODE_3) \
+	COREPORT_BIT(     0x0008, "Test 4", KEYCODE_4) \
+	COREPORT_BIT(     0x0010, "Test 5", KEYCODE_5) \
+	COREPORT_BIT(     0x0020, "Test 6", KEYCODE_6) \
+	COREPORT_BIT(     0x0040, "Test 7", KEYCODE_7) \
+	COREPORT_BIT(     0x0080, "Test 8", KEYCODE_8) \
+
   PORT_START /* 1 */ \
     COREPORT_DIPNAME( 0x0001, 0x0000, "S1") \
       COREPORT_DIPSET(0x0000, "0" ) \
@@ -299,8 +336,12 @@ ROM_START(aftor) \
 ROM_END
 
 static core_tLCDLayout dispAftor[] = {
-  {0, 0, 0,16,CORE_SEG10},
-  {2, 0,16,16,CORE_SEG10},
+  {0, 0, 0,7,CORE_SEG10},
+  {0,16, 7,7,CORE_SEG10},
+  {2, 0,14,7,CORE_SEG10},
+  {2,16,21,7,CORE_SEG10},
+  {4,10,28,2,CORE_SEG7},
+  {4,16,30,2,CORE_SEG7},
   {0}
 };
 static core_tGameData aftorGameData = {GEN_ALVG,dispAftor,{FLIP_SW(FLIP_L),0,2}};
