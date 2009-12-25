99fb98f6027805c0d5dba9d5bf340de36820d73f
Matthias Buecher <maddes.b@arcor.de>
2009-12-25 13:52:56 +0000

    Automated converting VC++ project files to VC 2003, 2005, 2008 by using a WSH/VBScript to do the necessary text replacements.
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@2937 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- copy_vc2002_to_vc2003.bat
+++ copy_vc2002_to_vc2003.bat
@@ -2,7 +2,22 @@ copy /V /-Y "InstallVPinMAME_VC2002.vcproj" "InstallVPinMAME_VC2003.vcproj"
 copy /V /-Y "PinMAME_VC2002.vcproj" "PinMAME_VC2003.vcproj"
 copy /V /-Y "PinMAME32_VC2002.vcproj" "PinMAME32_VC2003.vcproj"
 copy /V /-Y "VPinMAME_VC2002.vcproj" "VPinMAME_VC2003.vcproj"
+
+@cscript "simplereplace.wsf" //nologo /search:"VC2002" /replace:"VC2003" /in:"InstallVPinMAME_VC2003.vcproj" /out:"InstallVPinMAME_VC2003.vcproj"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"VC2002" /replace:"VC2003" /in:"PinMAME_VC2003.vcproj" /out:"PinMAME_VC2003.vcproj"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"VC2002" /replace:"VC2003" /in:"PinMAME32_VC2003.vcproj" /out:"PinMAME32_VC2003.vcproj"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"VC2002" /replace:"VC2003" /in:"VPinMAME_VC2003.vcproj" /out:"VPinMAME_VC2003.vcproj"
+@if errorlevel 1 goto manual
+
+@goto end
+
+:manual
 @echo.
-@echo Now replace all occurrences of VC2002 in them with VC2003
-@echo then convert them with VC2003 and compile.
+@echo Replace all occurrences of VC2002 in the project files with VC2003.
+
+:end
+@echo Convert the project files with VC2003 and compile.
 @pause
