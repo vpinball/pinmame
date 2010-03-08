0049a9355002c4901c1b775811921947ee30a083
Matthias Buecher <maddes.b@arcor.de>
2010-03-08 00:10:37 +0000

    Switched to subversion repository
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@2952 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- release_export_current_source.bat
+++ release_export_current_source.bat
@@ -1,12 +1,8 @@
 @cls
 @rem *** check parameter
 @if "%1a"=="a" goto error
-@echo *** !!! The password of anonymous is blank so just press enter !!! ***
-cvs -d:pserver:anonymous@pinmame.cvs.sourceforge.net:/cvsroot/pinmame login
 @rem *** most current source
-cvs -z3 -d:pserver:anonymous@pinmame.cvs.sourceforge.net:/cvsroot/pinmame export -DNOW -d "%1" pinmame
-@rem *** logout
-cvs -d:pserver:anonymous@pinmame.cvs.sourceforge.net:/cvsroot/pinmame logout
+svn export --force https://pinmame.svn.sourceforge.net/svnroot/pinmame/trunk "%1"
 @goto end
 
 :error
