283960fc74caf16a8c8cfc40c767f1424d139329
Matthias Buecher <maddes.b@arcor.de>
2010-03-08 00:29:09 +0000

    Added missing files from the current releases
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@2955 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

---
+++ release/romupd.bat
@@ -0,0 +1,43 @@
+cd roms
+
+copy eshak_f1.zip esha_pr4.zip > nul
+copy eshak_l1.zip esha_la1.zip> nul
+copy lwar.zip lwar_a83.zip > nul
+copy ssvc.zip ssvc_a26.zip > nul
+copy torpe.zip torp_e21.zip > nul
+copy tmach.zip tmac_a24.zip > nul
+copy play.zip play_a24.zip > nul
+copy mnfb.zip mnfb_c27.zip > nul
+copy robo.zip robo_a34.zip > nul
+copy poto.zip poto_a32.zip > nul
+copy bttf.zip bttf_a20.zip > nul
+copy simp.zip simp_a27.zip > nul
+copy chkpnt.zip ckpt_a17.zip > nul
+copy tmnt.zip tmnt_104.zip > nul
+copy batmn.zip btmn_101.zip > nul
+copy trek.zip trek_201.zip > nul
+copy hook.zip hook_408.zip > nul
+copy lw3.zip lw3_208.zip > nul
+copy stwarde.zip stwr_103.zip > nul
+copy rab.zip rab_130.zip > nul
+copy jurpark.zip jupk_513.zip > nul
+copy lah.zip lah_112.zip > nul
+copy tftc.zip tftc_303.zip > nul
+copy tommy.zip tomy_400.zip > nul
+copy wwfrumb.zip wwfr_103.zip > nul
+copy gnr.zip gnr_300.zip > nul
+
+copy eshak_l3.zip esha_la3.zip > nul
+copy southpk.zip sprk_103.zip > nul
+copy twister.zip twst_405.zip > nul
+
+copy maverick.zip mav_401.zip > nul
+copy truckstp.zip trucksp2.zip > nul
+copy harley.zip harl_a10.zip > nul
+
+copy swtril.zip swtril43.zip > nul
+copy jplstwld.zip jplstw22.zip > nul
+
+copy bnzai_p1.zip bnzai_pa.zip > nul
+
+cd ..
