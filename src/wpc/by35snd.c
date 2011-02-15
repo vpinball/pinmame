44d6c503793bd80f69ff2283cb626647ab398ab8
volkenborn <volkenborn@yahoo.com>
2011-02-15 12:00:25 +0000

    Updated TMS5220 emulation to latest MAME version
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3195 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/by35snd.c
+++ src/wpc/by35snd.c
@@ -443,7 +443,7 @@ static READ_HANDLER(snt_8910a_r);
 const struct sndbrdIntf by61Intf = {
   "BYSNT", snt_init, NULL, snt_diag, snt_manCmd_w, snt_data_w, NULL, snt_ctrl_w, NULL, 0//SNDBRD_NODATASYNC|SNDBRD_NOCTRLSYNC
 };
-static struct TMS5220interface snt_tms5220Int = { 660000, 80, snt_5220Irq };
+static struct TMS5220interface snt_tms5220Int = { 640000, 50, snt_5220Irq };
 static struct DACinterface     snt_dacInt = { 1, { 20 }};
 static struct AY8910interface  snt_ay8910Int = { 1, 3579545/4, {25}, {snt_8910a_r}};
 
@@ -504,7 +504,7 @@ static void snt_init(struct sndbrdData *brdData) {
   pia_config(SNT_PIA0, PIA_STANDARD_ORDERING, &snt_pia[0]);
   pia_config(SNT_PIA1, PIA_STANDARD_ORDERING, &snt_pia[1]);
   tms5220_reset();
-  tms5220_set_variant(variant_tmc0285);
+  tms5220_set_variant(TMS5220_IS_5200);
   for (i=0; i < 0x80; i++) memory_region(BY61_CPUREGION)[i] = 0xff;
 }
 static void snt_diag(int button) {
