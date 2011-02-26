df6974453ca5e2cf7f083f19d74061c2957be0b7
volkenborn <volkenborn@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2011-02-26 10:21:58 +0000

    Fixed display callbacks for RBION
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3211 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/segames.c
+++ src/wpc/segames.c
@@ -1396,9 +1396,9 @@ CORE_CLONEDEFNV(lotr_le,lotr,"Lord Of The Rings, The (10.02 Limited Edition)",20
 /-------------------------------------------------------------------*/
 static struct core_dispLayout dispBION[] = {
   DISP_SEG_IMPORT(se_dmd128x32),
-  {34,10, 7, 5, CORE_DMD, (void *)seminidmd1a_update},
-  {34,18, 7, 5, CORE_DMD, (void *)seminidmd1b_update},
-  {34,26, 7, 5, CORE_DMD, (void *)seminidmd1c_update}, {0}
+  {34,10, 7, 5, CORE_DMD, (genf *)seminidmd1s_update},
+  {34,18, 7, 5, CORE_DMD, (genf *)seminidmd1s_update},
+  {34,26, 7, 5, CORE_DMD, (genf *)seminidmd1s_update}, {0}
 };
 INITGAME(ripleys,GEN_WS,dispBION,SE_MINIDMD3)
 SE128_ROMSTART(ripleys, "ripcpu.320",CRC(aa997826) SHA1(2f9701370e64dd55a9bafe0c65e7eb4b9c5dbdd2))
