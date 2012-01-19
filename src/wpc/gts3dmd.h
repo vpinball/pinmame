cc03c14c7cb0155ade86a35949bc6bee9ef3069e
volkenborn <volkenborn@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2012-01-19 22:35:27 +0000

    After the successful fix for Alvin G, now correct DMD colors for Gottlieb System 3! :)
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3339 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/gts3dmd.h
+++ src/wpc/gts3dmd.h
@@ -4,15 +4,9 @@
 #pragma once
 #endif
 
-#define GTS3DMD_FRAMES	18  //# of Frames to capture
-#define GTS3DMD_100 	(GTS3DMD_FRAMES * 7 / 8)  //100% intensity minimum level
-#define GTS3DMD_66 		(GTS3DMD_FRAMES / 3)  //66% intensity minimum level
-#define GTS3DMD_33 		2  //33% intensity minimum level
-
-// void gts3_dmd128x32_refresh(struct mame_bitmap *bitmap,int full_refresh);
+#define GTS3DMD_FRAMES	15  //# of Frames to capture
 
 VIDEO_UPDATE(gts3_dmd128x32);
 VIDEO_UPDATE(gts3_dmd128x32a);
 
 #endif /* INC_GTS3DMD */
-
