4385f4778408a3fa512928151248d0f8d2fa9fe5
volkenborn <volkenborn@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2012-02-25 15:51:43 +0000

    Corrected off segments for WPC alpha games, added helmet lights for BOP
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3352 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/wpc.c
+++ src/wpc/wpc.c
@@ -71,7 +71,9 @@ static SWITCH_UPDATE(wpc);
 /---------------------*/
 UINT8 *wpc_data;     /* WPC registers */
 const struct core_dispLayout wpc_dispAlpha[] = {
-  DISP_SEG_16(0,CORE_SEG16R),DISP_SEG_16(1,CORE_SEG16R),{0}
+  {0,0, 0,13,CORE_SEG16R},{0,26,13,2,CORE_SEG16D},{0,30,15,1,CORE_SEG16N},
+  {4,0,20,13,CORE_SEG16R},{4,26,33,2,CORE_SEG16D},{4,30,35,1,CORE_SEG16N},
+  {0}
 };
 const struct core_dispLayout wpc_dispDMD[] = {
   {0,0,32,128,CORE_DMD,(genf *)wpcdmd_update,NULL}, {0}
@@ -278,7 +280,7 @@ static INTERRUPT_GEN(wpc_vblank) {
   /-------------------------------------------------------------*/
   if ((wpclocals.vblankCount % (WPC_VBLANKDIV*WPC_LAMPSMOOTH)) == 0) {
     memcpy(coreGlobals.lampMatrix, coreGlobals.tmpLampMatrix, sizeof(coreGlobals.tmpLampMatrix));
-    memset(coreGlobals.tmpLampMatrix, 0, sizeof(coreGlobals.tmpLampMatrix));
+    memset(coreGlobals.tmpLampMatrix, 0, 8);
   }
   if ((wpclocals.vblankCount % (WPC_VBLANKDIV*WPC_DISPLAYSMOOTH)) == 0) {
     if ((core_gameData->gen & GENWPC_HASDMD) == 0) {
