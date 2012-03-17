1cf9a91cdfed9421ec2c4e2b0f2aec4edb82d6d3
destruk <destruk@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2012-03-17 18:42:10 +0000

    added STTNG_P4 - thanks to Scott Charles
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3360 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/sims/wpc/full/sttng.c
+++ src/wpc/sims/wpc/full/sttng.c
@@ -617,6 +617,7 @@ DCS_SOUNDROM7x("su2-sp1.rom",CRC(bdef8b2c) SHA1(188d8d2a652844e9885bd9e9ad414392
                "ng_u8_s.l1",CRC(c9fb065e) SHA1(c148178ee0ea787acc88078db01d17073e75fdc7))
 WPC_ROMEND
 
+WPC_ROMSTART(sttng,p4,"sttng_p4.u6",0x80000,CRC(836774f0) SHA1(5784f77eaad41ccf07446874720be146fd562c68)) ST_SND WPC_ROMEND
 WPC_ROMSTART(sttng,p5,"sttng_p5.u6",0x80000,CRC(c1b80a8e) SHA1(90dd99efd41ec5405c631ad374a369f9fcb7217e)) ST_SND WPC_ROMEND
 WPC_ROMSTART(sttng,p6,"sttng_p6.u6",0x80000,CRC(3162c58a) SHA1(f9007a36b53f762fb220f23953cd9745e2ca3cba)) ST_SND WPC_ROMEND
 WPC_ROMSTART(sttng,g7,"trek_lg7.rom",0x80000,CRC(e723b8a1) SHA1(77c3f8ea378772ce45bb8de818069fc08cbc4574)) ST_SND WPC_ROMEND
@@ -633,6 +634,7 @@ CORE_GAMEDEF(sttng,l7,"Star Trek: The Next Generation (LX-7)",1994,"Williams",wp
 CORE_CLONEDEF(sttng,d7,l7,"Star Trek: The Next Generation (DX-7) LED Ghost Fix",1994,"Williams",wpc_mDCSS,0)
 CORE_CLONEDEF(sttng,x7,l7,"Star Trek: The Next Generation (LX-7 Special)",1994,"Williams",wpc_mDCSS,0)
 CORE_CLONEDEF(sttng,dx,l7,"Star Trek: The Next Generation (DX-7 Special) LED Ghost Fix",1994,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(sttng,p4,l7,"Star Trek: The Next Generation (P-4)",1993,"Williams",wpc_mDCSS,0)
 CORE_CLONEDEF(sttng,p5,l7,"Star Trek: The Next Generation (P-5)",1993,"Williams",wpc_mDCSS,0)
 CORE_CLONEDEF(sttng,p6,l7,"Star Trek: The Next Generation (P-6) LED Ghost Fix",1993,"Williams",wpc_mDCSS,0)
 CORE_CLONEDEF(sttng,s7,l7,"Star Trek: The Next Generation (LX-7) SP1",1994,"Williams",wpc_mDCSS,0)
