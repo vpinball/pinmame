cdf7f2123e86d7e1b9eacfb531ce52492230a6af
maddes <maddes@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2012-08-28 20:07:55 +0000

    Fixed missing End-Of-List markers. Thanks to "toxie" for the bug report.
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3385 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/sims/se/prelim/elvis.c
+++ src/wpc/sims/se/prelim/elvis.c
@@ -468,7 +468,7 @@ static sim_tSimData elvisSimData = {
 / Game Data Information
 /----------------------*/
 static struct core_dispLayout dispElvis[] = {
-  { 0, 0,32,128,CORE_DMD, (genf *)dedmd32_update, NULL},
+  { 0, 0,32,128,CORE_DMD, (genf *)dedmd32_update, NULL}, {0}
 };
 static core_tGameData elvisGameData = {
   GEN_WS, dispElvis,
