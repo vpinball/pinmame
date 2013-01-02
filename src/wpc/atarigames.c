c23427c7b3e911dd0031c07b6216dfa46127ac86
volkenborn <volkenborn@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2013-01-02 13:07:45 +0000

    Added sound PROM for Atari gen.1, corrected gen.1 sound
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3414 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/atarigames.c
+++ src/wpc/atarigames.c
@@ -6,9 +6,9 @@
 #define FLIPSW1920 FLIP_SWNO(19,20)
 #define FLIPSW6667 FLIP_SWNO(66,67)
 
-#define INITGAME1(name, disptype, flippers, balls) \
+#define INITGAME1(name, disptype, flippers, balls, sndType) \
 	ATARI1_INPUT_PORTS_START(name, balls) ATARI_INPUT_PORTS_END \
-	static core_tGameData name##GameData = {0,disptype,{flippers,0,9,0,SNDBRD_ATARI1,0}}; \
+	static core_tGameData name##GameData = {0,disptype,{flippers,0,9,0,SNDBRD_ATARI1,0,sndType}}; \
 	static void init_##name(void) { \
 		core_gameData = &name##GameData; \
 	}
@@ -52,56 +52,62 @@ core_tLCDLayout atari_disp2[] = {
 /*-------------------------------------------------------------------
 / The Atarians (11/1976)
 /-------------------------------------------------------------------*/
-INITGAME1(atarians, atari_disp1, FLIP_SWNO(36,37), 1)
+INITGAME1(atarians, atari_disp1, FLIP_SWNO(36,37), 1, 0)
 ATARI_2_ROMSTART(atarians,	"atarian.e0",	CRC(45cb0427) SHA1(e286930ca36bdd0f79acefd142d2a5431fa8005b),
 							"atarian.e00",	CRC(6066bd63) SHA1(e993497d0ca9f056e18838494089def8bdc265c9))
+ATARI_SNDSTART("82s130.bin", CRC(da1f77b4) SHA1(b21fdc1c6f196c320ec5404013d672c35f95890b))
 ATARI_ROMEND
 CORE_GAMEDEFNV(atarians,"The Atarians",1976,"Atari",gl_mATARI0,GAME_NOT_WORKING)
 
 /*-------------------------------------------------------------------
 / The Atarians (working bootleg)
 /-------------------------------------------------------------------*/
-INITGAME1(atarianb, atari_disp1, FLIP_SWNO(36,37), 1)
+INITGAME1(atarianb, atari_disp1, FLIP_SWNO(36,37), 1, 0)
 ATARI_2_ROMSTART(atarianb,	"atarian.e0",	CRC(45cb0427) SHA1(e286930ca36bdd0f79acefd142d2a5431fa8005b),
 							"atarianb.e00",	CRC(74fc86e4) SHA1(135d75e5c03feae0929fa84caa3c802353cdd94e))
+ATARI_SNDSTART("82s130.bin", CRC(da1f77b4) SHA1(b21fdc1c6f196c320ec5404013d672c35f95890b))
 ATARI_ROMEND
 CORE_CLONEDEFNV(atarianb,atarians,"The Atarians (working bootleg)",2002,"Atari / Gaston",gl_mATARI0,0)
 
 /*-------------------------------------------------------------------
 / Time 2000 (06/1977)
 /-------------------------------------------------------------------*/
-INITGAME1(time2000, atari_disp1, FLIPSW1920, 1)
+INITGAME1(time2000, atari_disp1, FLIPSW1920, 1, 0)
 ATARI_2_ROMSTART(time2000,	"time.e0",	CRC(1e79c133) SHA1(54ce5d59a00334fcec8b12c077d70e3629549af0),
 							"time.e00",	CRC(e380f35c) SHA1(f2b4c508c8b7a2ce9924da97c05fb31d5115f36f))
+ATARI_SNDSTART("82s130.bin", CRC(da1f77b4) SHA1(b21fdc1c6f196c320ec5404013d672c35f95890b))
 ATARI_ROMEND
 CORE_GAMEDEFNV(time2000,"Time 2000",1977,"Atari",gl_mATARI1,0)
 
 /*-------------------------------------------------------------------
 / Airborne Avenger (09/1977)
 /-------------------------------------------------------------------*/
-INITGAME1(aavenger, atari_disp1, FLIPSW1920, 1)
+INITGAME1(aavenger, atari_disp1, FLIPSW1920, 1, 1)
 ATARI_2_ROMSTART(aavenger,	"airborne.e0",	CRC(44e67c54) SHA1(7f94189c12e322c41908d651cf6a3b6061426959),
 							"airborne.e00",	CRC(05ac26b8) SHA1(114d587923ade9370d606e428af02a407d272c85))
+ATARI_SNDSTART("82s130.bin", CRC(da1f77b4) SHA1(b21fdc1c6f196c320ec5404013d672c35f95890b))
 ATARI_ROMEND
 CORE_GAMEDEFNV(aavenger,"Airborne Avenger",1977,"Atari",gl_mATARI1,0)
 
 /*-------------------------------------------------------------------
 / Middle Earth (02/1978)
 /-------------------------------------------------------------------*/
-INITGAME1(midearth, atari_disp1, FLIPSW1920, 1)
+INITGAME1(midearth, atari_disp1, FLIPSW1920, 1, 2)
 ATARI_2_ROMSTART(midearth,	"608.bin",	CRC(28b92faf) SHA1(8585770f4059049f1dcbc0c6ef5718b6ff1a5431),
 							"609.bin",	CRC(589df745) SHA1(4bd3e4f177e8d86bab41f3a14c169b936eeb480a))
+ATARI_SNDSTART("82s130.bin", CRC(da1f77b4) SHA1(b21fdc1c6f196c320ec5404013d672c35f95890b))
 ATARI_ROMEND
 CORE_GAMEDEFNV(midearth,"Middle Earth",1978,"Atari",gl_mATARI1A,0)
 
 /*-------------------------------------------------------------------
 / Space Riders (09/1978)
 /-------------------------------------------------------------------*/
-INITGAME1(spcrider, atari_disp1, FLIPSW6667, 1)
+INITGAME1(spcrider, atari_disp1, FLIPSW6667, 1, 2)
 ATARI_2_ROMSTART(spcrider,	"spacel.bin",	CRC(66ffb04e) SHA1(42d8b7fb7206b30478f631d0e947c0908dcf5419),
 							"spacer.bin",	CRC(3cf1cd73) SHA1(c46044fb815b439f12fb3e21c470c8b93ebdfd55))
+ATARI_SNDSTART("82s130.bin", CRC(da1f77b4) SHA1(b21fdc1c6f196c320ec5404013d672c35f95890b))
 ATARI_ROMEND
-CORE_GAMEDEFNV(spcrider,"Space Riders",1978,"Atari",gl_mATARI1,GAME_IMPERFECT_SOUND)
+CORE_GAMEDEFNV(spcrider,"Space Riders",1978,"Atari",gl_mATARI1,0)
 
 /*-------------------------------------------------------------------
 / Superman (03/1979)
