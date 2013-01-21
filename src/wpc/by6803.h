29a41e0a24aba3c44b32f524ce3f3fcd089dc845
volkenborn <volkenborn@yahoo.com>
2013-01-21 17:21:29 +0000

    Restore #pragma directive
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3423 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/by6803.h
+++ src/wpc/by6803.h
@@ -1,5 +1,8 @@
 #ifndef INC_BY6803
 #define INC_BY6803
+#if !defined(__GNUC__) || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || (__GNUC__ >= 4)	// GCC supports "pragma once" correctly since 3.4
+#pragma once
+#endif
 
 /*-- Common Inports for BY6803 Games --*/
 #define BY6803_COMPORTS \
