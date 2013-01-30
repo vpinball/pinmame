70a0cf50aa26d25614d8c048f0da66ebfc725c25
destruk <destruk@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2013-01-30 21:17:06 +0000

    Added snspare2 - Thanks PinKitty!
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3463 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/bowlgames.c
+++ src/wpc/bowlgames.c
@@ -536,6 +536,15 @@ NORMALREGION(0x100000, REGION_USER3) \
 GTS3_ROMEND
 CORE_CLONEDEFNV(snspare1,snspares,"Strikes n' Spares (rev.1)",1995,"Gottlieb",mGTS3DMDS2, 0)
 
+#define init_snspare2 init_snspares
+#define input_ports_snspare2 input_ports_snspares
+GTS3ROMSTART(snspare2,  "gprom2.bin",CRC(79906dfc) SHA1(1efb68dd391f79e6f8ad5a588145d6ad7b36743c))
+GTS3_DMD256_ROMSTART2(  "dsprom.bin",CRC(5c901899) SHA1(d106561b2e382afdb16e938072c9c8f1d1ccdae6))
+NORMALREGION(0x100000, REGION_USER3) \
+  GTS3S_ROMLOAD4(0x00000, "arom1.bin", CRC(e248574a) SHA1(d2bdc2b9a330bb81556d25d464f617e0934995eb)) \
+GTS3_ROMEND
+CORE_CLONEDEFNV(snspare2,snspares,"Strikes n' Spares (rev.2)",1995,"Gottlieb",mGTS3DMDS2, 0)
+
 /*-----------------------------
 / League Champ (Shuffle Alley)
 /------------------------------*/
