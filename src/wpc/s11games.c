e2c4b0a8e1d0722ac549e9a0525574af0fb46a47
destruk <brian@oddchange.com>
2013-01-30 06:25:30 +0000

    added Riverboat Gambler PA-7 -- thanks PinKitty!
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3443 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/s11games.c
+++ src/wpc/s11games.c
@@ -1030,6 +1030,14 @@ S11_ROMEND
 #define input_ports_rvrbt input_ports_s11
 CORE_GAMEDEF(rvrbt, l3, "Riverboat Gambler (L-3)", 1990, "Williams", s11_mS11CS,0)
 
+S11_ROMSTART48(rvrbt,p7,"gamb_u26.pa7", CRC(594db647) SHA1(914d10755c2a1609d3945554fdd8006f7d294287),
+                        "gamb_u27.pa7", CRC(696248c4) SHA1(892ea48121165792d10d64dcfc188e4a100e6371))
+S11CS_SOUNDROM000(      "gamb_u4.l2",  CRC(c0cfa9be) SHA1(352df9a4dcbc131ae249416e9e517137a04627ba),
+                        "gamb_u19.l1", CRC(04a3a8c8) SHA1(e72ef767f13282d2335cda3288037610d9bfedf2),
+                        "gamb_u20.l1", CRC(a60c734d) SHA1(76cfcf96276ca4f6b5eee0e0402fab5ee9685366))
+S11_ROMEND
+CORE_CLONEDEF(rvrbt, p7, l3, "Riverboat Gambler (PA-7)", 1990, "Williams", s11_mS11CS,0)
+
 /*--------------------
 / Bugs Bunny Birthday Ball 11/90
 /--------------------*/
