0db8e15bbed0b0e4a64669eeced56bdbba78e3e1
volkenborn <volkenborn@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2014-05-06 06:56:20 +0000

    Displays improved for V.1, but speech, lamps, and solenoids not done
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3536 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/idsa.c
+++ src/wpc/idsa.c
@@ -4,9 +4,10 @@
    Hardware:
    ---------
 		CPU:     Z80 @ 4 MHz
-			INT: IRQ @ 977 Hz (4MHz/2048/2)
+			INT: IRQ @ 977 Hz (4MHz/2048/2) or 488 Hz (4MHz/2048/4)
 		IO:      DMA, AY8910 ports
-		DISPLAY: 7-digit 7-segment panels with PROM-based 5-bit BCD data (allowing a simple alphabet)
+		DISPLAY: bsktball: 7-digit 7-segment panels with PROM-based 5-bit BCD data (allowing a simple alphabet)
+		         v1: 6-digit 7-segment panels with BCD decoding
 		SOUND:	 2 x AY8910 @ 2 MHz plus SP0256 @ 3.12 MHz on board
  ************************************************************************************************/
 
@@ -26,6 +27,7 @@ static struct {
   int dispCol;
   int dispRow;
   core_tSeg segments;
+  int isV1;
 } locals;
 
 static INTERRUPT_GEN(IDSA_irq) {
@@ -42,13 +44,14 @@ static void IDSA_nmi(int data) {
 static INTERRUPT_GEN(IDSA_vblank) {
   memcpy(coreGlobals.segments, locals.segments, sizeof(coreGlobals.segments));
 //  memset(locals.segments, 0, sizeof(locals.segments));
-  core_updateSw(core_getSol(10));
+  core_updateSw(core_getSol(locals.isV1 ? 2 : 10));
 }
 
 static SWITCH_UPDATE(IDSA) {
   if (inports) {
+    CORE_SETKEYSW(inports[CORE_COREINPORT] >> 12, 0x01, 0);
     CORE_SETKEYSW(inports[CORE_COREINPORT], 0x78, 1);
-    CORE_SETKEYSW(inports[CORE_COREINPORT] >> 8, 0x02, 3);
+    CORE_SETKEYSW(inports[CORE_COREINPORT] >> 8, 0x02, locals.isV1 ? 2 : 3);
   }
 }
 
@@ -82,19 +85,20 @@ static UINT16 idsa2seg7(UINT8 data) {
   }
 }
 
-static WRITE_HANDLER(ay8910_0_ctrl_w) { AY8910Write(0,0,data); }
-static WRITE_HANDLER(ay8910_0_data_w) { AY8910Write(0,1,data); }
-
-static WRITE_HANDLER(ay8910_1_ctrl_w) { AY8910Write(1,0,data); }
-static WRITE_HANDLER(ay8910_1_data_w) { AY8910Write(1,1,data); }
-
 static WRITE_HANDLER(ay8910_0_portA_w) {
   coreGlobals.solenoids = (coreGlobals.solenoids & 0xffffff00) | (data ^ 0xff);
+  if (locals.isV1) coreGlobals.lampMatrix[0] = ~data;
 }
 static WRITE_HANDLER(ay8910_0_portB_w) {
   coreGlobals.solenoids = (coreGlobals.solenoids & 0xff00ffff) | ((data ^ 0xff) << 16);
+  if (locals.isV1) coreGlobals.lampMatrix[2] = ~data;
 }
 static WRITE_HANDLER(ay8910_1_portA_w) {
+  if (locals.isV1) {
+    coreGlobals.solenoids = (coreGlobals.solenoids & 0x00ffffff) | ((data ^ 0xff) << 24);
+    coreGlobals.lampMatrix[3] = ~data;
+    return;
+  }
   if ((data >> 4) != 0x0f) {
     locals.dispCol = data >> 4;
   } else {
@@ -109,6 +113,7 @@ static WRITE_HANDLER(ay8910_1_portA_w) {
 }
 static WRITE_HANDLER(ay8910_1_portB_w) {
   coreGlobals.solenoids = (coreGlobals.solenoids & 0xffff00ff) | ((data ^ 0xff) << 8);
+  if (locals.isV1) coreGlobals.lampMatrix[1] = ~data;
 }
 struct AY8910interface IDSA_ay8910Int = {
 	2,					/* 2 chips */
@@ -121,15 +126,11 @@ struct AY8910interface IDSA_ay8910Int = {
 };
 
 static READ_HANDLER(sp0256_r) {
-  static UINT16 retVal;
-  if (!offset) retVal = spb640_r(0, 0);
-  return offset ? (UINT8)(retVal >> 8) : (UINT8)(retVal & 0xff);
-}
-static WRITE_HANDLER(sp0256_w) {
-  spb640_w(0, 0, data);
+  UINT16 data = spb640_r(offset / 2, 0);
+  return offset % 2 ? (UINT8)(data >> 8) : (UINT8)(data & 0xff);
 }
 struct sp0256_interface IDSA_sp0256Int = {
-  50, /* volume */
+  MIXER(30,MIXER_PAN_RIGHT), /* volume */
   3120000, /* clock */
   NULL,
   NULL,
@@ -144,6 +145,19 @@ static READ_HANDLER(dip_r) {
   return ~core_getDip(offset >> 4);
 }
 
+static READ_HANDLER(port_bd_r) {
+  return 0x01 ^ coreGlobals.swMatrix[0];
+}
+
+static WRITE_HANDLER(row_w) {
+  locals.dispRow++;
+}
+
+static WRITE_HANDLER(disp_w) {
+  if (locals.dispRow < 4)
+    locals.dispData[locals.dispRow] = data;
+}
+
 /*-----------------------------------------------
 / Load/Save static ram
 /-------------------------------------------------*/
@@ -153,7 +167,12 @@ static NVRAM_HANDLER(IDSA) {
 }
 static WRITE_HANDLER(IDSA_CMOS_w) {
   IDSA_CMOS[offset] = data;
-  if (offset > 0xa7 && offset < 0xb2)
+  if (locals.isV1) {
+    if (offset > 0x0c && offset < 0x27)
+      locals.segments[0x26 - offset].w = core_bcd2seg7e[data & 0x0f];
+    else if (offset > 0x81 && offset < 0x86)
+      locals.segments[6 * (3-(offset - 0x82)) + 7].w = core_bcd2seg7e[data & 0x0f];
+  } else if (offset > 0xa7 && offset < 0xb2)
     coreGlobals.lampMatrix[offset - 0xa8] = data;
 }
 static READ_HANDLER(IDSA_CMOS_r) {
@@ -170,48 +189,31 @@ static MEMORY_WRITE_START(IDSA_writemem)
   {0x8000,0x87ff, IDSA_CMOS_w, &IDSA_CMOS},
 MEMORY_END
 
-static WRITE_HANDLER(col_w) {
-//printf("%x:%02x ", offset, data);
-}
-
-static WRITE_HANDLER(disp_w) {
-  if (locals.dispRow < 4)
-    locals.dispData[locals.dispRow] = data;
-  locals.dispRow++;
-}
-
-static READ_HANDLER(port_b3_r) {
-  return 0xf0;
-}
-
-static READ_HANDLER(port_bd_r) {
-  return 0x0f;
-}
-
 static PORT_READ_START(IDSA_readport)
   {0x00,0x50, sw_r},
   {0x60,0x70, dip_r},
-  {0xb0,0xb1, sp0256_r},
-  {0xb3,0xb3, port_b3_r},
+  {0xb0,0xb3, sp0256_r},
   {0xbd,0xbd, port_bd_r},
 MEMORY_END
 
 static PORT_WRITE_START(IDSA_writeport)
-  {0x80,0x8f, col_w},
+  {0x80,0x8f, row_w},
   {0x90,0x90, disp_w},
-  {0xc0,0xc1, sp0256_w},
-  {0xe0,0xe0, ay8910_0_ctrl_w},
-  {0xe1,0xe1, ay8910_0_data_w},
-  {0xf0,0xf0, ay8910_1_ctrl_w},
-  {0xf1,0xf1, ay8910_1_data_w},
+  {0xd0,0xd0, sp0256_ALD_w},
+  {0xe0,0xe0, AY8910_control_port_0_w},
+  {0xe1,0xe1, AY8910_write_port_0_w},
+  {0xf0,0xf0, AY8910_control_port_1_w},
+  {0xf1,0xf1, AY8910_write_port_1_w},
 MEMORY_END
 
-static MACHINE_INIT(IDSA) {
+static void reset_common(void) {
+  memset(&locals, 0, sizeof locals);
+  sp0256_reset();
+//  sp0256_bitrevbuff(memory_region(REGION_SOUND1), 0, 0x10000);
 }
 
 static MACHINE_RESET(IDSA) {
-  memset(&locals, 0, sizeof locals);
-  sp0256_reset();
+  reset_common();
 }
 
 MACHINE_DRIVER_START(idsa)
@@ -222,13 +224,25 @@ MACHINE_DRIVER_START(idsa)
   MDRV_CPU_VBLANK_INT(IDSA_vblank, 1)
   MDRV_CPU_PERIODIC_INT(IDSA_irq, IDSA_CPUFREQ / 4096)
   MDRV_TIMER_ADD(IDSA_nmi, 0)
-  MDRV_CORE_INIT_RESET_STOP(IDSA,IDSA,NULL)
+  MDRV_CORE_INIT_RESET_STOP(NULL,IDSA,NULL)
   MDRV_NVRAM_HANDLER(IDSA)
   MDRV_DIPS(16)
   MDRV_SWITCH_UPDATE(IDSA)
   MDRV_SOUND_ADD(AY8910, IDSA_ay8910Int)
   MDRV_SOUND_ADD(SP0256, IDSA_sp0256Int)
   MDRV_SOUND_ATTRIBUTES(SOUND_SUPPORTS_STEREO)
+MACHINE_DRIVER_END
+
+static MACHINE_RESET(v1) {
+  reset_common();
+  locals.isV1 = 1;
+}
+
+MACHINE_DRIVER_START(v1)
+  MDRV_IMPORT_FROM(idsa)
+  MDRV_CPU_MODIFY("mcpu")
+  MDRV_CPU_PERIODIC_INT(IDSA_irq, IDSA_CPUFREQ / 8192)
+  MDRV_CORE_INIT_RESET_STOP(NULL,v1,NULL)
 MACHINE_DRIVER_END
 
 INPUT_PORTS_START(idsa)
@@ -240,6 +254,7 @@ INPUT_PORTS_START(idsa)
     COREPORT_BIT     (0x0010, "Coin 2", KEYCODE_4)
     COREPORT_BIT     (0x0008, "Coin 3", KEYCODE_5)
     COREPORT_BIT     (0x0200, "Tilt",   KEYCODE_INSERT)
+    COREPORT_BIT     (0x1000, "Slam Tilt", KEYCODE_HOME)
   PORT_START /* 1 */
     COREPORT_DIPNAME( 0x0001, 0x0000, "S1")
       COREPORT_DIPSET(0x0000, "0" )
@@ -291,15 +306,14 @@ INPUT_PORTS_START(idsa)
       COREPORT_DIPSET(0x8000, "1" )
 INPUT_PORTS_END
 
-core_tLCDLayout idsa_disp[] = {
-  {0, 0, 0, 7, CORE_SEG7}, {0,16, 7, 7, CORE_SEG7},
-  {3, 0,16, 7, CORE_SEG7}, {3,16,23, 7, CORE_SEG7},
-  {6, 6,36, 1, CORE_SEG7}, {6,10,38, 2, CORE_SEG7}, {6,16,41, 1, CORE_SEG7}, {6,20,43, 2, CORE_SEG7},
-  {9, 0,48, 2, CORE_SEG7},
+core_tLCDLayout v1_disp[] = {
+  {0, 0,20, 6, CORE_SEG7}, {0,20,14, 6, CORE_SEG7},
+  {3, 0, 8, 6, CORE_SEG7}, {3,20, 2, 6, CORE_SEG7},
+  {3,14, 0, 2, CORE_SEG7},
   {0}
 };
 
-static core_tGameData v1GameData = {0,idsa_disp,{FLIP_SWNO(28,30),0,2}};
+static core_tGameData v1GameData = {0,v1_disp,{FLIP_SW(FLIP_L),0,0}};
 static void init_v1(void) {
   core_gameData = &v1GameData;
 }
@@ -313,7 +327,15 @@ ROM_START(v1)
   ROM_RELOAD(0x8000, 0x4000)
   ROM_RELOAD(0xc000, 0x4000)
 ROM_END
-CORE_GAMEDEFNV(v1, "V.1", 198?, "IDSA (Spain)", idsa, GAME_NOT_WORKING)
+CORE_GAMEDEFNV(v1, "V.1", 198?, "IDSA (Spain)", v1, GAME_NOT_WORKING)
+
+core_tLCDLayout idsa_disp[] = {
+  {0, 0, 0, 7, CORE_SEG7}, {0,16, 7, 7, CORE_SEG7},
+  {3, 0,16, 7, CORE_SEG7}, {3,16,23, 7, CORE_SEG7},
+  {6, 6,36, 1, CORE_SEG7}, {6,10,38, 2, CORE_SEG7}, {6,16,41, 1, CORE_SEG7}, {6,20,43, 2, CORE_SEG7},
+  {9, 0,48, 2, CORE_SEG7},
+  {0}
+};
 
 static core_tGameData bsktballGameData = {0, idsa_disp,{FLIP_SWNO(28,30),0,2}};
 static void init_bsktball(void) {
