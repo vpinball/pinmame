152c069f80f26493e6936dd1ce308547ebacc554
toxieainc <toxieainc@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2014-10-13 20:19:26 +0000

    add additional paranoia-tests to PinDMD code and output error messages
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3569 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/pindmd/pinddrv.h
+++ src/pindmd/pinddrv.h
@@ -6,7 +6,9 @@ extern void pinddrvDeInit(void);
 extern void pinddrvSendFrame(void);
 
 UINT8			enabled;		// pindmd enabled? (ie device found)
+#ifndef PINDMD2
 UINT8			doOther;		// sam mode?
+#endif
 UINT8			do16;			// 16 shades?
 
 #endif
