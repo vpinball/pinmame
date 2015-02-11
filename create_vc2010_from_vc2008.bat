558e91d6ec674cde350f4f3fed6c4ae053162ff5
toxie <toxie@ainc.de>
2015-02-11 07:40:37 +0000

    tweak conversion to vc2010
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3631 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- create_vc2010_from_vc2008.bat
+++ create_vc2010_from_vc2008.bat
@@ -6,6 +6,10 @@ copy /V /-Y "VPinMAME_VC2008.vcproj" "VPinMAME_VC2010.vcproj"
 
 @cscript "simplereplace.wsf" //nologo /search:"VC2008" /replace:"VC2010" /in:"PinMAME_VC2010.sln" /out:"PinMAME_VC2010.sln"
 @if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"Visual Studio 2008" /replace:"Visual Studio 2010" /in:"PinMAME_VC2010.sln" /out:"PinMAME_VC2010.sln"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"Format Version 10.00" /replace:"Format Version 11.00" /in:"PinMAME_VC2010.sln" /out:"PinMAME_VC2010.sln"
+@if errorlevel 1 goto manual
 @cscript "simplereplace.wsf" //nologo /search:"VC2008" /replace:"VC2010" /in:"InstallVPinMAME_VC2010.vcproj" /out:"InstallVPinMAME_VC2010.vcproj"
 @if errorlevel 1 goto manual
 @cscript "simplereplace.wsf" //nologo /search:"VC2008" /replace:"VC2010" /in:"PinMAME_VC2010.vcproj" /out:"PinMAME_VC2010.vcproj"
