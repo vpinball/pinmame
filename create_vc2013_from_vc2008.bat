0655e9beeffde94a08d65a6e9a7a7c28643841c8
toxie <toxie@ainc.de>
2015-02-12 19:45:18 +0000

    have native vc2012 and up project files
    ..as too much changed in the meantime that needed tweaking
    
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3633 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- create_vc2013_from_vc2008.bat
+++ create_vc2013_from_vc2008.bat
@@ -1,31 +1,34 @@
-copy /V /-Y "PinMAME_VC2008.sln" "PinMAME_VC2013.sln"
-copy /V /-Y "InstallVPinMAME_VC2008.vcproj" "InstallVPinMAME_VC2013.vcproj"
-copy /V /-Y "PinMAME_VC2008.vcproj" "PinMAME_VC2013.vcproj"
-copy /V /-Y "PinMAME32_VC2008.vcproj" "PinMAME32_VC2013.vcproj"
-copy /V /-Y "VPinMAME_VC2008.vcproj" "VPinMAME_VC2013.vcproj"
+copy /V /-Y "PinMAME_VC2012.sln" "PinMAME_VC2013.sln"
+copy /V /-Y "InstallVPinMAME_VC2012.vcxproj" "InstallVPinMAME_VC2013.vcxproj"
+copy /V /-Y "PinMAME_VC2012.vcxproj" "PinMAME_VC2013.vcxproj"
+copy /V /-Y "PinMAME32_VC2012.vcxproj" "PinMAME32_VC2013.vcxproj"
+copy /V /-Y "VPinMAME_VC2012.vcxproj" "VPinMAME_VC2013.vcxproj"
+copy /V /-Y "InstallVPinMAME_VC2012.vcxproj.filters" "InstallVPinMAME_VC2013.vcxproj.filters"
+copy /V /-Y "PinMAME_VC2012.vcxproj.filters" "PinMAME_VC2013.vcxproj.filters"
+copy /V /-Y "PinMAME32_VC2012.vcxproj.filters" "PinMAME32_VC2013.vcxproj.filters"
+copy /V /-Y "VPinMAME_VC2012.vcxproj.filters" "VPinMAME_VC2013.vcxproj.filters"
 
-@cscript "simplereplace.wsf" //nologo /search:"VC2008" /replace:"VC2013" /in:"PinMAME_VC2013.sln" /out:"PinMAME_VC2013.sln"
+@cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2013" /in:"PinMAME_VC2013.sln" /out:"PinMAME_VC2013.sln"
 @if errorlevel 1 goto manual
-@cscript "simplereplace.wsf" //nologo /search:"Visual Studio 2008" /replace:"Visual Studio 2013" /in:"PinMAME_VC2013.sln" /out:"PinMAME_VC2013.sln"
+@cscript "simplereplace.wsf" //nologo /search:"Visual Studio 2012" /replace:"Visual Studio 2013" /in:"PinMAME_VC2013.sln" /out:"PinMAME_VC2013.sln"
 @if errorlevel 1 goto manual
-@cscript "simplereplace.wsf" //nologo /search:"Format Version 10.00" /replace:"Format Version 13.00" /in:"PinMAME_VC2013.sln" /out:"PinMAME_VC2013.sln"
+@cscript "simplereplace.wsf" //nologo /search:"Format Version 12.00" /replace:"Format Version 13.00" /in:"PinMAME_VC2013.sln" /out:"PinMAME_VC2013.sln"
 @if errorlevel 1 goto manual
-@cscript "simplereplace.wsf" //nologo /search:"VC2008" /replace:"VC2013" /in:"InstallVPinMAME_VC2013.vcproj" /out:"InstallVPinMAME_VC2013.vcproj"
+@cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2013" /in:"InstallVPinMAME_VC2013.vcxproj" /out:"InstallVPinMAME_VC2013.vcxproj"
 @if errorlevel 1 goto manual
-@cscript "simplereplace.wsf" //nologo /search:"VC2008" /replace:"VC2013" /in:"PinMAME_VC2013.vcproj" /out:"PinMAME_VC2013.vcproj"
+@cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2013" /in:"PinMAME_VC2013.vcxproj" /out:"PinMAME_VC2013.vcxproj"
 @if errorlevel 1 goto manual
-@cscript "simplereplace.wsf" //nologo /search:"VC2008" /replace:"VC2013" /in:"PinMAME32_VC2013.vcproj" /out:"PinMAME32_VC2013.vcproj"
+@cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2013" /in:"PinMAME32_VC2013.vcxproj" /out:"PinMAME32_VC2013.vcxproj"
 @if errorlevel 1 goto manual
-@cscript "simplereplace.wsf" //nologo /search:"VC2008" /replace:"VC2013" /in:"VPinMAME_VC2013.vcproj" /out:"VPinMAME_VC2013.vcproj"
+@cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2013" /in:"VPinMAME_VC2013.vcxproj" /out:"VPinMAME_VC2013.vcxproj"
 @if errorlevel 1 goto manual
 
 @goto end
 
 :manual
 @echo.
-@echo Replace all occurrences of VC2008 in the project files with VC2013.
+@echo Replace all occurrences of VC2012 in the project files with VC2013.
 
 :end
 @echo Convert the project files with VC2013 and compile.
-@echo After conversion delete the *_VC2013.vcproj files.
 @pause
--- create_vc2015_from_vc2008.bat
+++ create_vc2015_from_vc2008.bat
@@ -1,31 +1,34 @@
-copy /V /-Y "PinMAME_VC2008.sln" "PinMAME_VC2015.sln"
-copy /V /-Y "InstallVPinMAME_VC2008.vcproj" "InstallVPinMAME_VC2015.vcproj"
-copy /V /-Y "PinMAME_VC2008.vcproj" "PinMAME_VC2015.vcproj"
-copy /V /-Y "PinMAME32_VC2008.vcproj" "PinMAME32_VC2015.vcproj"
-copy /V /-Y "VPinMAME_VC2008.vcproj" "VPinMAME_VC2015.vcproj"
+copy /V /-Y "PinMAME_VC2012.sln" "PinMAME_VC2015.sln"
+copy /V /-Y "InstallVPinMAME_VC2012.vcxproj" "InstallVPinMAME_VC2015.vcxproj"
+copy /V /-Y "PinMAME_VC2012.vcxproj" "PinMAME_VC2015.vcxproj"
+copy /V /-Y "PinMAME32_VC2012.vcxproj" "PinMAME32_VC2015.vcxproj"
+copy /V /-Y "VPinMAME_VC2012.vcxproj" "VPinMAME_VC2015.vcxproj"
+copy /V /-Y "InstallVPinMAME_VC2012.vcxproj.filters" "InstallVPinMAME_VC2015.vcxproj.filters"
+copy /V /-Y "PinMAME_VC2012.vcxproj.filters" "PinMAME_VC2015.vcxproj.filters"
+copy /V /-Y "PinMAME32_VC2012.vcxproj.filters" "PinMAME32_VC2015.vcxproj.filters"
+copy /V /-Y "VPinMAME_VC2012.vcxproj.filters" "VPinMAME_VC2015.vcxproj.filters"
 
-@cscript "simplereplace.wsf" //nologo /search:"VC2008" /replace:"VC2015" /in:"PinMAME_VC2015.sln" /out:"PinMAME_VC2015.sln"
+@cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2015" /in:"PinMAME_VC2015.sln" /out:"PinMAME_VC2015.sln"
 @if errorlevel 1 goto manual
-@cscript "simplereplace.wsf" //nologo /search:"Visual Studio 2008" /replace:"Visual Studio 2015" /in:"PinMAME_VC2015.sln" /out:"PinMAME_VC2015.sln"
+@cscript "simplereplace.wsf" //nologo /search:"Visual Studio 2012" /replace:"Visual Studio 2015" /in:"PinMAME_VC2015.sln" /out:"PinMAME_VC2015.sln"
 @if errorlevel 1 goto manual
-@cscript "simplereplace.wsf" //nologo /search:"Format Version 10.00" /replace:"Format Version 14.00" /in:"PinMAME_VC2015.sln" /out:"PinMAME_VC2015.sln"
+@cscript "simplereplace.wsf" //nologo /search:"Format Version 12.00" /replace:"Format Version 14.00" /in:"PinMAME_VC2015.sln" /out:"PinMAME_VC2015.sln"
 @if errorlevel 1 goto manual
-@cscript "simplereplace.wsf" //nologo /search:"VC2008" /replace:"VC2015" /in:"InstallVPinMAME_VC2015.vcproj" /out:"InstallVPinMAME_VC2015.vcproj"
+@cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2015" /in:"InstallVPinMAME_VC2015.vcxproj" /out:"InstallVPinMAME_VC2015.vcxproj"
 @if errorlevel 1 goto manual
-@cscript "simplereplace.wsf" //nologo /search:"VC2008" /replace:"VC2015" /in:"PinMAME_VC2015.vcproj" /out:"PinMAME_VC2015.vcproj"
+@cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2015" /in:"PinMAME_VC2015.vcxproj" /out:"PinMAME_VC2015.vcxproj"
 @if errorlevel 1 goto manual
-@cscript "simplereplace.wsf" //nologo /search:"VC2008" /replace:"VC2015" /in:"PinMAME32_VC2015.vcproj" /out:"PinMAME32_VC2015.vcproj"
+@cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2015" /in:"PinMAME32_VC2015.vcxproj" /out:"PinMAME32_VC2015.vcxproj"
 @if errorlevel 1 goto manual
-@cscript "simplereplace.wsf" //nologo /search:"VC2008" /replace:"VC2015" /in:"VPinMAME_VC2015.vcproj" /out:"VPinMAME_VC2015.vcproj"
+@cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2015" /in:"VPinMAME_VC2015.vcxproj" /out:"VPinMAME_VC2015.vcxproj"
 @if errorlevel 1 goto manual
 
 @goto end
 
 :manual
 @echo.
-@echo Replace all occurrences of VC2008 in the project files with VC2015.
+@echo Replace all occurrences of VC2012 in the project files with VC2015.
 
 :end
 @echo Convert the project files with VC2015 and compile.
-@echo After conversion delete the *_VC2015.vcproj files.
 @pause
