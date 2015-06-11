4d2dba23c7cf576398a02a1d8650c0b6ca5aba04
toxieainc <toxieainc@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2015-06-11 23:29:00 +0000

    add rudimentary/whacky support for alternate/external sounds
    ..expects altsound\machinename\ filled with a directory&file structure inspired by/similar to the PinSound packages
    ..currently fully undef'd until cleaned up and made optional (currently always is triggered as soon as matching dirs&files are found)
    ..uses BASS to play all kinds of formats easily
    
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3665 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/sndbrd.c
+++ src/wpc/sndbrd.c
@@ -69,8 +69,8 @@ void sndbrd_diag(int board, int button) {
 
 void sndbrd_data_w(int board, int data) {
   const struct sndbrdIntf *b = intf[board].brdIntf;
-    if(b && (b->flags & SNDBRD_NOTSOUND)==0)
-		snd_cmd_log(board, data);
+  if(b && (b->flags & SNDBRD_NOTSOUND)==0)
+	snd_cmd_log(board, data);
   if (b && (coreGlobals.soundEn || (b->flags & SNDBRD_NOTSOUND)) && b->data_w) {
 #if 0
     if((b->flags & SNDBRD_NOTSOUND)==0)
