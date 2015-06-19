b118973d468102aab4b0eb0a13a4dc6bdbc5d45c
toxie <toxie@ainc.de>
2015-06-19 06:43:11 +0000

    add mrglees the shadow LH6 text index patched/bugfixed version
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3679 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/driver.c
+++ src/wpc/driver.c
@@ -2310,6 +2310,7 @@ DRIVER(ts,dx5)          //                  The Shadow (DX-5) LED Ghost Fix
 DRIVER(ts,la6)          //          05/95 B The Shadow (LA-6)
 DRIVER(ts,da6)          //                  The Shadow (DA-6) LED Ghost Fix
 DRIVER(ts,lh6)          //          05/95 B The Shadow (LH-6, Home version)
+DRIVER(ts,lh6p)         //          05/95 B The Shadow (LH-6, Home version, wrong text index bugfix patch)
 DRIVER(ts,dh6)          //                  The Shadow (DH-6, Home version) LED Ghost Fix
 DRIVER(ts,lf6)          //          05/95 B The Shadow (LF-6) French
 DRIVER(ts,df6)          //                  The Shadow (DF-6) French LED Ghost Fix
