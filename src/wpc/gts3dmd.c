de492ae30f3f2581438112c380249af62596dcd8
toxie <toxie@ainc.de>
2016-07-29 14:44:45 +0000

    re-retweak 4 color GTS3 games
    ..patch by lucky1
    
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3901 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/gts3dmd.c
+++ src/wpc/gts3dmd.c
@@ -16,8 +16,8 @@ extern int crtc6845_start_addr;
 
 //static int level4_a[9]  = { 0, 1, 5, 5, 5, 5, 5, 5, 15 }; // mapping for 4 color roms, mode a
 //static int level4_b[16] = { 0, 1, 1, 1, 5, 5, 5, 10, 10, 10, 15, 15, 15, 15, 15, 15 }; // mapping for 4 color roms, mode b
-static int level4_a[16] = { 0, 3, 3, 3, 6, 6, 6, 6, 8, 9, 10, 11, 12, 13, 14, 15 };
-static int level4_b[16] = { 0, 3, 3, 3, 6, 6, 6, 6, 8, 9, 10, 11, 12, 13, 14, 15 };
+static int level4_a[16] = { 0, 3, 3, 3, 6, 6, 6, 8, 8, 9, 10, 11, 12, 13, 14, 15 };
+static int level4_b[16] = { 0, 3, 3, 3, 6, 6, 6, 8, 8, 9, 10, 11, 12, 13, 14, 15 };
 static int level5[19]   = { 0, 3, 3, 4, 5, 5, 5, 7, 8, 9, 11, 11, 11, 12, 13, 14, 15, 15, 15 };
 //static int level[25]  = { 0, 0, 1, 1, 1, 5, 5, 5, 5, 5, 5, 5, 10, 10, 10, 10, 10, 15, 15, 15, 15, 15, 15, 15, 15 }; // temporary mapping for both 4 and 5 color roms // deprecated
 
