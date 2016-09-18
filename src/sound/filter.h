6538d633aec008e5b56dfeb0d3eb650bcb44c8e5
toxieainc <toxieainc@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2016-09-18 08:57:42 +0000

    use blackman-harris window for filtering (should be more general-purpose-capable than hamming), fix double precision filter path, and port op-amp multipole bandpass circuit code from MAME/MESS
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@3949 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/sound/filter.h
+++ src/sound/filter.h
@@ -9,7 +9,7 @@
 /* Max filter order */
 #define FILTER_ORDER_MAX 51
 
-/* Define to use interger calculation */
+/* Define to use integer calculation */
 #define FILTER_USE_INT
 
 #ifdef FILTER_USE_INT
@@ -55,5 +55,63 @@ INLINE void filter_insert(filter* f, filter_state* s, filter_real x) {
 
 /* Compute the filter output */
 filter_real filter_compute(filter* f, filter_state* s);
+
+
+/* Filter types */
+#define FILTER_LOWPASS		0
+#define FILTER_HIGHPASS		1
+#define FILTER_BANDPASS		2
+
+#define Q_TO_DAMP(q)	(1.0/q)
+
+typedef struct filter2_context_struct {
+	double x0, x1, x2;	/* x[k], x[k-1], x[k-2], current and previous 2 input values */
+	double y0, y1, y2;	/* y[k], y[k-1], y[k-2], current and previous 2 output values */
+	double a1, a2;		/* digital filter coefficients, denominator */
+	double b0, b1, b2;	/* digital filter coefficients, numerator */
+} filter2_context;
+
+
+/* Setup the filter context based on the passed filter type info.
+ * type - 1 of the 3 defined filter types
+ * fc   - center frequency
+ * d    - damp = 1/Q
+ * gain - overall filter gain. Set to 1 if not needed.
+ */
+void filter2_setup(int type, double fc, double d, double gain,
+                   filter2_context *filter2, unsigned int sample_rate);
+
+
+/* Reset the input/output voltages to 0. */
+void filter2_reset(filter2_context *filter2);
+
+
+/* Step the filter.
+ * x0 is the new input, which needs to be set before stepping.
+ * y0 is the new filter output.
+ */
+void filter2_step(filter2_context *filter2);
+
+
+/* Setup a filter2 structure based on an op-amp multipole bandpass circuit.
+ * NOTE: If r2 is not used then set to 0.
+ *       vRef is not needed to setup filter.
+ *
+ *                             .--------+---------.
+ *                             |        |         |
+ *                            --- c1    Z         |
+ *                            ---       Z r3      |
+ *                             |        Z         |
+ *            r1               |  c2    |  |\     |
+ *   In >----ZZZZ----+---------+--||----+  | \    |
+ *                   Z                  '--|- \   |
+ *                   Z r2                  |   >--+------> out
+ *                   Z                  .--|+ /
+ *                   |                  |  | /
+ *                  gnd        vRef >---'  |/
+ *
+ */
+void filter_opamp_m_bandpass_setup(double r1, double r2, double r3, double c1, double c2,
+                                   filter2_context *filter2, unsigned int sample_rate);
 
 #endif
