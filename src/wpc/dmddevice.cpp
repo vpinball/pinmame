72cc7fecba1d9e73bf928fad58cf01b4d66cb96f
toxie <toxie@ainc.de>
2016-10-26 23:13:18 +0000

    update to interface change
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@4059 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/dmddevice.cpp
+++ src/wpc/dmddevice.cpp
@@ -37,7 +37,7 @@ Render_16_Shades_t DmdDev_Render_16_Shades;
 typedef void (*Render_4_Shades_t)(UINT8 width, UINT8 height, UINT8 *currbuffer);
 Render_4_Shades_t DmdDev_Render_4_Shades;
 
-typedef void (*render_PM_Alphanumeric_Frame_t)(layout_t layout, UINT16 *seg_data, UINT16 *seg_data2);
+typedef void (*render_PM_Alphanumeric_Frame_t)(layout_t layout, const UINT16 *const seg_data, const UINT16 *const seg_data2);
 render_PM_Alphanumeric_Frame_t DmdDev_render_PM_Alphanumeric_Frame;
 
 
