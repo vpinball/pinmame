9e46098bab085b8702330d06c566a6039a7461da
toxie <toxie@ainc.de>
2016-11-26 23:24:59 +0000

    patch by DJRobX: Fix InstallVPM compile problem (Issue on VS2015, should not affect older compilers)
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@4105 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/instvpm/InstallVPinMAME.cpp
+++ src/instvpm/InstallVPinMAME.cpp
@@ -23,7 +23,7 @@ int RegisterUnregisterVPinMAME(HWND hWnd, int fRegister)
 	// so now let's see if we can load vpinmame.dll
 	hModule = LoadLibrary(VPMDLLNAME);
 	if ( !hModule ) {
-		DisplayError(hWnd, GetLastError(), "The '"VPMDLLNAME"' file can't be loaded.", "Be sure to have this file in the current directory!");
+		DisplayError(hWnd, GetLastError(), "The '" VPMDLLNAME "' file can't be loaded.", "Be sure to have this file in the current directory!");
 		return 0;
 	}
 
@@ -53,14 +53,14 @@ int RegisterUnregisterVPinMAME(HWND hWnd, int fRegister)
 
 		DLLREGISTERSERVER* DllRegisterServer = (DLLREGISTERSERVER*) GetProcAddress(hModule, "DllRegisterServer");
 		if ( !DllRegisterServer ) {
-			DisplayError(hWnd, GetLastError(), "Can't find the registering function (DllRegisterServer) in the "VPMDLLNAME".", "Please check if you have a valid vpinmame.dll!");
+			DisplayError(hWnd, GetLastError(), "Can't find the registering function (DllRegisterServer) in the " VPMDLLNAME ".", "Please check if you have a valid vpinmame.dll!");
 			FreeLibrary(hModule);
 			return 0;
 		}
 
 		hr = (*DllRegisterServer)();
 		if ( FAILED(hr) ) {
-			DisplayError(hWnd, hr, "Unable to register the class object!", "Please check if you have a valid "VPMDLLNAME"!");
+			DisplayError(hWnd, hr, "Unable to register the class object!", "Please check if you have a valid " VPMDLLNAME "!");
 			FreeLibrary(hModule);
 			return 0;
 		}
@@ -89,14 +89,14 @@ int RegisterUnregisterVPinMAME(HWND hWnd, int fRegister)
 
 		DLLUNREGISTERSERVER* DllUnregisterServer = (DLLUNREGISTERSERVER*) GetProcAddress(hModule, "DllUnregisterServer");
 		if ( !DllUnregisterServer ) {
-			DisplayError(hWnd, GetLastError(), "Can't find the unregistering function (DllUnegisterServer) in the "VPMDLLNAME".", "Please check if you have a valid vpinmame.dll!");
+			DisplayError(hWnd, GetLastError(), "Can't find the unregistering function (DllUnegisterServer) in the " VPMDLLNAME ".", "Please check if you have a valid vpinmame.dll!");
 			FreeLibrary(hModule);
 			return 0;
 		}
 
 		hr = (*DllUnregisterServer)();
 		if ( FAILED(hr) ) {
-			DisplayError(hWnd, hr, "Unable to unregister the class object!", "Please check if you have a valid "VPMDLLNAME"!");
+			DisplayError(hWnd, hr, "Unable to unregister the class object!", "Please check if you have a valid " VPMDLLNAME "!");
 			FreeLibrary(hModule);
 			return 0;
 		}
