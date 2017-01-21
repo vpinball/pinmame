b15a14be2362b05ec9a9503db1b6b241bf79f1e8
toxieainc <toxieainc@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2017-01-21 19:45:49 +0000

    check if same frame again (patch by lucky1)
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@4187 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- ext/dmddevice/pinDMDv3/dmddevice_dll/dmddevice.cpp
+++ ext/dmddevice/pinDMDv3/dmddevice_dll/dmddevice.cpp
@@ -10,6 +10,7 @@
 
 bool isOpen = false;
 bool useRGB = false;
+static UINT8 oldbuffer[16384] = {};
 
 
 DMDDEV int Open()
@@ -79,6 +80,11 @@ DMDDEV void Set_16_Colors_Palette(rgb24 *color)
 
 DMDDEV void Render_4_Shades(UINT16 width, UINT16 height, UINT8 *currbuffer)
 {
+	if (!memcmp(oldbuffer, currbuffer, width*height)) //check if same frame again
+		return;
+
+	memcpy(oldbuffer, currbuffer, width*height);
+
 	if (isOpen) {
 		renderDMDFrame(0x00000000080, (UINT8) width, (UINT8) height, currbuffer, 0x00); 
 	}
@@ -86,6 +92,11 @@ DMDDEV void Render_4_Shades(UINT16 width, UINT16 height, UINT8 *currbuffer)
 
 DMDDEV void Render_16_Shades(UINT16 width, UINT16 height, UINT8 *currbuffer) 
 {
+	if (!memcmp(oldbuffer, currbuffer, width*height)) //check if same frame again
+		return;
+
+	memcpy(oldbuffer, currbuffer, width*height);
+
 	if (isOpen) {
 		render16ShadeFrame(currbuffer);
 	}
@@ -162,6 +173,11 @@ DMDDEV void Render_PM_Alphanumeric_Frame(layout_t layout, const UINT16 *const se
 
 DMDDEV void Render_RGB24(UINT16 width, UINT16 height, rgb24 *currbuffer)
 {
+	if (!memcmp(oldbuffer, currbuffer, width*height)) //check if same frame again
+		return;
+
+	memcpy(oldbuffer, currbuffer, width*height);
+
 	if (!isOpen && !useRGB) {
 		tPMoptions Options;
 		Options.dmd_red = 255;
