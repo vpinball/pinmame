d5ff958f2f49e640d418081fd0d0c913bb2b76d5
volkenborn <volkenborn@yahoo.com>
2017-01-31 09:47:57 +0000

    Added LTD games provided by exegeta
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@4200 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/ltdgames.c
+++ src/wpc/ltdgames.c
@@ -2,12 +2,151 @@
 #include "sim.h"
 #include "ltd.h"
 
-#define INITGAME(name, disptype, balls, lamps) \
+// Sistema 3 games below
+
+#define INITGAME(name, disptype, balls, lamps, gs1) \
 	LTD_INPUT_PORTS_START(name, balls) LTD_INPUT_PORTS_END \
-	static core_tGameData name##GameData = {0,disptype,{FLIP_SW(FLIP_L),0,lamps}}; \
+	static core_tGameData name##GameData = {0,disptype,{FLIP_SW(FLIP_L),0,6,0,0,0,gs1}}; \
 	static void init_##name(void) { \
 		core_gameData = &name##GameData; \
 	}
+
+static core_tLCDLayout disp2p5[] = {
+  { 1, 0,14,2,CORE_SEG7 }, { 1, 4,18,2,CORE_SEG7 }, { 1, 8,10,1,CORE_SEG7 },
+  { 1,16,12,2,CORE_SEG7 }, { 1,20,16,2,CORE_SEG7 }, { 1,24,11,1,CORE_SEG7 },
+  { 0,12, 3,1,CORE_SEG7 }, { 2,12, 2,1,CORE_SEG7 },
+  {0}
+};
+
+/*-------------------------------------------------------------------
+/ Arizona
+/-------------------------------------------------------------------*/
+INITGAME(arizona, disp2p5, 1, 5, 0x1f3)
+LTD_1_ROMSTART(arizona, "arizltd.bin", CRC(908f00d8) SHA1(98f28f1aedbad43e0e096959fdef45e038405473))
+LTD_ROMEND
+CORE_GAMEDEFNV(arizona,"Arizona",19??,"LTD",gl_mLTD,GAME_NO_SOUND)
+
+/*-------------------------------------------------------------------
+/ Atlantis
+/-------------------------------------------------------------------*/
+INITGAME(atla_ltd, disp2p5, 1, 5, 0x1f3)
+LTD_2_ROMSTART(atla_ltd, "atlantis.bin", CRC(c61be043) SHA1(e6c4463f59a5743fa34aa55beeb6f536ad9f1b56))
+LTD_ROMEND
+CORE_GAMEDEFNV(atla_ltd,"Atlantis (LTD)",19??,"LTD",gl_mLTD,GAME_NO_SOUND)
+
+/*-------------------------------------------------------------------
+/ Disco Dancing
+/-------------------------------------------------------------------*/
+INITGAME(discodan, disp2p5, 1, 5, 0x1f3)
+LTD_2_ROMSTART(discodan, "disco.bin", CRC(83c79157) SHA1(286fd0c984870639fcd7d7b8f6a5a5ddabcddcf5))
+LTD_ROMEND
+CORE_GAMEDEFNV(discodan,"Disco Dancing",19??,"LTD",gl_mLTD,GAME_NO_SOUND)
+
+/*-------------------------------------------------------------------
+/ Hustler
+/-------------------------------------------------------------------*/
+INITGAME(hustler, disp2p5, 1, 5, 0x1f3)
+LTD_2_ROMSTART(hustler, "hustler_1.bin", CRC(43f323f5) SHA1(086b81699bea08b10b4231e398f4f689395355b0))
+LTD_ROMEND
+CORE_GAMEDEFNV(hustler,"Hustler",19??,"LTD",gl_mLTD,GAME_NO_SOUND)
+
+INITGAME(hustlera, disp2p5, 1, 5, 0x1f3)
+LTD_2_ROMSTART(hustlera, "hustler_2.bin", CRC(83c79157) SHA1(286fd0c984870639fcd7d7b8f6a5a5ddabcddcf5))
+LTD_ROMEND
+CORE_CLONEDEFNV(hustlera,hustler,"Hustler (alternate set)",19??,"LTD",gl_mLTD,GAME_NO_SOUND)
+
+/*-------------------------------------------------------------------
+/ Martian Queen
+/-------------------------------------------------------------------*/
+INITGAME(marqueen, disp2p5, 1, 5, 0x1f3)
+LTD_2_ROMSTART(marqueen, "mqueen.bin", CRC(c80dfdd8) SHA1(b81bf952f4f74c714484e0d1e6af41c155e09c4f))
+LTD_ROMEND
+CORE_GAMEDEFNV(marqueen,"Martian Queen",19??,"LTD",gl_mLTD,GAME_NOT_WORKING)
+
+/*-------------------------------------------------------------------
+/ King Kong
+/-------------------------------------------------------------------*/
+INITGAME(kkongltd, disp2p5, 1, 5, 0x1f3)
+LTD_2_ROMSTART(kkongltd, "kong.bin", CRC(5b2a3123) SHA1(eee417d17d3272ee63c728915af84da33f1f73a2))
+LTD_ROMEND
+CORE_GAMEDEFNV(kkongltd,"King Kong (LTD)",19??,"LTD",gl_mLTD,GAME_NO_SOUND)
+
+/*-------------------------------------------------------------------
+/ Space Poker
+/-------------------------------------------------------------------*/
+INITGAME(spcpoker, disp2p5, 1, 5, 0x1f3)
+LTD_2_ROMSTART(spcpoker, "spoker.bin", CRC(a6ee0369) SHA1(e664a32dc69162e8a7980abf21515887b9bf0cba))
+LTD_ROMEND
+CORE_GAMEDEFNV(spcpoker,"Space Poker",19??,"LTD",gl_mLTD,GAME_NOT_WORKING)
+
+/*-------------------------------------------------------------------
+/ Viking King
+/-------------------------------------------------------------------*/
+INITGAME(vikngkng, disp2p5, 1, 5, 0x1f3)
+LTD_2_ROMSTART(vikngkng, "vikking.bin", CRC(aa32d158) SHA1(b24294ae4ecb2ab3119ad7fe79ef567b19ac792a))
+LTD_ROMEND
+CORE_GAMEDEFNV(vikngkng,"Viking King",19??,"LTD",gl_mLTD,GAME_NO_SOUND)
+
+static core_tLCDLayout disp2p5a[] = {
+  { 1, 0,14,2,CORE_SEG7 }, { 1, 4,18,2,CORE_SEG7 }, { 1, 8,11,1,CORE_SEG7 },
+  { 1,16,12,2,CORE_SEG7 }, { 1,20,16,2,CORE_SEG7 }, { 1,24, 9,1,CORE_SEG7 },
+  { 0,12, 3,1,CORE_SEG7 }, { 2,12, 2,1,CORE_SEG7 },
+  {0}
+};
+
+/*-------------------------------------------------------------------
+/ Force
+/-------------------------------------------------------------------*/
+INITGAME(force, disp2p5a, 1, 6, 0x5f3)
+LTD_2_ROMSTART(force, "forceltd.bin", CRC(48f9ebbe) SHA1(8aaab352fb21263b1b93ffefd9b5169284083beb))
+LTD_ROMEND
+CORE_GAMEDEFNV(force,"Force",19??,"LTD",gl_mLTD,GAME_NO_SOUND)
+
+static core_tLCDLayout disp2p6[] = {
+  { 1, 0,10,2,CORE_SEG7 }, { 1, 4,14,2,CORE_SEG7 }, { 1, 8,18,2,CORE_SEG7 },
+  { 1,18, 8,2,CORE_SEG7 }, { 1,22,12,2,CORE_SEG7 }, { 1,26,16,2,CORE_SEG7 },
+  { 0,14, 3,1,CORE_SEG7 }, { 2,14, 2,1,CORE_SEG7 },
+  {0}
+};
+
+/*-------------------------------------------------------------------
+/ Black Hole
+/-------------------------------------------------------------------*/
+INITGAME(bhol_ltd, disp2p6, 1, 4, 0xf3)
+LTD_2_ROMSTART(bhol_ltd, "blackhol.bin", CRC(9f6ae35e) SHA1(c17bf08a41c6cf93550671b0724c58e8ac302c33))
+LTD_ROMEND
+CORE_GAMEDEFNV(bhol_ltd,"Black Hole (LTD)",19??,"LTD",gl_mLTD,GAME_NO_SOUND)
+
+static core_tLCDLayout disp3p6[] = {
+  { 0, 0,14,6,CORE_SEG7 },
+  { 3, 0, 8,6,CORE_SEG7 },
+  { 6, 0, 2,6,CORE_SEG7 },
+  { 9, 2, 1,1,CORE_SEG7 }, { 9, 8, 0,1,CORE_SEG7 },
+  {0}
+};
+
+/*-------------------------------------------------------------------
+/ Cowboy Eight Ball
+/-------------------------------------------------------------------*/
+INITGAME(cowboy3p, disp3p6, 1, 6, 0)
+LTD_4_ROMSTART(cowboy3p, "cowboy3p.bin", CRC(5afa29af) SHA1(a5ccf5cd17c63d4292222b792535187b1bcfa786))
+LTD_ROMEND
+CORE_GAMEDEFNV(cowboy3p,"Cowboy Eight Ball",1981,"LTD",gl_mLTD,GAME_NO_SOUND)
+
+/*-------------------------------------------------------------------
+/ Zephy
+/-------------------------------------------------------------------*/
+INITGAME(zephy, disp3p6, 2, 6, 0)
+LTD_4_ROMSTART(zephy, "zephy.l2", CRC(8dd11287) SHA1(8133d0c797eb0fdb56d83fc55da91bfc3cddc9e3))
+LTD_ROMEND
+CORE_GAMEDEFNV(zephy,"Zephy",1982,"LTD",gl_mLTD,GAME_NO_SOUND)
+
+INITGAME(zephya, disp3p6, 2, 6, 0)
+LTD_4_ROMSTART(zephya, "zephy1.bin", CRC(ae189c8a) SHA1(c309b436ef94cd5c266c88fe5f222261e083e4eb))
+LTD_ROMEND
+CORE_CLONEDEFNV(zephya,zephy,"Zephy (alternate set)",1982,"LTD",gl_mLTD,GAME_NO_SOUND)
+
+// Sistema 4 games below
 
 #define INITGAME4(name, disptype, balls) \
 	LTD4_INPUT_PORTS_START(name, balls) LTD_INPUT_PORTS_END \
@@ -16,130 +155,98 @@
 		core_gameData = &name##GameData; \
 	}
 
-/*
-O Gaucho 1977
-Samba 1977
-Grand Prix 1978
-Al Capone 1981
-Amazon 1981
-Arizona 1981
-Atlantis 1981
-Black Hole 1981
-Carnaval no Rio 1981
-Cowboy Eight Ball 1981
-Disco Dancing 1981
-Force 1981
-Galaxia 1981
-Haunted Hotel 1981
-Hustler 1981
-King Kong 1981
-Kung Fu 1981
-Mr. & Mrs. Pec-Men 1981
-Martian Queen 1981
-Space Poker 1981
-Time Machine 1981
-Zephy 1981
-Alien Warrior 1982
-Columbia 1982
-Cowboy 2 1982
-Trick Shooter 1982
-Viking King
-*/
-
-/*-------------------------------------------------------------------
-/ Atlantis
-/-------------------------------------------------------------------*/
-core_tLCDLayout atla_disp[] = {
-  { 0, 0,14,2,CORE_SEG7 }, { 0, 4,18,2,CORE_SEG7 }, { 0, 8,10,1,CORE_SEG7 },
-  { 0,12,12,2,CORE_SEG7 }, { 0,16,16,2,CORE_SEG7 }, { 0,20,11,1,CORE_SEG7 },
-  { 3, 8, 2,1,CORE_SEG7 }, { 3,12, 3,1,CORE_SEG7 },
-  { 3,17, 8,1,CORE_SEG7 }, { 3,20, 9,1,CORE_SEG7 },
-  {0}
-};
-INITGAME(atla_ltd, atla_disp, 1, -1)
-LTD_2_ROMSTART(atla_ltd, "atlantis.bin", CRC(c61be043) SHA1(e6c4463f59a5743fa34aa55beeb6f536ad9f1b56))
-LTD_ROMEND
-CORE_GAMEDEFNV(atla_ltd,"Atlantis (LTD)",19??,"LTD",gl_mLTD,GAME_NO_SOUND)
-
-/*-------------------------------------------------------------------
-/ Black Hole
-/-------------------------------------------------------------------*/
-core_tLCDLayout bhol_disp[] = {
-  { 0, 0,10,2,CORE_SEG7 }, { 0, 4,14,2,CORE_SEG7 }, { 0, 8,18,2,CORE_SEG7 },
-  { 0,14, 8,2,CORE_SEG7 }, { 0,18,12,2,CORE_SEG7 }, { 0,22,16,2,CORE_SEG7 },
-  { 3,10, 2,1,CORE_SEG7 }, { 3,14, 3,1,CORE_SEG7 },
-  {0}
-};
-INITGAME(bhol_ltd, bhol_disp, 1, -1)
-LTD_2_ROMSTART(bhol_ltd, "blackhol.bin", CRC(9f6ae35e) SHA1(c17bf08a41c6cf93550671b0724c58e8ac302c33))
-LTD_ROMEND
-CORE_GAMEDEFNV(bhol_ltd,"Black Hole (LTD)",19??,"LTD",gl_mLTD,GAME_NO_SOUND)
-
-/*-------------------------------------------------------------------
-/ Zephy
-/-------------------------------------------------------------------*/
-core_tLCDLayout zephy_disp[] = {
-  { 0, 0,14,6,CORE_SEG7 }, { 3, 0, 8,6,CORE_SEG7 }, { 6, 0, 2,6,CORE_SEG7 },
-  { 9, 2, 1,1,CORE_SEG7 }, { 9, 8, 0,1,CORE_SEG7 },
-  {0}
-};
-INITGAME(zephy, zephy_disp, 1, 6)
-LTD_4_ROMSTART(zephy, "zephy.l2", CRC(8dd11287) SHA1(8133d0c797eb0fdb56d83fc55da91bfc3cddc9e3))
-LTD_ROMEND
-CORE_GAMEDEFNV(zephy,"Zephy",198?,"LTD",gl_mLTD,GAME_NO_SOUND)
-
-/*-------------------------------------------------------------------
-/ Cowboy Eight Ball
-/-------------------------------------------------------------------*/
-core_tLCDLayout cowboy_disp[] = {
-  { 0, 0,25,6,CORE_SEG7 }, { 3, 0,17,6,CORE_SEG7 }, { 6, 0, 9,6,CORE_SEG7 }, { 9, 0, 1,6,CORE_SEG7 },
+static core_tLCDLayout disp4p6[] = {
+  { 0, 0,25,6,CORE_SEG7 },
+  { 3, 0,17,6,CORE_SEG7 },
+  { 6, 0, 9,6,CORE_SEG7 },
+  { 9, 0, 1,6,CORE_SEG7 },
   {12, 2,15,1,CORE_SEG7 }, {12, 8, 0,1,CORE_SEG7 },
   {0}
 };
-INITGAME4(cowboy, cowboy_disp, 1)
+
+/*-------------------------------------------------------------------
+/ Cowboy Eight Ball 2
+/-------------------------------------------------------------------*/
+INITGAME4(cowboy, disp4p6, 1)
 LTD_44_ROMSTART(cowboy, "cowboy_l.bin", CRC(87befe2a) SHA1(93fdf40b10e53d7d95e5dc72923b6be887411fc0),
                         "cowboy_h.bin", CRC(105e5d7b) SHA1(75edeab8c8ba19f334479133802acbc25f405763))
 LTD_ROMEND
-CORE_GAMEDEFNV(cowboy,"Cowboy Eight Ball",198?,"LTD",gl_mLTD4,0)
+CORE_GAMEDEFNV(cowboy,"Cowboy Eight Ball 2",198?,"LTD",gl_mLTD4,0)
 
 /*-------------------------------------------------------------------
-/ Columbia
+/ Haunted Hotel
 /-------------------------------------------------------------------*/
-core_tLCDLayout columbia_disp[] = {
-  { 0, 0,25,6,CORE_SEG7 }, { 3, 0,17,6,CORE_SEG7 }, { 6, 0, 9,6,CORE_SEG7 }, { 9, 0, 1,6,CORE_SEG7 },
-  {12, 2,15,1,CORE_SEG7 }, {12, 8, 0,1,CORE_SEG7 },
-  {0}
-};
-INITGAME4(columbia, columbia_disp, 1)
-LTD_44_ROMSTART(columbia, "columb-d.bin", NO_DUMP,
-                          "columb-e.bin", CRC(013abca0) SHA1(269376af92368d214c3d09ec6d3eb653841666f3))
+INITGAME4(hhotel, disp4p6, 1)
+LTD_44_ROMSTART(hhotel, "hh1.bin", CRC(a107a683) SHA1(5bb79d9a0a6b33f067cdd54942784c67ab557909),
+                        "hh2.bin", CRC(e0c2ebc1) SHA1(131240589162c7b3f44a2bb951945c7d64f89c8d))
 LTD_ROMEND
-CORE_GAMEDEFNV(columbia,"Columbia",198?,"LTD",gl_mLTD4,0)
+CORE_GAMEDEFNV(hhotel,"Haunted Hotel",198?,"LTD",gl_mLTD4HH,0)
 
 /*-------------------------------------------------------------------
 / Mr. & Mrs. Pec-Men
 /-------------------------------------------------------------------*/
-core_tLCDLayout pecmen_disp[] = {
-  { 0, 0,25,6,CORE_SEG7 }, { 3, 0,17,6,CORE_SEG7 }, { 6, 0, 9,6,CORE_SEG7 }, { 9, 0, 1,6,CORE_SEG7 },
-  {12, 2,15,1,CORE_SEG7 }, {12, 8, 0,1,CORE_SEG7 },
-  {0}
-};
-INITGAME4(pecmen, pecmen_disp, 1)
+INITGAME4(pecmen, disp4p6, 1)
 LTD_44_ROMSTART(pecmen, "pecmen_l.bin", CRC(f86c724e) SHA1(635ec94a1c6e77800ef9774102cc639be86c4261),
                         "pecmen_h.bin", CRC(013abca0) SHA1(269376af92368d214c3d09ec6d3eb653841666f3))
 LTD_ROMEND
 CORE_GAMEDEFNV(pecmen,"Mr. & Mrs. Pec-Men",198?,"LTD",gl_mLTD4,0)
 
+static core_tLCDLayout disp4p7[] = {
+  { 0, 0,24,7,CORE_SEG7 },
+  { 3, 0,16,7,CORE_SEG7 },
+  { 6, 0, 8,7,CORE_SEG7 },
+  { 9, 0, 0,7,CORE_SEG7 },
+  {12, 2,15,1,CORE_SEG7 }, {12,10, 7,1,CORE_SEG7 },
+  {0}
+};
+
 /*-------------------------------------------------------------------
 / Al Capone
 /-------------------------------------------------------------------*/
-core_tLCDLayout alcapone_disp[] = {
-  { 0, 0,25,6,CORE_SEG7 }, { 3, 0,17,6,CORE_SEG7 }, { 6, 0, 9,6,CORE_SEG7 }, { 9, 0, 1,6,CORE_SEG7 },
-  {12, 2,15,1,CORE_SEG7 }, {12, 8, 7,1,CORE_SEG7 },
-  {0}
-};
-INITGAME4(alcapone, alcapone_disp, 1)
+INITGAME4(alcapone, disp4p7, 1)
 LTD_44_ROMSTART(alcapone, "alcapo_l.bin", CRC(c4270ba8) SHA1(f3d80af9900c94df2d43f2755341a346a0b64c87),
                           "alcapo_h.bin", CRC(279f766d) SHA1(453c58e44c4ef8f1f9eb752b6163c61ebed70b27))
 LTD_ROMEND
 CORE_GAMEDEFNV(alcapone,"Al Capone",198?,"LTD",gl_mLTD4,0)
+
+/*-------------------------------------------------------------------
+/ Alien Warrior
+/-------------------------------------------------------------------*/
+INITGAME4(alienwar, disp4p7, 1)
+LTD_44_ROMSTART(alienwar, "alwar_l.bin", NO_DUMP,
+                          "alwar_h.bin", NO_DUMP)
+LTD_ROMEND
+CORE_GAMEDEFNV(alienwar,"Alien Warrior",198?,"LTD",gl_mLTD4,0)
+
+/*-------------------------------------------------------------------
+/ Columbia
+/-------------------------------------------------------------------*/
+INITGAME4(columbia, disp4p7, 1)
+LTD_44_ROMSTART(columbia, "columb_l.bin", CRC(ac345dee) SHA1(14f03fa8059de5cd69cc83638aa6533fbcead37e),
+                          "columb_h.bin", CRC(acd2a85b) SHA1(30889ee4230ce05f6060f926b2137bbf5939db2d))
+LTD_ROMEND
+CORE_GAMEDEFNV(columbia,"Columbia",198?,"LTD",gl_mLTD4,0)
+
+/*-------------------------------------------------------------------
+/ Time Machine
+/-------------------------------------------------------------------*/
+INITGAME4(tmacltd4, disp4p7, 1)
+LTD_44_ROMSTART(tmacltd4, "tm4_l.bin", CRC(36655dbe) SHA1(5d5f7f7a4fc01de3ed2d420db0fc264bb31d7196),
+                          "tm4_h.bin", CRC(f5f97992) SHA1(ba31f71a600e7061b500e0750f50643503e52a80))
+LTD_ROMEND
+CORE_GAMEDEFNV(tmacltd4,"Time Machine (4 players)(LTD)",198?,"LTD",gl_mLTD4,GAME_NOT_WORKING)
+
+INITGAME4(tmacltd2, disp4p7, 1)
+LTD_44_ROMSTART(tmacltd2, "tm2_l.bin", NO_DUMP,
+                          "tm2_h.bin", CRC(f717c9db) SHA1(9ca5819b707fa20edfc289734e1aa189ae242aa3))
+LTD_ROMEND
+CORE_CLONEDEFNV(tmacltd2,tmacltd4,"Time Machine (2 players) (LTD)",198?,"LTD",gl_mLTD4,GAME_NOT_WORKING)
+
+/*-------------------------------------------------------------------
+/ Trick Shooter
+/-------------------------------------------------------------------*/
+INITGAME4(tricksht, disp4p7, 1)
+LTD_44_ROMSTART(tricksht, "tricks_l.bin", CRC(951413ff) SHA1(f4a28f7b41cb077377433dc7bfb6647e5d392481),
+                          "tricks_h.bin", CRC(2e4efb51) SHA1(3dd20addecf4b47bd68b05d557c378d1dbbbd892))
+LTD_ROMEND
+CORE_GAMEDEFNV(tricksht,"Trick Shooter",198?,"LTD",gl_mLTD4,0)
