b207541e607bcf3e526441a949df4d9ce91d2aa9
toxie <toxie@ainc.de>
2017-03-07 19:21:12 +0000

    add vc2017 conversion
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@4233 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

---
+++ create_vc2017_from_vc2012.bat
@@ -0,0 +1,42 @@
+copy /V /-Y "PinMAME_VC2012.sln" "PinMAME_VC2017.sln"
+copy /V /-Y "InstallVPinMAME_VC2012.vcxproj" "InstallVPinMAME_VC2017.vcxproj"
+copy /V /-Y "PinMAME_VC2012.vcxproj" "PinMAME_VC2017.vcxproj"
+copy /V /-Y "PinMAME32_VC2012.vcxproj" "PinMAME32_VC2017.vcxproj"
+copy /V /-Y "VPinMAME_VC2012.vcxproj" "VPinMAME_VC2017.vcxproj"
+copy /V /-Y "InstallVPinMAME_VC2012.vcxproj.filters" "InstallVPinMAME_VC2017.vcxproj.filters"
+copy /V /-Y "PinMAME_VC2012.vcxproj.filters" "PinMAME_VC2017.vcxproj.filters"
+copy /V /-Y "PinMAME32_VC2012.vcxproj.filters" "PinMAME32_VC2017.vcxproj.filters"
+copy /V /-Y "VPinMAME_VC2012.vcxproj.filters" "VPinMAME_VC2017.vcxproj.filters"
+
+@cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2017" /in:"PinMAME_VC2017.sln" /out:"PinMAME_VC2017.sln"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"Visual Studio 2012" /replace:"Visual Studio 2017" /in:"PinMAME_VC2017.sln" /out:"PinMAME_VC2017.sln"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"Format Version 12.00" /replace:"Format Version 14.00" /in:"PinMAME_VC2017.sln" /out:"PinMAME_VC2017.sln"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2017" /in:"InstallVPinMAME_VC2017.vcxproj" /out:"InstallVPinMAME_VC2017.vcxproj"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"v110" /replace:"v141_xp" /in:"InstallVPinMAME_VC2017.vcxproj" /out:"InstallVPinMAME_VC2017.vcxproj"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2017" /in:"PinMAME_VC2017.vcxproj" /out:"PinMAME_VC2017.vcxproj"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"v110" /replace:"v141_xp" /in:"PinMAME_VC2017.vcxproj" /out:"PinMAME_VC2017.vcxproj"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2017" /in:"PinMAME32_VC2017.vcxproj" /out:"PinMAME32_VC2017.vcxproj"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"v110" /replace:"v141_xp" /in:"PinMAME32_VC2017.vcxproj" /out:"PinMAME32_VC2017.vcxproj"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2017" /in:"VPinMAME_VC2017.vcxproj" /out:"VPinMAME_VC2017.vcxproj"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"v110" /replace:"v141_xp" /in:"VPinMAME_VC2017.vcxproj" /out:"VPinMAME_VC2017.vcxproj"
+@if errorlevel 1 goto manual
+
+@goto end
+
+:manual
+@echo.
+@echo Replace all occurrences of VC2012 in the project files with VC2017.
+
+:end
+@echo Convert the project files with VC2017 and compile.
+@pause
