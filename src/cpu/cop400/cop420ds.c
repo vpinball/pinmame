48f0fa692b6fa9a48af98aadb39ac69f1908c8c3
toxie <toxie@ainc.de>
2017-03-15 20:38:17 +0000

    port MAME/MESS changes
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@4248 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/cpu/cop400/cop420ds.c
+++ src/cpu/cop400/cop420ds.c
@@ -22,22 +22,24 @@ int DasmCOP420(char *buffer, unsigned pc)
 
 	if ((op >= 0x80 && op <= 0xBE) || (op >= 0xC0 && op <= 0xFE))
 	{
-		if ((pc & 0x3E0) >= 0x80 && (pc & 0x3E0) < 0x100) //JP pages 2,3
+		int page = pc >> 6;
+
+		if (page == 2 || page == 3) //JP pages 2,3
 		{
 			addr = (UINT16)((pc & 0x380) | (op & 0x7F));
-			sprintf(buffer,"JP %03x",addr);
+			sprintf(buffer,"JP %03X",addr);
 		}
 		else
 		{
 			if ((op & 0xC0) == 0xC0) //JP other pages
 			{
 				addr = (UINT16)((pc & 0x3C0) | (op & 0x3F));
-				sprintf(buffer,"JP %03x",addr);
+				sprintf(buffer,"JP %03X",addr);
 			}
 			else					//JSRP
 			{
 				addr = (UINT16)(0x80 | (op & 0x3F));
-				sprintf(buffer,"JSRP %02x",addr);
+				sprintf(buffer,"JSRP %03X",addr);
 //				flags = DASMFLAG_STEP_OVER;
 			}
 		}
@@ -65,13 +67,13 @@ int DasmCOP420(char *buffer, unsigned pc)
 	else if (op >= 0x60 && op <= 0x63)
 	{
 		addr = ((op & 0x03) << 8) | oprom[1];
-		sprintf(buffer,"JMP %03x",addr);
+		sprintf(buffer,"JMP %03X",addr);
 		cnt = 2;
 	}
 	else if (op >= 0x68 && op <= 0x6B)
 	{
 		addr = ((op & 0x03) << 8) | oprom[1];
-		sprintf(buffer,"JSR %03x",addr);
+		sprintf(buffer,"JSR %03X",addr);
 //		flags = DASMFLAG_STEP_OVER;
 		cnt = 2;
 	}
