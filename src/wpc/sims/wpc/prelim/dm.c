e4dab4e4c6fcf6daff30e4b9146c1f0fdeab1248
toxieainc <toxieainc@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2017-05-14 14:35:51 +0000

    add changed DM coin patches
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@4279 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/sims/wpc/prelim/dm.c
+++ src/wpc/sims/wpc/prelim/dm.c
@@ -298,38 +298,38 @@ DCS_SOUNDROM6x("dm_u2_s.l1",CRC(f72dc72e) SHA1(a1267c32f70b4bfe6058d7e28d8200654
                "dm_u7_s.l2",CRC(75066af1) SHA1(4d70bce8a96343afcf02c89240b11faf19e11f02))
 WPC_ROMEND
 
-WPC_ROMSTART(dm,lx3,"dman_lx3.rom",0x80000,CRC(5aa57674) SHA1(e02d91a705799866bd741b998d93413ec5bced25)) DM_SOUND_L2 WPC_ROMEND
-WPC_ROMSTART(dm,dx3,"dman_dx3.rom",0x80000,CRC(aede7ca6) SHA1(874aca191ca7bbee108f2f0204a060966b66623e)) DM_SOUND_L2 WPC_ROMEND
-WPC_ROMSTART(dm,lx4,"dman_lx4.rom",0x80000,CRC(c2d0f493) SHA1(26ee970827dd96f3b3c56aa548cf7629ed6a16c1)) DM_SOUND_L2 WPC_ROMEND
-WPC_ROMSTART(dm,dx4,"dman_dx4.rom",0x80000,CRC(6974789d) SHA1(9cf2c49f879fa1f2547e1e7c853e12dc4a35b97d)) DM_SOUND_L2 WPC_ROMEND
-WPC_ROMSTART(dm,h5, "dman_h5.rom", 0x80000,CRC(bdcc62f7) SHA1(d6f3181970f3f71a876e9a2166156eb8fc405af0)) DM_SOUND_H1 WPC_ROMEND
-WPC_ROMSTART(dm,dh5, "dman_dh5.rom", 0x80000,CRC(91388e13) SHA1(b09a628afd921e34b99d0db3005ab12bbec189b5)) DM_SOUND_H1 WPC_ROMEND
-WPC_ROMSTART(dm,h5b, "dman_h5b.rom", 0x80000,CRC(134aa8ed) SHA1(3fe1e10be2b65c11967dc5fbef64f658db7387fb)) DM_SOUND_H1 WPC_ROMEND
-WPC_ROMSTART(dm,dh5b, "dmandh5b.rom", 0x80000,CRC(e9d4776d) SHA1(fa1b16b284d547b1ab211244f5d40db5cca3d6a6)) DM_SOUND_H1 WPC_ROMEND
-WPC_ROMSTART(dm,h6, "dman_h6.rom", 0x80000,CRC(3a079b80) SHA1(94a7ee94819ec878ced5e07745bf52b6c65e06c9)) DM_SOUND_H1 WPC_ROMEND
-WPC_ROMSTART(dm,h6b, "dman_h6b.rom", 0x80000,CRC(004bde84) SHA1(cf3f44a65e0598bdf257f61939bab4e9c03339a6)) DM_SOUND_H1 WPC_ROMEND
-WPC_ROMSTART(dm,dt099, "dm_dt099.rom", 0x100000,CRC(64428b78) SHA1(ac0b0174dcecd1b1cd467c4369cb78c2430e9e4b)) DM_SOUND_H1 WPC_ROMEND
-WPC_ROMSTART(dm,dt101, "dm_dt101.rom", 0x100000,CRC(ce36910f) SHA1(45902edd6d8ac2bff95d0c7f65e0f80c4c17b8f2)) DM_SOUND_H1 WPC_ROMEND
+WPC_ROMSTART(dm,lx3,  "dman_lx3.rom",0x80000, CRC(5aa57674) SHA1(e02d91a705799866bd741b998d93413ec5bced25)) DM_SOUND_L2 WPC_ROMEND
+WPC_ROMSTART(dm,dx3,  "dman_dx3.rom",0x80000, CRC(aede7ca6) SHA1(874aca191ca7bbee108f2f0204a060966b66623e)) DM_SOUND_L2 WPC_ROMEND
+WPC_ROMSTART(dm,lx4,  "dman_lx4.rom",0x80000, CRC(c2d0f493) SHA1(26ee970827dd96f3b3c56aa548cf7629ed6a16c1)) DM_SOUND_L2 WPC_ROMEND
+WPC_ROMSTART(dm,dx4,  "dman_dx4.rom",0x80000, CRC(6974789d) SHA1(9cf2c49f879fa1f2547e1e7c853e12dc4a35b97d)) DM_SOUND_L2 WPC_ROMEND
+WPC_ROMSTART(dm,h5,   "dman_h5.rom", 0x80000, CRC(bdcc62f7) SHA1(d6f3181970f3f71a876e9a2166156eb8fc405af0)) DM_SOUND_H1 WPC_ROMEND
+WPC_ROMSTART(dm,dh5,  "dman_dh5.rom",0x80000, CRC(91388e13) SHA1(b09a628afd921e34b99d0db3005ab12bbec189b5)) DM_SOUND_H1 WPC_ROMEND
+WPC_ROMSTART(dm,h5b,  "dman_h5b.rom",0x80000, CRC(b7d0e138) SHA1(5533f36f9eab8626bc1240bdea11ae7757db5244)) DM_SOUND_H1 WPC_ROMEND
+WPC_ROMSTART(dm,dh5b, "dmandh5b.rom",0x80000, CRC(e2e77372) SHA1(df1384f57339c077d98bda5e6ad8ca1d38e3e6ce)) DM_SOUND_H1 WPC_ROMEND
+WPC_ROMSTART(dm,h6,   "dman_h6.rom", 0x80000, CRC(3a079b80) SHA1(94a7ee94819ec878ced5e07745bf52b6c65e06c9)) DM_SOUND_H1 WPC_ROMEND
+WPC_ROMSTART(dm,h6b,  "dman_h6b.rom",0x80000, CRC(047711de) SHA1(7a62b5862f7b5414e3eabe75f117866e6921efaf)) DM_SOUND_H1 WPC_ROMEND
+WPC_ROMSTART(dm,dt099,"dm_dt099.rom",0x100000,CRC(64428b78) SHA1(ac0b0174dcecd1b1cd467c4369cb78c2430e9e4b)) DM_SOUND_H1 WPC_ROMEND
+WPC_ROMSTART(dm,dt101,"dm_dt101.rom",0x100000,CRC(ce36910f) SHA1(45902edd6d8ac2bff95d0c7f65e0f80c4c17b8f2)) DM_SOUND_H1 WPC_ROMEND
 
 /*--------------
 /  Game drivers
 /---------------*/
-CORE_GAMEDEF (dm,lx4,    "Demolition Man (LX-4)",1994,"Williams",wpc_mDCSS,0)
-CORE_CLONEDEF(dm,dx4,lx4,"Demolition Man (DX-4 LED Ghost Fix)",1994,"Williams",wpc_mDCSS,0)
-CORE_CLONEDEF(dm,pa2,lx4,"Demolition Man (PA-2)",1994,"Williams",wpc_mDCSS,0)
-CORE_CLONEDEF(dm,pa3,lx4,"Demolition Man (PA-3 LED Ghost Fix)",1994,"Williams",wpc_mDCSS,0)
-CORE_CLONEDEF(dm,px5,lx4,"Demolition Man (PX-5)",1994,"Williams",wpc_mDCSS,0)
-CORE_CLONEDEF(dm,px6,lx4,"Demolition Man (PX-6 LED Ghost Fix)",1994,"Williams",wpc_mDCSS,0)
-CORE_CLONEDEF(dm,la1,lx4,"Demolition Man (LA-1)",1994,"Williams",wpc_mDCSS,0)
-CORE_CLONEDEF(dm,da1,lx4,"Demolition Man (DA-1 LED Ghost Fix)",1994,"Williams",wpc_mDCSS,0)
-CORE_CLONEDEF(dm,lx3,lx4,"Demolition Man (LX-3)",1994,"Williams",wpc_mDCSS,0)
-CORE_CLONEDEF(dm,dx3,lx4,"Demolition Man (DX-3 LED Ghost Fix)",1994,"Williams",wpc_mDCSS,0)
-CORE_CLONEDEF(dm,h5,lx4, "Demolition Man (H-5)", 1995,"Williams",wpc_mDCSS,0)
-CORE_CLONEDEF(dm,dh5,lx4, "Demolition Man (DH-5 LED Ghost Fix)", 1995,"Williams",wpc_mDCSS,0)
-CORE_CLONEDEF(dm,h5b,lx4, "Demolition Man (H-5B Coin Play", 1995,"Williams",wpc_mDCSS,0)
-CORE_CLONEDEF(dm,dh5b,lx4, "Demolition Man (DH-5B Coin Play LED Ghost Fix)", 1995,"Williams",wpc_mDCSS,0)
-CORE_CLONEDEF(dm,h6,lx4, "Demolition Man (H-6)", 1995,"Williams",wpc_mDCSS,0)
-CORE_CLONEDEF(dm,h6b,lx4, "Demolition Man (H-6B Coin Play)", 1995,"Williams",wpc_mDCSS,0)
+CORE_GAMEDEF (dm,lx4,       "Demolition Man (LX-4)",1994,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(dm,dx4,  lx4, "Demolition Man (DX-4 LED Ghost Fix)",1994,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(dm,pa2,  lx4, "Demolition Man (PA-2)",1994,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(dm,pa3,  lx4, "Demolition Man (PA-3 LED Ghost Fix)",1994,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(dm,px5,  lx4, "Demolition Man (PX-5)",1994,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(dm,px6,  lx4, "Demolition Man (PX-6 LED Ghost Fix)",1994,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(dm,la1,  lx4, "Demolition Man (LA-1)",1994,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(dm,da1,  lx4, "Demolition Man (DA-1 LED Ghost Fix)",1994,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(dm,lx3,  lx4, "Demolition Man (LX-3)",1994,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(dm,dx3,  lx4, "Demolition Man (DX-3 LED Ghost Fix)",1994,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(dm,h5,   lx4, "Demolition Man (H-5)", 1995,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(dm,dh5,  lx4, "Demolition Man (DH-5 LED Ghost Fix)", 1995,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(dm,h5b,  lx4, "Demolition Man (H-5B Coin Play)", 1995,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(dm,dh5b, lx4, "Demolition Man (DH-5B Coin Play LED Ghost Fix)", 1995,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(dm,h6,   lx4, "Demolition Man (H-6)", 1995,"Williams",wpc_mDCSS,0)
+CORE_CLONEDEF(dm,h6b,  lx4, "Demolition Man (H-6B Coin Play)", 1995,"Williams",wpc_mDCSS,0)
 CORE_CLONEDEF(dm,dt099,lx4, "Demolition Man (FreeWPC/Demolition Time 0.99)", 2014,"FreeWPC",wpc_mDCSS,0)
 CORE_CLONEDEF(dm,dt101,lx4, "Demolition Man (FreeWPC/Demolition Time 1.01)", 2014,"FreeWPC",wpc_mDCSS,0)
 
@@ -338,12 +338,12 @@ CORE_CLONEDEF(dm,dt101,lx4, "Demolition Man (FreeWPC/Demolition Time 1.01)", 201
 /-----------------------*/
 static sim_tSimData dmSimData = {
   2,    				/* 2 game specific input ports */
-  dm_stateDef,				/* Definition of all states */
-  dm_inportData,			/* Keyboard Entries */
+  dm_stateDef,			/* Definition of all states */
+  dm_inportData,		/* Keyboard Entries */
   { stTrough1, stTrough2, stTrough3, stTrough4, stTrough5, stDrain, stDrain },	/*Position where balls start.. Max 7 Balls Allowed*/
   NULL, 				/* no init */
-  dm_handleBallState,			/*Function to handle ball state changes*/
-  dm_drawStatic,			/*Function to handle mechanical state changes*/
+  dm_handleBallState,	/*Function to handle ball state changes*/
+  dm_drawStatic,		/*Function to handle mechanical state changes*/
   FALSE, 				/* Simulate manual shooter? */
   NULL  				/* Custom key conditions? */
 };
@@ -482,5 +482,3 @@ static int dm_getSol(int solNo) {
     return ((wpc_data[WPC_EXTBOARD1]>>(solNo-CORE_CUSTSOLNO(1)))&0x01);
   return 0;
 }
-
-
