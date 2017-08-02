ddd80cdd8e7a88eb718c1f987f69ce2b6281d485
syllebra <no.author@mail.com>
2017-08-02 16:31:53 +0000

    PinMAMEdll: some code organization
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/branches/dll@4357 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/dll/PinMAMEdll.h
+++ src/dll/PinMAMEdll.h
@@ -11,26 +11,39 @@
 
 extern "C"
 {
-
+	// Setup related functions
+	// -----------------------
 	PINMAMEDLL_API void SetVPMPath(char* path);
 	PINMAMEDLL_API void SetSampleRate(int sampleRate);
 
+	// Game related functions
+	// ----------------------
 	PINMAMEDLL_API int StartThreadedGame(char* gameName, bool showConsole = false);
 	PINMAMEDLL_API void StopThreadedGame(bool locking = true);
 	//PINMAMEDLL_API void KillThreadedGame(char* gameName);
 	PINMAMEDLL_API void ResetGame();
 
+	// Pause related functions
+	// -----------------------
 	PINMAMEDLL_API void Pause();
 	PINMAMEDLL_API void Continue();
 	PINMAMEDLL_API bool IsPaused();
 
+
+	// DMD related functions
+	// ---------------------
 	PINMAMEDLL_API bool NeedsDMDUpdate();
 	PINMAMEDLL_API int GetRawDMDWidth();
 	PINMAMEDLL_API int GetRawDMDHeight();
 	PINMAMEDLL_API int GetRawDMDPixels(unsigned char* buffer);
 
+
+	// Audio related functions
+	// -----------------------
 	PINMAMEDLL_API int GetPendingAudioSamples(float* buffer, int maxNumber);
 
+	// Switch related functions
+	// ------------------------
 	PINMAMEDLL_API bool GetSwitch(int slot);
 	PINMAMEDLL_API void SetSwitch(int slot, bool state);
 }
