d3aea99109c90d27b8413d668f28ca533c6d3b6d
toxie <toxie@ainc.de>
2018-12-09 13:13:13 +0000

    add Mousin' Around Competition MOD and the 2 new PFX MODs
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@4665 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/sims/wpc/full/pz.c
+++ src/wpc/sims/wpc/full/pz.c
@@ -532,6 +532,12 @@ WPCS_SOUNDROM224("pz_u18.l1",CRC(b7fbba98) SHA1(6533a1474dd335419331d37d4a444795
                  "pz_u14.l1",CRC(4d8897ce) SHA1(7a4ac9e849dae93078ddd60adbd34f3930e4cd46))
 WPC_ROMEND
 
+WPC_ROMSTART(pz,f4pfx,"pzonef_4_pfx.rom",0x40000,CRC(d7353c26) SHA1(5ae2708936ab9ebf180cf795a786f4361df82414))
+WPCS_SOUNDROM224("pz_u18.l1",CRC(b7fbba98) SHA1(6533a1474dd335419331d37d4a4447951171412b),
+                 "pz_u15.l1",CRC(168bcc52) SHA1(0bae89278cd24950b2e247bba48eaa636f7b566c),
+                 "pz_u14.l1",CRC(4d8897ce) SHA1(7a4ac9e849dae93078ddd60adbd34f3930e4cd46))
+WPC_ROMEND
+
 WPC_ROMSTART(pz,f5,"pzonef_5.rom",0x40000,CRC(4e344002) SHA1(f6685a3fc56d33c3aa39928208a90be490d5362e))
 WPCS_SOUNDROM224("pz_u18.l1",CRC(b7fbba98) SHA1(6533a1474dd335419331d37d4a4447951171412b),
                  "pz_u15.l1",CRC(168bcc52) SHA1(0bae89278cd24950b2e247bba48eaa636f7b566c),
@@ -581,14 +587,15 @@ WPC_ROMEND
 /* F-4: This is a special revision intended for use with the Fliptronic
 Solid-State flipper system. DO NOT USE this revision on games that
 use conventional flippers, use revision L-3 instead. */
-CORE_GAMEDEF(pz, f4, "Party Zone (F-4 Fliptronic)", 1991, "Bally", wpc_mFliptronS, 0)
-CORE_CLONEDEF(pz,f5,f4,"Party Zone (F-5 LED Ghost Fix)",1991,"Bally",wpc_mFliptronS,0)
-CORE_CLONEDEF(pz,l1,f4,"Party Zone (L-1)",1991,"Bally",wpc_mFliptronS,0)
-CORE_CLONEDEF(pz,d1,f4,"Party Zone (D-1 LED Ghost Fix)",1991,"Bally",wpc_mFliptronS,0)
-CORE_CLONEDEF(pz,l2,f4,"Party Zone (L-2)",1991,"Bally",wpc_mFliptronS,0)
-CORE_CLONEDEF(pz,d2,f4,"Party Zone (D-2 LED Ghost Fix)",1991,"Bally",wpc_mFliptronS,0)
-CORE_CLONEDEF(pz,l3,f4,"Party Zone (L-3)",1991,"Bally",wpc_mFliptronS,0)
-CORE_CLONEDEF(pz,d3,f4,"Party Zone (D-3 LED Ghost Fix)",1991,"Bally",wpc_mFliptronS,0)
+CORE_GAMEDEF(pz, f4, "Party Zone, The (F-4 Fliptronic)", 1991, "Bally", wpc_mFliptronS, 0)
+CORE_CLONEDEF(pz,f4pfx,f4,"Party Zone, The (F-4 Pinball FX)",2018,"Zen Studios",wpc_mFliptronS,0)
+CORE_CLONEDEF(pz,f5,f4,"Party Zone, The (F-5 LED Ghost Fix)",1991,"Bally",wpc_mFliptronS,0)
+CORE_CLONEDEF(pz,l1,f4,"Party Zone, The (L-1)",1991,"Bally",wpc_mFliptronS,0)
+CORE_CLONEDEF(pz,d1,f4,"Party Zone, The (D-1 LED Ghost Fix)",1991,"Bally",wpc_mFliptronS,0)
+CORE_CLONEDEF(pz,l2,f4,"Party Zone, The (L-2)",1991,"Bally",wpc_mFliptronS,0)
+CORE_CLONEDEF(pz,d2,f4,"Party Zone, The (D-2 LED Ghost Fix)",1991,"Bally",wpc_mFliptronS,0)
+CORE_CLONEDEF(pz,l3,f4,"Party Zone, The (L-3)",1991,"Bally",wpc_mFliptronS,0)
+CORE_CLONEDEF(pz,d3,f4,"Party Zone, The (D-3 LED Ghost Fix)",1991,"Bally",wpc_mFliptronS,0)
 
 /*-----------------------
 / Simulation Definitions
