0e1aa6bb1fb5c1c1f80360ee9885a13f60d41302
mjroberts <mjroberts@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2019-05-04 21:30:10 +0000

    Fix the PREDCS_FIRQ_HACK for real (missed file from commit 4736)
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@4738 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/wmssnd.c
+++ src/wpc/wmssnd.c
@@ -21,12 +21,224 @@
  #include "sound/filter.h"
 #endif
 
+
+// The PREDCS_FIRQ_HACK explained and fixed - MJR 5/3/2019
+//
+// It took 16 years for someone to look into this, but someone finally did!
+//
+// The reason for the pitch change observed in the pre-DCS hack (see below) was NOT that
+// the FIRQ interrupt rate changed in the absence of YM2151 music.  The YM2151 emulation
+// kept cranking out timer A interrupts at a steady pace, as it should have.  The problem,
+// rather, was that a certain percentage of the properly generated interrupts were 
+// "missed".
+//
+// What's a missed interrupt?  To understand that, we have to understand how the 6809
+// processes interrupts.  The 6809 handles IRQ and FIRQ lines by latching them at the 
+// start of each instruction cycle.  If IRQ or FIRQ is ON (in hardware, low), AND the 
+// corresponding CC "inhibit interrupt" flag bit is CLEAR, the processor takes the 
+// interrupt.  If a line is ON and the corresponding CC inhibit interrupt flag bit is 
+// SET, the processor ignores the interrupt line status for the duration of this 
+// instruction cycle.  It checks again at the start of the next instruction cycle, 
+// latching the new status of the line.  The latch status is renewed on each instruction
+// cycle.  That means that if an interrupt line turns ON during a section of code where
+// the "inhibit" flag is SET, and then turns OFF again before the "inhibit" flag is 
+// CLEARed, the signal on the line will never cause a CPU interrupt.  The interrupt
+// is missed.  It's as though the electrical IRQ/FIRQ signal never happened.
+//
+// That's *what* was happening.  In the absence of FM music, we missed about 15% of
+// timer A interrupts from the YM2151.  When FM music *was* playing, we missed about 
+// 1% of them.  The slow-down effect was actually occurring ALL THE TIME; it was just
+// so small when FM music was playing that it was practically inaudible and no one
+// noticed.  I only noticed because I instrumented the code and observed the ratio of
+// missed interrupts directly.
+//
+// *Why* it was happening is pretty subtle, but once you see the reason, it becomes
+// clear why the missed interrupt ratio depends on whether or not FM music playing.
+// The problem isn't a core MAME timing bug as SJE supposed; it's an inaccuracy in 
+// the YM2151 emulation, specifically a tiny detail of the original hardware's timing 
+// that wasn't faithfully reproduced in the emulation.  It turns out that the Williams 
+// DCS89 sound board software is written in such a way that it relies on this particular
+// tiny detail.  I think it's arguable that the bug is actually in the Williams DCS89 
+// sound board software, because to modern eyes it looks like a classic race condition. 
+// I'd be willing to believe that the Williams programmer who wrote this code wrote 
+// this part intentionally, based on the known timing of the YM2151 hardware, but I 
+// tend to think not; I tend to think that it was just a bad bit of design that
+// happened to work because of the hardware timing.
+//
+// Let's look at the Williams sound board code in question.  In the Williams code,
+// the main idle loop looks something like this:
+//
+//  do
+//    check for and process any commands received from the main board
+//    process FM command output
+//    reset the YM2115 clock settings
+//  forever
+//
+// That "reset the YM2115 clock settings" part is responsible for all of our woes.
+// I'm not entirely sure why that's even there, but I'm guessing it was for fault 
+// tolerance, to make sure that the YM clock keeps running even if something gets 
+// screwed up elsewhere in the software and we somehow manage to get the YM chip
+// into a wedged state.  The YM chip has some twitchy command timing rules, so maybe
+// they found that commands would sometimes get lost on the wire, and they wanted 
+// to make sure that the clock kept running come hell or high water.  The YM clock
+// is critical to the whole sound system, since it drives the sample output process 
+// for the DAC and HC55516 streams; if it stops, pretty much everything else stops.
+//
+// Drilling down into the "reset" code, we have this 6809 snippet:
+//
+//     ORCC $50    ; IF|II (inhibit FIRQ + IRQ)
+// L0: LDA [2401]  ; YM2151 status register
+//     BMI DF46    ; loop until ready
+//     LDA $14     ; register $14 - clock settings
+//     STA [2400]  ; write YM2151 address
+// L1: LDA [2401]  ; read YM2151 status
+//     BMI DF50    ; loop until ready
+//     LDA $15     ; start timer A, enable timer A IRQ, clear timer A IRQ
+//     STA [2401]  ; write YM2151 data register
+//     ANDCC $AF   ; IF|II (enable FIRQ + IRQ)
+//
+// As you can see, this section of code runs with the CC "inhibit interrupts"
+// flags set.   They're turning off interrupts beacuse it's absolutely required
+// that we do these register writes to the YM chip atomically.  We have to make
+// two writes, one to send the register address and one to send the data byte.
+// If we got interrupted in the middle of this, the interrupt handler could
+// send its own address/data sequence, which would leave the YM chip in a state
+// where it's not ready to receive the second half of our write.  So we have to
+// make sure that we can't get interrupted during this write.  They got this
+// part right via the ORCC $50 at the top, which disables interrupts until
+// further notice.   As discussed above, this means that the processor will 
+// ignore any IRQ or FIRQ electrical signals during this stretch of code.
+//
+// The second-to-last instruction, STA [2401], is where we do the actual clock 
+// reset: that writes $15 to the clock control register on the YM, which does 
+// three things to timer A.  It starts the timer (or simply leaves it running,
+// if it was running already), enables the timer's interrupt signal (so that
+// the timer interrupt occurs whenever the timer rolls over, which is really
+// the whole point), and clears its current interrupt flag (which turns off the
+// IRQ output from the YM chip, and thus the FIRQ input to the 6809, if it was
+// previously on).
+//
+// Now, suppose that YM timer A fires *while this code is running*.  The YM turns
+// on its IRQ output line, which is connected to the 6809's FIRQ input line.  
+// Normally, this would send us directly into the FIRQ interrupt handler code in
+// the Williams software.  But during this little block, interrupts are disabled,
+// so as each instruction starts, the CPU will observe the FIRQ line but will do
+// nothing about it, since it sees that IF (inhibit FIRQ) is set in CC.  When
+// we reach that penultimate instruction, STA [2401], we're telling the YM to
+// clear its Timer A interrupt flag, which in turns makes the YM turn OFF its
+// IRQ output and our FIRQ input.  We then reach the next instruction, where we
+// re-enable interrupts.  At that point, the CPU will be ready to take the FIRQ.
+// But we just turned it off!!!  That interrupt is missed.  Sorry, have to wait
+// for the next one!
+//
+// What are the odds, though, right?  Well, it turns out that they're pretty
+// good - about 15% when no FM commands are being sent.  And it even happens
+// when FM samples AREN'T being sent, but it drops to about 1% probability.
+// The reason for the difference - and why this APPEARED to be related to the
+// FM playback - is pretty simple now that we see how the main loop is laid out. 
+// When FM is idle, the main loop really doesn't have much else to do other 
+// than executing that timer reset code over and over, so we spend A LOT of
+// time there.  When FM is running, the part of the loop that says "process
+// FM command output" takes up a lot more time, so the portion of time we 
+// spend in the clock reset block decreases, and our ratio of missed interrupts
+// drops accordingly.
+//
+// Okay, now that we understand the problem, the fix is clear: we just have to
+// fix the original Williams code!  Um... no.  That defeats the whole purpose
+// of emulation, which is to run the original software exactly as it was
+// written.  Plus it would be a huge amount of work to find the right code
+// section and patch it in all of the different ROMs.  So we want to fix the
+// emulator, not the "emulatee".  More importantly, perhaps, we can see that
+// the emulator is actually at fault here, not the original code, beacuse this
+// problem obviously didn't happen on the real hardware.  People would have 
+// noticed.  So given that it doesn't happen on the hardware, why does it
+// happen in the emulator?  Because the emulator, being an emulator, is an 
+// imperfect imitation of the real thing.  Some of the details aren't perfect
+// replicas.  In this case, the little detail that the emulator gets wrong is
+// the finite amount of time that the STA [2401] operation takes before the
+// change in FIRQ status propagates to the physical pin.  In the emulation, 
+// this occurred instantaneously.  In the real hardware, there's a delay
+// time.  It's not due to the time it takes electrons to go down the wire
+// or anything silly like that; it's due to the internal architecture of
+// the YM2151 chip and the amount of time it takes to process commands.
+//
+// Why does a delay in the IRQ output response matter here?  Because the
+// Williams software clearly depends on there being a delay.  The ROM code
+// enables interrupts on the next instruction after writing the register.  If 
+// we count clock cycles, we see that it takes 3 clocks to perform the ANDCC, 
+// so we can expect the CPU to sample the FIRQ input line again about 2us 
+// after the register write.  As long as the YM doesn't drop the IRQ output
+// signal for 2us or longer after the register write, the 6809 will see any
+// FIRQ signal that came into effect while the "interrupts off" section was
+// running, so we won't lose such FIRQs.  The immediate juxtaposition of the
+// register write and interrupt restore makes it believable that this was
+// actually intentional.  (My friend the chip designer tells me that it was
+// quite common back in the 8-bit days for software engineers to take this
+// kind of cavalier approach to race conditions, because the hardware back
+// then was so preditable about timing and clocks were relatively slow.
+// To modern eyes, accustomed as we are to pipelined architectures with
+// timing that even the designers can't precisely predict, it looks like a
+// blatant race condition bug.  But the 80s were a simpler time.)
+//
+// I can't find anything in the YM data sheet or application guide about the
+// timing of the IRQ output signal change after writing register $14.  But
+// the data sheet does document the amount of time it takes for the chip to
+// finish processing ANY register write: 68 cycles of its 3.58MHz clock, or
+// about 19us.  This is probably the time it takes for the thing to process
+// some big internal shift register, so my bet is that the new registers are
+// all latched at once 68 cycles after a write, and the EFFECTS of those new
+// register settings propagate out to the rest of the chip some number of 
+// clocks after that.  But even if some register update faster than others, 
+// I think we can safely infer from the Williams code that the IRQ change
+// must take much longer than 2us - long enough that there's no risk from
+// little clock wobbles that it'll sometimes be shorter than 2us.  Would
+// a 5X margin be safe enough?  Okay, good, let's call it 10us.  
+//
+// The correct fix, then, is to change the YM2151 emulator code to add a 
+// 10us delay on an IRQ CLEAR change taking effect.  With this change in
+// effect, we can drop the 2400 Hz timer hack.
+//
+// Let's take a moment to look at why that 2400 Hz hack helped in the 
+// place.  The problem is that we're losing a lot of interrupts.  The
+// hack helped by forcing in a bunch of new ones to compensate for the
+// original ones that got lost.  The YM interrupts happen every 178us, or
+// about 5600 Hz, and the 2400 Hz timer was really just a 1200 Hz "set"
+// cycle (as the "clear" half of its cycle had no effect).  Just like
+// the original interrupts, some number of the "fake" interrupts got 
+// lost, for the same reasons.  So the hack effectively added back maybe
+// 850 Hz worth of interrupts.  You can see how the combination of the 
+// real and fake interrupts averaged out to close to the original number 
+// of interrupts that the Williams software was actually written for. 
+//
+// Given that the hack more or less restores the right interrupt count,
+// why bother with something more elaborate?  Well, for one thing, it's
+// just cleaner from a software design perspective to fix the problem at
+// its source, in the YM code.  And it's more faithful to the hardware
+// we're trying to emulate, which is surely a good thing.  But the real
+// advantage of the YM fix is that it gets the interrupt PACING right.
+// The hack got the total interrupt COUNT right, averaged over time,
+// but due to the random nature of the missed interrupts, the timing
+// of the interrupts as wildly erratic.  The hack didn't get rid of
+// the missed interrupts; it just jammed in a bunch of new ones to
+// make up for the missed ones.  The result was that the clock that
+// drove the DAC and HC55516 was inconsistent.  I observed a range of
+// a factor of about 30 in sample clock times to those chips.  That was
+// part of the reason that the HC55516 output sounded so terrible in the
+// past.  (A small part, though, as it turns out; the real culprit there
+// is a whole different story, which you can find more about in the 
+// HC55516 emulation code.)  Fixing the problem in the YM code makes
+// the clock consistent like it was designed to be.
+//
+#define PREDCS_FIRQ_HACK_FIXED
+
 //This awful hack is here to prevent the bug where the speech pitch is too low on pre-dcs games when
 //the YM2151 is not outputing music. In the hardware the YM2151's Timer A is set to control the FIRQ of the sound cpu 6809.
 //The 6809 will output CVSD speech data based on the speed of the FIRQ. The faster the speed, the higher the
 //pitch. For some reason, when the YM2151 is not outputting sound, the FIRQ rate goes down.. Def. some kind of
 //MAME core bug with timing, but I can't find it. I really hope someone can fix this hack someday..SJE 09/17/03
-#define PREDCS_FIRQ_HACK
+#ifndef PREDCS_FIRQ_HACK_FIXED
+#define PREDCS_FIRQ_HACK  // no longer needed! see above
+#endif
 
 #ifndef MIN
 #define MIN(x,y) ((x)<(y)?(x):(y))
