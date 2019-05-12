6b6be1549aa68c98b1a3ecd40e6a7222248c59e0
toxieainc <toxieainc@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2019-05-12 11:19:30 +0000

    was rev 4745: Added MC3417 chip (old HC55516 code) for Bally Vocalizer games
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@4759 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/sound/mc3417.h
+++ src/sound/mc3417.h
@@ -1,48 +1,46 @@
-#ifndef HC55516_H
-#define HC55516_H
+#ifndef MC3417_H
+#define MC3417_H
 #if !defined(__GNUC__) || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || (__GNUC__ >= 4)	// GCC supports "pragma once" correctly since 3.4
 #pragma once
 #endif
 
-#define MAX_HC55516		4
+#define MAX_MC3417		2
 
-struct hc55516_interface
+struct mc3417_interface
 {
 	int num;
-	int volume[MAX_HC55516];
+	int volume[MAX_MC3417];
 };
 
-int hc55516_sh_start(const struct MachineSound *msound);
+int mc3417_sh_start(const struct MachineSound *msound);
 
-#ifdef PINMAME
 /* sets the gain (10000 is normal) */
-void hc55516_set_gain(int num, double gain);
-#endif
+void mc3417_set_gain(int num, double gain);
 
 /* sets the databit (0 or 1) */
-void hc55516_digit_w(int num, int data);
+void mc3417_digit_w(int num, int data);
 
 /* sets the clock state (0 or 1, clocked on the rising edge) */
-void hc55516_clock_w(int num, int state);
+void mc3417_clock_w(int num, int state);
 
 /* clears or sets the clock state */
-void hc55516_clock_clear_w(int num, int data);
-void hc55516_clock_set_w(int num, int data);
+void mc3417_clock_clear_w(int num, int data);
+void mc3417_clock_set_w(int num, int data);
 
 /* clears the clock state and sets the databit */
-void hc55516_digit_clock_clear_w(int num, int data);
+void mc3417_digit_clock_clear_w(int num, int data);
 
-WRITE_HANDLER( hc55516_0_digit_w );
-WRITE_HANDLER( hc55516_0_clock_w );
-WRITE_HANDLER( hc55516_0_clock_clear_w );
-WRITE_HANDLER( hc55516_0_clock_set_w );
-WRITE_HANDLER( hc55516_0_digit_clock_clear_w );
+WRITE_HANDLER( mc3417_0_digit_w );
+WRITE_HANDLER( mc3417_0_clock_w );
+WRITE_HANDLER( mc3417_0_clock_clear_w );
+WRITE_HANDLER( mc3417_0_clock_set_w );
+WRITE_HANDLER( mc3417_0_digit_clock_clear_w );
 
-WRITE_HANDLER( hc55516_1_digit_w );
-WRITE_HANDLER( hc55516_1_clock_w );
-WRITE_HANDLER( hc55516_1_clock_clear_w );
-WRITE_HANDLER( hc55516_1_clock_set_w );
-WRITE_HANDLER( hc55516_1_digit_clock_clear_w );
+WRITE_HANDLER( mc3417_1_digit_w );
+WRITE_HANDLER( mc3417_1_clock_w );
+WRITE_HANDLER( mc3417_1_clock_clear_w );
+WRITE_HANDLER( mc3417_1_clock_set_w );
+WRITE_HANDLER( mc3417_1_digit_clock_clear_w );
 
 
 #endif
