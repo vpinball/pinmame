6df3873e250b15ef2046bcd34c53911da65d9157
djrobx <djrobx@6f51dfdd-75df-4273-8f3b-818b1e6aa8bf>
2019-10-31 03:54:56 +0000

    Add VPMAlias.txt file so users can make custom game names that point to a specific VPM rom.
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@4870 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/win32com/Alias.cpp
+++ src/win32com/Alias.cpp
@@ -3,6 +3,7 @@ extern "C" {
   #include "driver.h"
   #include "audit.h"
 }
+#include <windows.h>
 #include "alias.h"
 
 static const struct tAliasTable { const char* alias; const char* real; } aliasTable[] = {
@@ -48,14 +49,48 @@ static const struct tAliasTable { const char* alias; const char* real; } aliasTa
   { NULL, NULL }
 };
 
+static char alias_from_file[50];
+
 static const char* crcOfGamesNotSupported[] = {
 	NULL
 };
 
 const char* checkGameAlias(const char* aRomName) {
-  for (const tAliasTable* ii = aliasTable; ii->alias; ++ii)
-    if (_stricmp(aRomName, ii->alias) == 0) return ii->real;
-  return aRomName;
+
+	char *ptr;
+	char AliasFilename[MAX_PATH];
+
+#ifndef _WIN64
+	const HINSTANCE hInst = GetModuleHandle("VPinMAME.dll");
+#else
+	const HINSTANCE hInst = GetModuleHandle("VPinMAME64.dll");
+#endif
+	GetModuleFileName(hInst, AliasFilename, MAX_PATH);
+	ptr = strrchr(AliasFilename, '\\');
+	if (ptr != NULL)
+	{
+		strcpy_s(ptr + 1, 13, "vpmalias.txt");
+
+		FILE *f = fopen(AliasFilename, "r");
+
+		if (f != NULL)
+		{
+			char line[128];
+			while (fgets(line, sizeof(line), f) != NULL)
+			{
+				char *token = strtok(line, ", ");
+				if (_stricmp(token, aRomName) == 0)
+				{
+					strcpy_s(alias_from_file, sizeof(alias_from_file), strtok(NULL, " ,\n#;'"));
+					return alias_from_file;
+				}
+			}
+			fclose(f);
+		}
+	}
+	for (const tAliasTable* ii = aliasTable; ii->alias; ++ii)
+		if (_stricmp(aRomName, ii->alias) == 0) return ii->real;
+	return aRomName;
 }
 
 bool checkGameNotSupported(const struct GameDriver* aGameDriver) {
