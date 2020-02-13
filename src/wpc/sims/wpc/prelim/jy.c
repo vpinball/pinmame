27396b3e2001a21a2b433981dbbef9fe99cb5852
toxie <toxie@ainc.de>
2020-02-13 08:33:38 +0000

    fix wrong date
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@4905 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/sims/wpc/prelim/jy.c
+++ src/wpc/sims/wpc/prelim/jy.c
@@ -276,7 +276,7 @@ WPC_ROMEND
 /  Game drivers
 /---------------*/
 CORE_GAMEDEF(jy,12,"Junk Yard (1.2)",1996,"Williams",wpc_m95S,0)
-CORE_CLONEDEF(jy,12c,12,"Junk Yard (1.2C Competition MOD)",2020,"Williams",wpc_m95S,0) //rev. 1.2 patch 41d7
+CORE_CLONEDEF(jy,12c,12,"Junk Yard (1.2C Competition MOD)",2019,"Williams",wpc_m95S,0) //rev. 1.2 patch 41d7
 CORE_CLONEDEF(jy,11,12,"Junk Yard (1.1)",1996,"Williams",wpc_m95S,0)
 CORE_CLONEDEF(jy,03,12,"Junk Yard (0.3 Prototype)",1996,"Williams",wpc_m95S,0)
 
