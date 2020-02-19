428cc28603364be42b5795db5a4e78982abc2ecc
toxie <toxie@ainc.de>
2020-02-19 12:21:54 +0000

    fix DE 128x16 DMD colors/brightness mapping, and fix RAW dumps of GTS3 DMDs
    patch by lucky1
    
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@4915 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/gts3dmd.c
+++ src/wpc/gts3dmd.c
@@ -13,6 +13,10 @@ extern GTS3_DMDlocals GTS3_dmdlocals[2];
 extern int crtc6845_start_addr;
 #endif
 
+static unsigned char lookup[16] = {
+0x0, 0x8, 0x4, 0xc, 0x2, 0xa, 0x6, 0xe,
+0x1, 0x9, 0x5, 0xd, 0x3, 0xb, 0x7, 0xf, };
+
 
 //static int level4_a[9]  = { 0, 1, 5, 5, 5, 5, 5, 5, 15 }; // mapping for 4 color roms, mode a
 //static int level4_b[16] = { 0, 1, 1, 1, 5, 5, 5, 10, 10, 10, 15, 15, 15, 15, 15, 15 }; // mapping for 4 color roms, mode b
@@ -29,6 +33,11 @@ extern UINT8  g_raw_gtswpc_dmd[GTS3DMD_FRAMES_5C*0x200];
 extern UINT32 g_raw_gtswpc_dmdframes;
 #endif
 
+INLINE UINT8 reverse(UINT8 n) {
+  // Reverse the top and bottom nibble then swap them.
+  return (lookup[n & 0x0f] << 4) | lookup[n >> 4];
+}
+
 //DMD #2 Display routine for Strikes N Spares - code is IDENTICAL to the gts3_dmd128x32
 PINMAME_VIDEO_UPDATE(gts3_dmd128x32a) {
   tDMDDot dotCol;
@@ -38,11 +47,18 @@ PINMAME_VIDEO_UPDATE(gts3_dmd128x32a) {
   int *level = GTS3_dmdlocals[0].color_mode == 0 ? level4_a : (GTS3_dmdlocals[0].color_mode == 1 ? level4_b : level5);
 
   memset(dotCol,0,sizeof(tDMDDot));
+#ifdef VPINMAME
+  int i = 0;
+#endif
   for (ii = 0; ii < frames; ii++) {
     for (jj = 1; jj <= 32; jj++) {           // 32 lines
       UINT8 *line = &dotCol[jj][0];
       for (kk = 0; kk < 16; kk++) {      // 16 columns/line
         UINT8 data = *frameData++;
+#ifdef VPINMAME
+        g_raw_gtswpc_dmd[i] = reverse(data);
+        i++;
+#endif
         for (ll = 0; ll < 8; ll++)          // 8 pixels/column
           { (*line++) += (data>>7); data <<= 1; }
       }
@@ -90,32 +106,36 @@ PINMAME_VIDEO_UPDATE(gts3_dmd128x32) {
   /* Drawing is not optimised so just clear everything */
   // !!! if (fullRefresh) fillbitmap(bitmap,Machine->pens[0],NULL);
   memset(dotCol,0,sizeof(tDMDDot));
+#ifdef VPINMAME
+  int i = 0;
+#endif
   for (ii = 0; ii < frames; ii++) {
     for (jj = 1; jj <= 32; jj++) {          // 32 lines
       UINT8 *line = &dotCol[jj][0];
       for (kk = 0; kk < 16; kk++) {         // 16 columns/line
         UINT8 data = *frameData++;
+#ifdef VPINMAME
+        g_raw_gtswpc_dmd[i] = reverse(data);
+        i++;
+#endif
         for (ll = 0; ll < 8; ll++)          // 8 pixels/column
           { (*line++) += (data>>7); data <<= 1; }
       }
     }
   }
 
-#ifdef VPINMAME
-  memcpy(g_raw_gtswpc_dmd, &DMDFrames[0][0], g_raw_gtswpc_dmdframes * 0x200);
-#endif
 
   // detect special case for some otherwise flickering frames
   if (frames == GTS3DMD_FRAMES_4C_a) {
 	  for (ii = 1; ii <= 32; ii++)               // 32 lines
 		  for (jj = 0; jj < 128; jj++) {         // 128 pixels/line
-			  if (dotCol[ii][jj] == 4){
+			  if (dotCol[ii][jj] == 4) {
 				  level = level4_a2;
 				  break;
 			  }
 		  }
   }
-  
+
   for (ii = 1; ii <= 32; ii++)              // 32 lines
     for (jj = 0; jj < 128; jj++) {          // 128 pixels/line
       UINT8 data = dotCol[ii][jj];
