0192f636b1dd7469538a59a10e4c1dcde9655c9c
toxie <toxie@ainc.de>
2020-02-20 22:00:20 +0000

    adapt some conversion bats
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@4923 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- create_vc2019_from_vc2012.bat
+++ create_vc2019_from_vc2012.bat
@@ -7,6 +7,10 @@ copy /V /-Y "InstallVPinMAME_VC2012.vcxproj.filters" "InstallVPinMAME_VC2019.vcx
 copy /V /-Y "PinMAME_VC2012.vcxproj.filters" "PinMAME_VC2019.vcxproj.filters"
 copy /V /-Y "PinMAME32_VC2012.vcxproj.filters" "PinMAME32_VC2019.vcxproj.filters"
 copy /V /-Y "VPinMAME_VC2012.vcxproj.filters" "VPinMAME_VC2019.vcxproj.filters"
+copy /V /-Y "PinMAMEdll_VC2015.vcxproj" "PinMAMEdll_VC2019.vcxproj"
+copy /V /-Y "PinMAMEdll_VC2015.vcxproj.filters" "PinMAMEdll_VC2019.vcxproj.filters"
+copy /V /-Y "PinMAMEdllTest_VC2015.vcxproj" "PinMAMEdllTest_VC2019.vcxproj"
+copy /V /-Y "PinMAMEdllTest_VC2015.vcxproj.filters" "PinMAMEdllTest_VC2019.vcxproj.filters"
 
 @cscript "simplereplace.wsf" //nologo /search:"VC2012" /replace:"VC2019" /in:"PinMAME_VC2019.sln" /out:"PinMAME_VC2019.sln"
 @if errorlevel 1 goto manual
@@ -31,6 +35,16 @@ copy /V /-Y "VPinMAME_VC2012.vcxproj.filters" "VPinMAME_VC2019.vcxproj.filters"
 @cscript "simplereplace.wsf" //nologo /search:"v110" /replace:"v142" /in:"VPinMAME_VC2019.vcxproj" /out:"VPinMAME_VC2019.vcxproj"
 @if errorlevel 1 goto manual
 @cscript "simplereplace.wsf" //nologo /search:"inline=__inline;" /replace:"" /in:"VPinMAME_VC2019.vcxproj" /out:"VPinMAME_VC2019.vcxproj"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"VC2015" /replace:"VC2019" /in:"PinMAMEdll_VC2019.vcxproj" /out:"PinMAMEdll_VC2019.vcxproj"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"v140_xp" /replace:"v142" /in:"PinMAMEdll_VC2019.vcxproj" /out:"PinMAMEdll_VC2019.vcxproj"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"inline=__inline;" /replace:"" /in:"PinMAMEdll_VC2019.vcxproj" /out:"PinMAMEdll_VC2019.vcxproj"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"VC2015" /replace:"VC2019" /in:"PinMAMEdllTest_VC2019.vcxproj" /out:"PinMAMEdllTest_VC2019.vcxproj"
+@if errorlevel 1 goto manual
+@cscript "simplereplace.wsf" //nologo /search:"v140_xp" /replace:"v142" /in:"PinMAMEdllTest_VC2019.vcxproj" /out:"PinMAMEdllTest_VC2019.vcxproj"
 @if errorlevel 1 goto manual
 
 @goto end
