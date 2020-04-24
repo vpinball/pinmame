dead4007f8c0b9ca78710603ac5f32a965baf8b0
toxie <toxie@ainc.de>
2020-04-24 22:00:36 +0000

    add some comments
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@5011 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/driver.c
+++ src/wpc/driver.c
@@ -1293,8 +1293,8 @@ DRIVERNV(princess)      //MPU-100: 08/79 Cosmic Princess
 DRIVERNV(princefp)      //MPU-100: 08/79 Cosmic Princess (Free Play)
 // MPU-200 - Sound Board: SB-300
 DRIVERNV(meteor)        //MPU-200: 09/79 Meteor
-DRIVERNV(meteora)       //MPU-200: ??/?? Meteor (Bugfix)
-DRIVERNV(meteorbf)      //MPU-200: 05/09 Meteor (Bonus Count Fix, 'Fixes a rare bug where the bonus seems to countdown endlessly')
+DRIVERNV(meteora)       //MPU-200: ??/?? Meteor (Bonus Count Official Fix)
+DRIVERNV(meteorbf)      //MPU-200: 05/09 Meteor (Bonus Count Fix)
 DRIVERNV(meteorfp)      //MPU-200: 09/79 Meteor (Free Play)
 DRIVERNV(meteorns)      //MPU-200: 08/11 Meteor (No Background Sound)
 DRIVERNV(meteorb)       //MPU-200: 11/03 Meteor (7-Digit conversion)
