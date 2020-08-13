207af341c02969846be845b17722f4fe40dbdd96
toxie <toxie@ainc.de>
2020-08-13 20:58:01 +0000

    add LW3 3.00 from pinballcode
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@5125 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/degames.c
+++ src/wpc/degames.c
@@ -692,6 +692,15 @@ DE2S_SOUNDROM022(  "lw3u7.dat"  ,CRC(ba845ac3) SHA1(bb50413ace1885870cb3817edae4
 DE_ROMEND
 CORE_CLONEDEF(lw3,200,208,"Lethal Weapon 3 (2.00)",1992,"Data East",de_mDEDMD32S2A_hack,0)
 
+DE_ROMSTARTx0(lw3_300,"LW3CPUU.300",CRC(1ebcf869) SHA1(f708635bd3dedca0979a31de4151a2569e040639))
+DE_DMD32ROM44(     "lw3drom1.300",CRC(38f0ab03) SHA1(6ab549c6388aacedadd5a16e937ec0612475a0ac),
+                   "lw3drom0.300",CRC(af9c066b) SHA1(bc3ba45f3031ffec654172f29c8660ac2e606b68))
+DE2S_SOUNDROM022(  "lw3u7.dat"  ,CRC(ba845ac3) SHA1(bb50413ace1885870cb3817edae478904b0eefb8),
+                   "lw3u17.dat" ,CRC(e34cf2fc) SHA1(417c83ded6637f891c8bb42b32d6898c90a0e5cf),
+                   "lw3u21.dat" ,CRC(82bed051) SHA1(49ddc4190762d9b473fda270e0d6d88a4422d5d7))
+DE_ROMEND
+CORE_CLONEDEF(lw3,300,208,"Lethal Weapon 3 (3.00 unofficial MOD)",2020,"Data East",de_mDEDMD32S2A_hack,0)
+
 /*-------------------------------------------------------------
 / Aaron Spelling - CPU Rev 3 /DMD  Type 2 512K Rom - 64K CPU Rom
 /------------------------------------------------------------*/
