af77816679ae03e9f469947a7d214789ad080502
toxie <toxie@ainc.de>
2020-12-03 07:06:49 +0000

    wire Spellbinder up correctly
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@5234 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/wpc/s7games.c
+++ src/wpc/s7games.c
@@ -320,7 +320,7 @@ S7_ROMEND
 CORE_CLONEDEF(hypbl,l6,l4,"HyperBall (L-6 MOD High score save)",2006,"Jess M. Askey",s7_mS7S,0)
 
 /*----------------------------
-/ Barracora- Sys.7 (Game #510)
+/ Barracora - Sys.7 (Game #510)
 /----------------------------*/
 // Tournament MOD exists: Bara_20.mod.716 & bara_26.mod.716 and/or ic14_l2_multiplier.716
 INITGAMEFULL(barra,s7_dispS7,34,35,0, 0,20,21,22,29,30)
@@ -334,7 +334,7 @@ S7_ROMEND
 CORE_GAMEDEF(barra,l1,"Barracora (L-1)",1981,"Williams",s7_mS7S,0)
 
 /*----------------------------
-/ Varkon- Sys.7 (Game #512)
+/ Varkon - Sys.7 (Game #512)
 /----------------------------*/
 INITGAMEFULL(vrkon,s7_dispS7,40,39,0, 33,41,0,0,0,0)
 S7_ROMSTART8088(vrkon,l1, "ic14.716",   CRC(3baba324) SHA1(522654e0d81458d8b31150dcb0cb53c29b334358),
@@ -349,30 +349,24 @@ CORE_GAMEDEF(vrkon,l1,"Varkon (L-1)",1982,"Williams",s7_mS7S,0)
 /*-------------------------------
 / Spellbinder - Sys.7 - (Game #513) - NOT Released - Developed by Jess Askey see https://github.com/jessaskey/wms_perc
 /-------------------------------*/
-static const core_tLCDLayout dispHypbl[] = {
-  { 0, 0, 1, 7,CORE_SEG87 }, { 0,16, 9, 7,CORE_SEG87 },
-  { 2,10,20, 1,CORE_SEG87 }, { 2,12,28, 1,CORE_SEG87 },
-  { 2,16, 0, 1,CORE_SEG87 }, { 2,18, 8, 1,CORE_SEG87 },
-  { 4, 3,43, 5,CORE_SEG16 }, { 4,13,49, 7,CORE_SEG16 }, {0}
-};
 static core_tGameData splbnGameData = { GEN_S7, dispHypbl, {FLIP_SWNO(33,34), 0, 4}, NULL, {"", {0,0,0,0xf8,0x0f}, {0} } };
-static void init_hypbl(void) { core_gameData = &splbnGameData; }
-#define input_ports_hypbl input_ports_s7
+static void init_splbn(void) { core_gameData = &splbnGameData; }
+#define input_ports_splbn input_ports_s7
 
 S7_ROMSTART000x(splbn,l0, "ic14.532",    CRC(940a817a) SHA1(2583ff6f6b6985d3ac85b4f120ebb002a10b65af),
                           "ic17.532",    CRC(b38fde72) SHA1(17ef3ca354431307b6a79992c50cb2491b8a7631),
                           "ic20.532",    CRC(ff765ebf) SHA1(14d42735291c3f6e112f19bd49a39e00059cf907))
 S67S_SOUNDROMS0(          "sound12.532", CRC(06051e5e) SHA1(f0ab4be812ceaf771829dd549f2a612156102a93))
 S7_ROMEND
-CORE_GAMEDEF(splbn,l0,"Spellbinder (L-0 BETA)",1982,"Williams/JessAskey",s7_mS7S,0)
+CORE_GAMEDEF(splbn,l0,"Spellbinder (L-0 BETA)",1982,"Williams / Jess M. Askey",s7_mS7S,0)
 
 /*----------------------------------
-/Time Fantasy- Sys.7 (Game #515)
+/Time Fantasy - Sys.7 (Game #515)
 /----------------------------------*/
 // MOVED TO tmfnt.c
 
 /*----------------------------
-/ Warlok- Sys.7 (Game #516)
+/ Warlok - Sys.7 (Game #516)
 /----------------------------*/
 INITGAMEFULL(wrlok,s7_dispS7,0,36,0, 13,14,15,20,21,0)
 S7_ROMSTART8088(wrlok,l3, "ic14.716",   CRC(291be241) SHA1(77fffa878f760583ef152a7939867621a61d58dc),
