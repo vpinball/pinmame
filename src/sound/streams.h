0b454fb26d305404d386c7e980f8d839c34752c3
toxie <toxie@ainc.de>
2021-01-12 08:52:25 +0000

    wire up new YM2151 core
    in order to keep up with the CPU core(s) of the machines, this employs a timer (running at sample rate speed) for all machines that need IRQ and/or port callbacks (i.e. NOT GTS3, but only WMS and DE)
    this is especially important for WMS which relies on very exact YM timing behavior
    
    also YM2151_register_port_X_w and YM2151_data_port_X_w does not work at the moment,
    but one should always use YM2151_word_X_w instead
    
    ..also still disabled for now (as for example some DEs run/sound too fast??)
    
    git-svn-id: https://svn.code.sf.net/p/pinmame/code/trunk@5282 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

--- src/sound/streams.h
+++ src/sound/streams.h
@@ -23,6 +23,9 @@ int stream_init_float(const char *name,int default_mixing_level,
 int stream_init_multi(int channels,const char **names,const int *default_mixing_levels,
 		double sample_rate,
 		int param,void (*callback)(int param,INT16 **buffer,int length));
+int stream_init_multi_float(int channels,const char **names,const int *default_mixing_levels,
+		double sample_rate,
+		int param,void (*callback)(int param,INT16 **buffer,int length),int is_float);
 void stream_update(int channel,int min_interval);	/* min_interval is in usec */
 
 void stream_set_sample_rate(int channel, double sample_rate);
