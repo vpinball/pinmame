23c007b45883cf095bf3f4b9c8b712fbbd8947e3
toxie <toxieainc@users.noreply.github.com>
2023-03-17 22:16:39 +0100

    d'oh!

--- src/sound/filter.c
+++ src/sound/filter.c
@@ -10,7 +10,7 @@
  #if (defined(_M_IX86_FP) && _M_IX86_FP >= 2) || defined(__SSE2__) || defined(_M_X64) || defined(_M_AMD64)
   #include <xmmintrin.h>
  #else // Arm Neon
-  #include "../sse2neon.h"
+  #include "../../ext/sse2neon.h"
  #endif
  #if !defined(_MSC_VER) || !defined(_WIN32) || defined(__clang__)
      typedef union __attribute__ ((aligned (16))) Windows__m128
