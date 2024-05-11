0ee7ceb8d79ab1ae1736564f684b0ebb80228d03
toxieainc <toxie@ainc.de>
2024-05-11 21:20:14 +0200

    update from VPX

--- src/wpc/bulb.c
+++ src/wpc/bulb.c
@@ -17,6 +17,8 @@ typedef struct {
 /*-------------------
 /  local variables
 /-------------------*/
+static int initialized = FALSE;
+
 static struct {
   double                      t_to_p[BULB_T_MAX + 1 - 1500];
   double                      p_to_t[512];
@@ -31,20 +33,46 @@ static bulb_tLampCharacteristics bulbs[BULB_MAX] = {
    { 0.000007413493071564570, 0.000001709095572478890, 1.51222718202281000 * 0.00100636573473889440796764003454 }, // #89 Bulb characteristics (13V, 580mA, 7.54W, 75.4lm)
 };
 
+// Linear RGB tint of a blackbody for temperatures ranging from 1500 to 3000K. The values are normalized for a relative luminance of 1.
+// These values were evaluated using Blender's blackbody implementation, then normalized using the standard relative luminance formula (see https://en.wikipedia.org/wiki/Relative_luminance)
+// We use a minimum channel value at 0.000001 to avoid divide by 0 in client application.
+static const float temperatureToTint[3 * 16] = {
+   3.253114f, 0.431191f, 0.000001f,
+   3.074210f, 0.484372f, 0.000001f,
+   2.914679f, 0.531794f, 0.000001f,
+   2.769808f, 0.574859f, 0.000001f,
+   2.643605f, 0.612374f, 0.000001f,
+   2.523686f, 0.645953f, 0.020487f,
+   2.414433f, 0.676211f, 0.042456f,
+   2.316033f, 0.703137f, 0.065485f,
+   2.225598f, 0.727599f, 0.089456f,
+   2.144543f, 0.749200f, 0.114156f,
+   2.070389f, 0.768694f, 0.139412f,
+   1.997974f, 0.787618f, 0.165180f,
+   1.935725f, 0.803465f, 0.191508f,
+   1.876871f, 0.818242f, 0.218429f,
+   1.821461f, 0.832006f, 0.245241f,
+   1.772554f, 0.843853f, 0.271900f 
+};
+
 /*-------------------------------
 /  Initialize all pre-computed LUTs
 /-------------------------------*/
 void bulb_init()
 {
+   if (initialized)
+      return;
+   initialized = TRUE;
+
    // Compute filament temperature to visible emission power LUT, normalized by visible emission power at T=2700K, according 
    // to the formula from "Luminous radiation from a black body and the mechanical equivalentt of light" by W.W.Coblentz and W.B.Emerson
-   for (int i=0; i<= BULB_T_MAX - 1500; i++)
+   for (int i=0; i <= BULB_T_MAX - 1500; i++)
    {
       double T = 1500.0 + i;
       locals.t_to_p[i] = 1.247/pow(1.0+129.05/T, 204.0) + 0.0678/pow(1.0+78.85/T, 404.0) + 0.0489/pow(1.0+23.52/T, 1004.0) + 0.0406/pow(1.0+13.67/T, 2004.0);
    }
    double P2700 = locals.t_to_p[2700 - 1500];
-   for (int i=0; i<= BULB_T_MAX - 1500; i++)
+   for (int i=0; i <= BULB_T_MAX - 1500; i++)
    {
       locals.t_to_p[i] /= P2700;
    }
@@ -61,7 +89,7 @@ void bulb_init()
    for (int i=0; i <= BULB_T_MAX; i++)
    {
       double T = i;
-      // Compute Tungsten specific heat (energy to temperature transfer, depending on temperature) according to forumla from "Heating-times of tungsten filament incandescent lamps" by Dulli Chandra Agrawal
+      // Compute Tungsten specific heat (energy to temperature transfer, depending on temperature) according to formula from "Heating-times of tungsten filament incandescent lamps" by Dulli Chandra Agrawal
       locals.specific_heat[i] = 3.0 * 45.2268 * (1.0 - 310.0 * 310.0 / (20.0 * T*T)) + (2.0 * 0.0045549 * T) + (4 * 0.000000000577874 * T*T*T);
       // Compute cooldown and heat up factor for the predefined bulbs
       for (int j=0; j<BULB_MAX; j++)
@@ -83,10 +111,40 @@ float bulb_filament_temperature_to_emission(const float T)
    if (T >= (float)BULB_T_MAX) return (float)locals.t_to_p[BULB_T_MAX - 1500];
    return (float)locals.t_to_p[(int)T - 1500];
    // Linear interpolation is not worth its cost
-   // int lower_T = (int) T, upper_T = (int) (T + 0.5);
-   // double alpha = T - lower_T;
-   // return (1.0 - alpha) * locals.t_to_p[lower_T - 1500] + alpha * locals.t_to_p[upper_T - 1500];
+   // int lower_T = (int) T, upper_T = lower_T+1;
+   // float alpha = T - (float)lower_T;
+   // return (1.0f - alpha) * (float)locals.t_to_p[lower_T - 1500] + alpha * (float)locals.t_to_p[upper_T - 1500];
 }
+
+/*-------------------------------
+// Returns linear RGB tint for a given filament temperature. The values are normalized for a perceived luminance of 1.
+/-------------------------------*/
+void bulb_filament_temperature_to_tint(const float T, float* linear_RGB)
+{
+   if (T < 1500.0f)
+   {
+      linear_RGB[0] = temperatureToTint[0];
+      linear_RGB[1] = temperatureToTint[1];
+      linear_RGB[2] = temperatureToTint[2];
+   }
+   else if (T >= 2999.0f)
+   {
+      linear_RGB[0] = temperatureToTint[15 * 3 + 0];
+      linear_RGB[1] = temperatureToTint[15 * 3 + 1];
+      linear_RGB[2] = temperatureToTint[15 * 3 + 2];
+   }
+   else
+   {
+      // Linear interpolation between the precomputed values
+      float t_ref = (T - 1500.0f) * (float)(1./100.);
+      int lower_T = (int)t_ref, upper_T = lower_T+1;
+      float alpha = t_ref - (float)lower_T;
+      linear_RGB[0] = (1.0f - alpha) * temperatureToTint[lower_T * 3 + 0] + alpha * temperatureToTint[upper_T * 3 + 0];
+      linear_RGB[1] = (1.0f - alpha) * temperatureToTint[lower_T * 3 + 1] + alpha * temperatureToTint[upper_T * 3 + 1];
+      linear_RGB[2] = (1.0f - alpha) * temperatureToTint[lower_T * 3 + 2] + alpha * temperatureToTint[upper_T * 3 + 2];
+   }
+}
+
 
 /*-------------------------------
 /  Returns filament temperature for a given visible emission power normalized for an emission power of 1.0 at 2700K
@@ -108,11 +166,11 @@ double bulb_cool_down_factor(const int bulb, const double T)
 /*-------------------------------
 /  Compute cool down factor of a filament over a given period
 /-------------------------------*/
-double bulb_cool_down(const int bulb, double T, double duration)
+double bulb_cool_down(const int bulb, double T, float duration)
 {
-   while (duration > 0.0)
+   while (duration > 0.0f)
    {
-      double dt = duration > 0.001 ? 0.001 : duration;
+      float dt = duration > 0.001f ? 0.001f : duration;
       T += dt * bulbs[bulb].cool_down[(int) T];
       if (T <= 294.0)
       {
@@ -141,14 +199,14 @@ float bulb_heat_up_factor(const int bulb, const float T, const float U, const fl
 /*-------------------------------
 /  Compute temperature of a filament under a given voltage over a given period (sum of heating and cooldown)
 /-------------------------------*/
-double bulb_heat_up(const int bulb, double T, double duration, const double U, const double serial_R)
+double bulb_heat_up(const int bulb, double T, float duration, const float U, const float serial_R)
 {
-   while (duration > 0.0)
+   while (duration > 0.0f)
    {
       T = T < 293.0 ? 293.0 : T > BULB_T_MAX ? BULB_T_MAX : T; // Keeps T within the range of the LUT (between room temperature and melt down point)
       double energy;
       double U1 = U;
-      if (serial_R)
+      if (serial_R != 0.f)
       {
          const double R = bulbs[bulb].r0 * pow(T, 1.215);
          U1 *= R / (R + serial_R);
@@ -159,16 +217,16 @@ double bulb_heat_up(const int bulb, double T, double duration, const double U, c
          // Stable state reached since electric heat (roughly) equals radiation cool down
          return T;
       }
-      double dt;
+      float dt;
       if (energy > 1000e3)
       {
          // Initial current surge, 0.5ms integration period in order to account for the fast resistor rise that will quickly lower the current
-         dt = duration > 0.0005 ? 0.0005 : duration;
+         dt = duration > 0.0005f ? 0.0005f : duration;
       }
       else
       {
          // Ramping up, 1ms integration period
-         dt = duration > 0.001 ? 0.001 : duration;
+         dt = duration > 0.001f ? 0.001f : duration;
       }
       T += dt * energy;
       duration -= dt;
--- src/wpc/bulb.h
+++ src/wpc/bulb.h
@@ -15,10 +15,11 @@
 
 extern void bulb_init();
 extern float bulb_filament_temperature_to_emission(const float T);
+extern void bulb_filament_temperature_to_tint(const float T, float* linear_RGB);
 extern double bulb_emission_to_filament_temperature(const double p);
 extern double bulb_cool_down_factor(const int bulb, const double T);
-extern double bulb_cool_down(const int bulb, double T, double duration);
+extern double bulb_cool_down(const int bulb, double T, float duration);
 extern float bulb_heat_up_factor(const int bulb, const float T, const float U, const float serial_R);
-extern double bulb_heat_up(const int bulb, double T, double duration, const double U, const double serial_R);
+extern double bulb_heat_up(const int bulb, double T, float duration, const float U, const float serial_R);
 
 #endif /* INC_BULB */
