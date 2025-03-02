ce04827c6c0f5ed90f8358dc596823d73ff5d618
Jarr3 <Richard.Ludwig+gh@gmail.com>
2025-03-02 15:02:19 +0100

    Add new COM Version method including build number as double #249

--- src/win32com/Controller.h
+++ src/win32com/Controller.h
@@ -205,6 +205,7 @@ public:
 	STDMETHOD(put_ModOutputType)(/*[in]*/ int output, /*[in]*/ int no, /*[in]*/ int newVal);
 
 	STDMETHOD(put_TimeFence)(/*[in]*/ double fenceIns);
+	STDMETHOD(get_PMBuildVersion)(/*[out, retval]*/ double *pVal);
 };
 
 #endif // !defined(AFX_Controller_H__D2811491_40D6_4656_9AA7_8FF85FD63543__INCLUDED_)
