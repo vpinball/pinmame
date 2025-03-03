982a68d78e1ae131b1d286e10cb98cbf3b61e82c
Jarr3 <Richard.Ludwig+gh@gmail.com>
2025-03-03 19:47:27 +0100

    #214 Easier to upgrade: Rename VPMAlias.txt

--- src/win32com/Alias.cpp
+++ src/win32com/Alias.cpp
@@ -22,46 +22,8 @@ extern "C" {
 #include "Alias.h"
 
 static const struct tAliasTable { const char* alias; const char* real; } aliasTable[] = {
-  { "eshak_f1", "esha_pr4" },
-  { "eshak_l1", "esha_la1" },
-  { "eshak_l3", "esha_la3" },
-  { "lwar", "lwar_a83" },
-  { "ssvc", "ssvc_a26" },
-  { "torpe", "torp_e21" },
-  { "tmach", "tmac_a24" },
-  { "play", "play_a24" },
-  { "maverick", "mav_401" },
-  { "mnfb", "mnfb_c27" },
-  { "robo", "robo_a34" },
-  { "poto", "poto_a32" },
-  { "bttf", "bttf_a20" },
-  { "simp", "simp_a27" },
-  { "chkpnt", "ckpt_a17" },
-  { "tmnt", "tmnt_104" },
-  { "batmn", "btmn_101" },
-  { "trek", "trek_201" },
-  { "hook", "hook_408" },
-  { "lw3", "lw3_208" },
-  { "stwarde", "stwr_103" },
-  { "rab", "rab_130" },
-  { "jurpark", "jupk_513" },
-  { "lah", "lah_112" },
-  { "tftc", "tftc_303" },
-  { "tommy", "tomy_400" },
-  { "wwfrumb", "wwfr_103" },
-  { "gnr", "gnr_300" },
-  { "twister", "twst_405" },
-  { "southpk", "sprk_103" },
-  { "truckstp", "trucksp2" },
-  { "harley", "harl_a10" },
-  { "swtril", "swtril43" },
-  { "jplstwld", "jplstw22" },
-  { "bnzai_p1", "bnzai_pa" },
-  { "gs_l4", "gs_lu4" },
-  { "gs_l3", "gs_la3" },
-  { "kpv106", "kpb105" },
-  { "mousn_lx", "mousn_l4" },
-  { NULL, NULL }
+/*VPMALIAS*/
+	  { NULL, NULL }
 };
 
 static char alias_from_file[50];
