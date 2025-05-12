vpinball/pinmame

commit c50171d22271a7233f173f7de17d55c8c914199a

proc: sync proc codebase with https://github.com/tomlogic/pinmame
git-svn-id: https://svn.code.sf.net/p/pinmame/code/branches/proc@3724 6f51dfdd-75df-4273-8f3b-818b1e6aa8bf

src/unix/config.c
@@ -70,6 +70,32 @@ static int add_device(struct rc_option *option, const char *arg, int priority);
 #endif
 
 /* struct definitions */
+#ifdef PINMAME
+struct rc_option pinmame_opts[] = {
+	/* PinMAME options */
+	{ "PinMAME options", NULL, rc_seperator, NULL, NULL, 0, 0, NULL, NULL },
+#ifdef VPINMAME
+	{ "dmd_red",    NULL, rc_int, &pmoptions.dmd_red,   "255", 0, 255, NULL, "DMD color: Red" },
+	{ "dmd_green",  NULL, rc_int, &pmoptions.dmd_green, "88", 0, 255, NULL, "DMD color: Green" },
+#else
+	{ "dmd_red",    NULL, rc_int, &pmoptions.dmd_red,   "225", 0, 255, NULL, "DMD color: Red" },
+	{ "dmd_green",  NULL, rc_int, &pmoptions.dmd_green, "224", 0, 255, NULL, "DMD color: Green" },
+#endif
+	{ "dmd_blue",   NULL, rc_int, &pmoptions.dmd_blue,   "32", 0, 255, NULL, "DMD color: Blue" },
+	{ "dmd_perc0",	NULL, rc_int, &pmoptions.dmd_perc0,  "20", 0, 100, NULL, "DMD off intensity [%]" },
+	{ "dmd_perc33",	NULL, rc_int, &pmoptions.dmd_perc33,  "33", 0, 100, NULL, "DMD low intensity [%]" },
+	{ "dmd_perc66", NULL, rc_int, &pmoptions.dmd_perc66,  "67", 0, 100, NULL, "DMD medium intensity [%]" },
+	{ "dmd_only",	NULL, rc_bool,&pmoptions.dmd_only,    "0",  0, 0,   NULL, "Show only DMD" },
+	{ "dmd_compact",NULL, rc_bool,&pmoptions.dmd_compact, "0",  0, 0,   NULL, "Show compact display" },
+	{ "dmd_antialias",NULL, rc_int,&pmoptions.dmd_antialias,  "50", 0, 100, NULL, "DMD antialias intensity [%]" },
+#ifdef PROC_SUPPORT
+	{ "alpha_on_dmd",NULL, rc_bool,&pmoptions.alpha_on_dmd, "0",  0, 0, NULL, "Emulate alphanumeric display on DMD" },
+	{ "p-roc",NULL, rc_string,&pmoptions.p_roc, "None",  0, 0, NULL, "YAML Machine description file" },
+	{ "virtual_dmd", NULL, rc_bool,&pmoptions.virtual_dmd,  "1",  0, 0, NULL, "Enable DMD emulation" },
+#endif
+	{ NULL,	NULL, rc_end, NULL, NULL, 0, 0,	NULL, NULL }
+};
+#endif /* PINMAME */
 static struct rc_option opts[] = {
    /* name, shortname, type, dest, deflt, min, max, func, help */
 	{ NULL, NULL, rc_link, video_opts, NULL, 0, 0, NULL, NULL },
@@ -77,6 +103,9 @@ static struct rc_option opts[] = {
 	{ NULL, NULL, rc_link, input_opts, NULL, 0, 0, NULL, NULL },
 	{ NULL, NULL, rc_link, network_opts, NULL, 0, 0, NULL, NULL },
 	{ NULL, NULL, rc_link, fileio_opts, NULL, 0, 0, NULL, NULL },
+#ifdef PINMAME
+	{ NULL, NULL, rc_link, pinmame_opts, NULL, 0,	0, NULL, NULL },
+#endif /* PINMAME */
 #ifdef MESS
 	/* FIXME - these option->names should NOT be hardcoded! */
 	{ "MESS specific options", NULL, rc_seperator, NULL, NULL, 0, 0, NULL, NULL },
@@ -509,7 +538,7 @@ int config_init (int argc, char *argv[])
 				/* game is a clone */
 				if (drivers[i]->clone_of != 0 && !(drivers[i]->clone_of->flags & NOT_A_DRIVER))
 				{
-					if ((!(drivers[game_index]->flags & GAME_NOT_WORKING)) || (drivers[i]->flags & GAME_NOT_WORKING))
+					if ((!drivers[game_index]->flags & GAME_NOT_WORKING) || (drivers[i]->flags & GAME_NOT_WORKING))
 						continue;
 				}
 				else continue;

src/windows/config.c
@@ -77,8 +77,9 @@ struct rc_option pinmame_opts[] = {
         { "dmd_blue0", NULL, rc_int, &pmoptions.dmd_blue0, "0", 0, 0, NULL, "Colorized DMD: 0%: Blue" },
 #ifdef PROC_SUPPORT
 // TODO/PROC: Correct implemenatation?
-        { "p-roc", NULL, rc_string, &pmoptions.p_roc, "None",  0, 0, NULL, "YAML Machine description file" },
-        { "alpha_on_dmd", NULL, rc_bool, &pmoptions.alpha_on_dmd, "0",  0, 0, NULL, "Emulate alphanumeric display on DMD" },
+	{ "p-roc", NULL, rc_string, &pmoptions.p_roc, "None",  0, 0, NULL, "YAML Machine description file" },
+	{ "alpha_on_dmd", NULL, rc_bool, &pmoptions.alpha_on_dmd, "0",  0, 0, NULL, "Emulate alphanumeric display on DMD" },
+	{ "virtual_dmd",  NULL, rc_bool, &pmoptions.virtual_dmd,  "1",  0, 0, NULL, "Enable DMD emulation" },
 #endif /* PROC_SUPPORT */
         { NULL, NULL, rc_end, NULL, NULL, 0, 0, NULL, NULL }
 };

PinMAME_VC2008.vcproj
@@ -75,7 +75,7 @@
 				OutputFile="$(OutDir)\$(ProjectName)_VC2008.exe"
 				LinkIncremental="0"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories="ext\zlib\lib_vc9;ext\pinproc\lib_vc6;ext\yaml-cpp\lib_vc2005"
+				AdditionalLibraryDirectories="ext\zlib\lib_vc9;ext\pinproc\lib_vc9;ext\yaml-cpp\lib_vc2008"
 				DelayLoadDLLs="ftd2xx.dll;pinproc.dll"
 				ProgramDatabaseFile="$(OutDir)\$(TargetName).pdb"
 				SubSystem="1"
@@ -164,7 +164,7 @@
 				OutputFile="$(OutDir)\$(ProjectName)_VC2008vcd.exe"
 				LinkIncremental="2"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories="ext\zlib\lib_vc9;ext\pinproc\lib_vc6;ext\yaml-cpp\lib_vc2005"
+				AdditionalLibraryDirectories="ext\zlib\lib_vc9;ext\pinproc\lib_vc9;ext\yaml-cpp\lib_vc2008"
 				DelayLoadDLLs="ftd2xx.dll;pinproc.dll"
 				GenerateDebugInformation="true"
 				ProgramDatabaseFile="$(OutDir)\$(TargetName).pdb"
@@ -255,7 +255,7 @@
 				OutputFile="$(OutDir)\$(ProjectName)_VC2008md.exe"
 				LinkIncremental="0"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories="ext\zlib\lib_vc9;ext\pinproc\lib_vc6;ext\yaml-cpp\lib_vc2005"
+				AdditionalLibraryDirectories="ext\zlib\lib_vc9;ext\pinproc\lib_vc9;ext\yaml-cpp\lib_vc2008"
 				DelayLoadDLLs="ftd2xx.dll;pinproc.dll"
 				ProgramDatabaseFile="$(OutDir)\$(TargetName).pdb"
 				SubSystem="1"
@@ -344,7 +344,7 @@
 				OutputFile="$(OutDir)\$(ProjectName)_VC2008vcmd.exe"
 				LinkIncremental="2"
 				SuppressStartupBanner="true"
-				AdditionalLibraryDirectories="ext\zlib\lib_vc9;ext\pinproc\lib_vc6;ext\yaml-cpp\lib_vc2005"
+				AdditionalLibraryDirectories="ext\zlib\lib_vc9;ext\pinproc\lib_vc9;ext\yaml-cpp\lib_vc2008"
 				DelayLoadDLLs="ftd2xx.dll;pinproc.dll"
 				GenerateDebugInformation="true"
 				ProgramDatabaseFile="$(OutDir)\$(TargetName).pdb"

ext/pinproc/include/p-roc/pinproc.h
@@ -111,6 +111,7 @@ typedef enum PRMachineType {
     kPRMachineWPC95 = 4,
     kPRMachineSternWhitestar = 5,
     kPRMachineSternSAM = 6,
+    kPRMachinePDB = 7,
 } PRMachineType;
 
 // PRHandle Creation and Deletion
@@ -203,6 +204,7 @@ typedef struct PRDriverState {
     uint8_t patterOnTime;
     uint8_t patterOffTime;
     bool_t patterEnable;
+    bool_t futureEnable;
 } PRDriverState;
 
 typedef struct PRDriverAuxCommand {
@@ -256,6 +258,11 @@ PINPROC_API PRResult PRDriverDisable(PRHandle handle, uint8_t driverNum);
  */
 PINPROC_API PRResult PRDriverPulse(PRHandle handle, uint8_t driverNum, uint8_t milliseconds);
 /**
+ * Pulses the given driver for a number of milliseconds when the hardware reaches a specific timestamp.
+ * This function is provided for convenience.  See PRDriverStatePulse() for a full description.
+ */
+PINPROC_API PRResult PRDriverFuturePulse(PRHandle handle, uint8_t driverNum, uint8_t milliseconds, uint32_t futureTime);
+/**
  * Assigns a repeating schedule to the given driver.
  * This function is provided for convenience.  See PRDriverStateSchedule() for a full description.
  */
@@ -264,17 +271,12 @@ PINPROC_API PRResult PRDriverSchedule(PRHandle handle, uint8_t driverNum, uint32
  * Assigns a pitter-patter schedule (repeating on/off) to the given driver.
  * This function is provided for convenience.  See PRDriverStatePatter() for a full description.
  */
-PINPROC_API PRResult PRDriverPatter(PRHandle handle, uint8_t driverNum, uint8_t millisecondsOn, uint8_t millisecondsOff, uint8_t originalOnTime);
+PINPROC_API PRResult PRDriverPatter(PRHandle handle, uint8_t driverNum, uint8_t millisecondsOn, uint8_t millisecondsOff, uint8_t originalOnTime, bool_t now);
 /**
  * Assigns a pitter-patter schedule (repeating on/off) to the given driver on for the given duration.
  * This function is provided for convenience.  See PRDriverStatePulsedPatter() for a full description.
  */
-PINPROC_API PRResult PRDriverPulsedPatter(PRHandle handle, uint8_t driverNum, uint8_t millisecondsOn, uint8_t millisecondsOff, uint8_t originalOnTime);
-/**
- * Assigns a pitter-patter schedule (repeating on/off) to the given driver for the given duration.
- * This function is provided for convenience.  See PRDriverStatePatter() for a full description.
- */
-PINPROC_API PRResult PRDriverPulsedPatter(PRHandle handle, uint8_t driverNum, uint8_t millisecondsOn, uint8_t millisecondsOff, uint8_t originalOnTime);
+PINPROC_API PRResult PRDriverPulsedPatter(PRHandle handle, uint8_t driverNum, uint8_t millisecondsOn, uint8_t millisecondsOff, uint8_t originalOnTime, bool_t now);
 /**
  * Prepares an Aux Command to drive the Aux bus.
  * This function is provided for convenience.
@@ -316,6 +318,13 @@ PINPROC_API void PRDriverStateDisable(PRDriverState *driverState);
  */
 PINPROC_API void PRDriverStatePulse(PRDriverState *driverState, uint8_t milliseconds);
 /**
+ * Changes the given #PRDriverState to reflect a future scheduled pulse state.
+ * @param milliseconds Number of milliseconds to pulse the driver for.
+ * @param futureTime Value indicating at which HW timestamp the pulse should occur.  Currently only the low 10-bits are used.
+ * @note The driver state structure must be applied using PRDriverUpdateState() or linked to a switch rule using PRSwitchUpdateRule() to have any effect.
+ */
+PINPROC_API void PRDriverStateFuturePulse(PRDriverState *driverState, uint8_t milliseconds, uint32_t futureTime);
+/**
  * Changes the given #PRDriverState to reflect a scheduled state.
  * Assigns a repeating schedule to the given driver.
  * @note The driver state structure must be applied using PRDriverUpdateState() or linked to a switch rule using PRSwitchUpdateRule() to have any effect.
@@ -328,14 +337,14 @@ PINPROC_API void PRDriverStateSchedule(PRDriverState *driverState, uint32_t sche
  *
  * Use originalOnTime to pulse the driver for a number of milliseconds before the pitter-patter schedule begins.
  */
-PINPROC_API void PRDriverStatePatter(PRDriverState *driverState, uint8_t millisecondsOn, uint8_t millisecondsOff, uint8_t originalOnTime);
+PINPROC_API void PRDriverStatePatter(PRDriverState *driverState, uint8_t millisecondsOn, uint8_t millisecondsOff, uint8_t originalOnTime, bool_t now);
 
 /**
  * @brief Changes the given #PRDriverState to reflect a pitter-patter schedule state.
  * Just like the regular Patter above, but PulsePatter only drives the patter
  * scheduled for the given number of milliseconds before disabling the driver.
  */
-PINPROC_API void PRDriverStatePulsedPatter(PRDriverState *driverState, uint8_t millisecondsOn, uint8_t millisecondsOff, uint8_t patterTime);
+PINPROC_API void PRDriverStatePulsedPatter(PRDriverState *driverState, uint8_t millisecondsOn, uint8_t millisecondsOff, uint8_t patterTime, bool_t now);
 
 /**
  * Write Aux Port commands into the Aux Port command memory.
@@ -383,9 +392,9 @@ PINPROC_API int PRGetEvents(PRHandle handle, PREvent *eventsOut, int maxEvents);
 
 
 #define kPRSwitchPhysicalFirst (0)   /**< Switch number of the first physical switch. */
-#define kPRSwitchPhysicalLast (223)  /**< Switch number of the last physical switch.  */
-#define kPRSwitchVirtualFirst (224)  /**< Switch number of the first virtual switch.  */
-#define kPRSwitchVirtualLast (255)   /**< Switch number of the last virtual switch.   */
+#define kPRSwitchPhysicalLast (255)  /**< Switch number of the last physical switch.  */
+#define kPRSwitchNeverDebounceFirst (192)  /**< Switch number of the first switch that doesn't need to debounced.  */
+#define kPRSwitchNeverDebounceLast (255)   /**< Switch number of the last switch that doesn't need to be debounce.   */
 #define kPRSwitchCount (256)
 #define kPRSwitchRulesCount (kPRSwitchCount << 2) /**< Total number of available switch rules. */
 
@@ -461,7 +470,7 @@ PINPROC_API PRResult PRSwitchUpdateConfig(PRHandle handle, PRSwitchConfig *switc
  * @param linkedDrivers An array of #PRDriverState structures describing the driver state changes to be made when this switch rule is triggered.  May be NULL if numDrivers is 0.
  * @param numDrivers Number of elements in the linkedDrivers array.  May be zero or more.
  */
-PINPROC_API PRResult PRSwitchUpdateRule(PRHandle handle, uint8_t switchNum, PREventType eventType, PRSwitchRule *rule, PRDriverState *linkedDrivers, int numDrivers);
+PINPROC_API PRResult PRSwitchUpdateRule(PRHandle handle, uint8_t switchNum, PREventType eventType, PRSwitchRule *rule, PRDriverState *linkedDrivers, int numDrivers, bool_t drive_outputs_now);
 
 /** Returns a list of PREventTypes describing the states of the requested number of switches  */
 PINPROC_API PRResult PRSwitchGetStates(PRHandle handle, PREventType * switchStates, uint16_t numSwitches);

makefile
@@ -114,11 +114,11 @@ endif
 
 # uncomment next line to include the symbols (for debugging)
 # [PinMAME] always create symbols file (separated from executable)
-SYMBOLS = 1
+#SYMBOLS = 1
 
 # uncomment next line to generate a link map for exception handling in windows
 # [PinMAME] always create map file
-MAP = 1
+#MAP = 1
 
 # uncomment next line to generate verbose build information
 # VERBOSE = 1
@@ -449,7 +449,7 @@ endif
 
 # [PinMAME] avoid dynamic dependencies on Windows, so link statically
 ifeq ($(MAMEOS),windows)
-LDFLAGS += -static -static-libgcc -static-libstdc++
+LDFLAGS += -static -static-libgcc
 endif
 
 

makefile.unix
@@ -65,6 +65,9 @@ TARGET = pinmame
 # Uncomment to use DRC MIPS3 engine
 # X86_MIPS3_DRC = 1
 
+# uncomment next line to include P-ROC support
+# see http://www.pinballcontrollers.com/
+PROC = 1
 
 ###########################################################################
 # Development environment options 
@@ -88,10 +91,12 @@ TARGET = pinmame
 # possible.
 
 #CC	= /usr/i686-pc-linux-gnu/gcc-bin/3.4.6/gcc
-# CC   = gcc
-CC	= cc
+#CC	= gcc-3.4.6
+CC    = cc
 # CC	= icc
 # CC	= c89
+
+CPP    = g++
 
 # Uncomment for Sun Forte 7 and above C/C++ compiler, see below for
 # additional optimizations.
@@ -351,7 +356,8 @@ IL	= '-DINLINE=static __inline__'
 ###########################################################################
 
 # Normal linking.
-LD	= $(CC) -Wl,-s
+#LD	= $(CC) -Wl,-s
+LD	= g++ -Wl,-s
 
 # For the Intel C++ Compiler -- expect large link time due to -ipo 
 # optimization.
@@ -465,13 +471,13 @@ XMAMEROOT = $(PREFIX)/share/x$(TARGET)
 ###########################################################################
 
 # i386, GNU asm
-MY_CPU = i386
+# MY_CPU = i386
 
 # i386, no asm -- needed for the Intel C++ compiler, which does not fully
 # understand gcc's inline assembly syntax, though you may still enable 
 # X86_ASM_68000, etc., which are assembled by NASM.  You may also need to 
 # use this for BeOS.
-# MY_CPU = i386_noasm
+MY_CPU = i386_noasm
 
 # IA64
 # MY_CPU = ia64
@@ -594,10 +600,10 @@ ARCH  = linux
 # use SDL.
 
 # X11
-DISPLAY_METHOD = x11
+#DISPLAY_METHOD = x11
 
 # SDL library 
-# DISPLAY_METHOD = SDL
+DISPLAY_METHOD = SDL
 
 # SVGALib, only supported under Linux
 # DISPLAY_METHOD = svgalib
@@ -742,6 +748,10 @@ NASM_FMT  = -f elf
 # ASM_STRIP = true  # no strip command
 # NASM_FMT  = -f aoutb
 
+ifdef PROC
+# Enable support for P-ROC
+CFLAGS += -DPROC_SUPPORT
+endif
 
 ###########################################################################
 # All done.  Type make -f makefile.unix and enjoy xmame/xmess.  ;)

src/driver.h
@@ -94,9 +94,9 @@ typedef struct {
   int dmd_red33, dmd_green33, dmd_blue33;
   int dmd_red0, dmd_green0, dmd_blue0;
 #ifdef PROC_SUPPORT
-  int p_roc;                   /* TODO/PROC: explanation */
-  int alpha_on_dmd;            /* Virtual alphanumeric displays on P-ROC DMD */
-  int virtual_dmd;             /* If we have no screen, then we can suppress the DMD */
+	char *p_roc;					/* YAML Machine description file */
+	int alpha_on_dmd;			/* Virtual alphanumeric displays on P-ROC DMD */
+        int virtual_dmd;                     /* If we have no screen, then we can suppress the DMD */
 #endif /* PROC_SUPPORT */
 } tPMoptions;
 extern tPMoptions pmoptions;

src/mame.c
@@ -347,7 +347,7 @@ int run_game(int game)
 static int init_machine(void)
 {
 #if defined(PINMAME) && defined(PROC_SUPPORT)
-	char * yaml_filename = (char *)pmoptions.p_roc;
+	char * yaml_filename = pmoptions.p_roc;
 #endif /* PINMAME && PROC_SUPPORT */
 
 	/* load the localization file */

commit a1bc2f93957cbeb76eb574cef4b611825c2c480b

fix IMHO wrong fuzzy-compare change introduced in c50171d22271a7233f173f7de17d55c8c914199a

src/unix/config.c
@@ -502,7 +502,7 @@ int config_init (int argc, char *argv[])
 		if (end == 0)
 			len = strlen(begin);
 		else
-			len = end - begin;            
+			len = end - begin;
 
 		for (i = 0; drivers[i]; i++)
 		{
@@ -521,7 +521,7 @@ int config_init (int argc, char *argv[])
 			}
 		}
 	}
-#endif                                
+#endif
 
 	/* educated guess on what the user wants to play */
 	if ( (game_index == -1) && use_fuzzycmp)
@@ -541,12 +541,11 @@ int config_init (int argc, char *argv[])
 				/* game is a clone */
 				if (drivers[i]->clone_of != 0 && !(drivers[i]->clone_of->flags & NOT_A_DRIVER))
 				{
-					if ((!drivers[game_index]->flags & GAME_NOT_WORKING) || (drivers[i]->flags & GAME_NOT_WORKING))
+					if ((!(drivers[game_index]->flags & GAME_NOT_WORKING)) || (drivers[i]->flags & GAME_NOT_WORKING))
 						continue;
 				}
 				else continue;
 			}
-
 
 			/* we found a better match */
 			game_index = i;